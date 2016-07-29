7d72bf68091fba73c37c3ea6b55019845a6ae1aa
wenchaomeng <oytmfc@gmail.com>
2016-07-29 13:50:38 +0800

    meta server use spring boot

--- core/src/main/java/com/ctrip/xpipe/spring/AbstractSpringConfigContext.java
+++ core/src/main/java/com/ctrip/xpipe/spring/AbstractSpringConfigContext.java
@@ -6,6 +6,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.ctrip.xpipe.lifecycle.SpringComponentLifecycleManager;
 
