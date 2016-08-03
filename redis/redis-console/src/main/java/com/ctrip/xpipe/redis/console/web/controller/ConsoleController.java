e3b1ab167d0b455fbcad27ea488883d14c2bc39a
shyin <tjuyinsh@gmail.com>
2016-08-03 10:58:07 +0800

    Project structure initialized

---
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/web/controller/ConsoleController.java
@@ -0,0 +1,16 @@
+package com.ctrip.xpipe.redis.console.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author shyin
+ *
+ * Aug 3, 2016
+ */
+
+@RestController
+@RequestMapping("/api")
+public class ConsoleController {
+	
+}
---
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/web/service/MetaService.java
@@ -0,0 +1,10 @@
+package com.ctrip.xpipe.redis.console.web.service;
+
+/**
+ * @author shyin
+ *
+ * Aug 3, 2016
+ */
+public class MetaService {
+
+}
