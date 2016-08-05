4ec49032481f968dae07ef27c966c18a59afc760
Jason Song <nobodyiam@gmail.com>
2016-08-05 15:22:36 +0800

    support dynamically loading apollo data source provider

--- redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/build/ComponentsConfigurator.java
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/build/ComponentsConfigurator.java
@@ -1,20 +1,27 @@
 package com.ctrip.xpipe.redis.console.build;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import com.ctrip.xpipe.redis.console.ds.XpipeDataSourceProvider;
+import org.unidal.dal.jdbc.datasource.DataSourceProvider;
 import org.unidal.lookup.configuration.AbstractResourceConfigurator;
 import org.unidal.lookup.configuration.Component;
 
-import com.ctrip.framework.apollo.ds.ApolloDataSourceProvider;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ComponentsConfigurator extends AbstractResourceConfigurator {
-   @Override
-   public List<Component> defineComponents() {
-      List<Component> all = new ArrayList<Component>();
+    @Override
+    public List<Component> defineComponents() {
+        List<Component> all = new ArrayList<Component>();
 
-      all.addAll(new FxxpipeDatabaseConfigurator().defineComponents());
-      all.add(A(ApolloDataSourceProvider.class));
-      return all;
-   }
+
+        all.addAll(new FxxpipeDatabaseConfigurator().defineComponents());
+        all.add(C(DataSourceProvider.class, XpipeDataSourceProvider.class)
+                .config(E("datasourceFile").value("datasources.xml"),
+                        E("baseDirRef").value("FXXPIPE_HOME")));
+        return all;
+    }
+
+    public static void main(String[] args) {
+        generatePlexusComponentsXmlFile(new ComponentsConfigurator());
+    }
 }
