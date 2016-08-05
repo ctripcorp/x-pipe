1a3af6becac672c074708244c1184d6b13d6cb42
wenchaomeng <oytmfc@gmail.com>
2016-08-05 09:53:10 +0800

    exceptio handler

---
+++ redis/redis-meta/src/main/java/com/ctrip/xpipe/redis/meta/server/spring/MetaExecptionHandler.java
@@ -0,0 +1,15 @@
+package com.ctrip.xpipe.redis.meta.server.spring;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import com.ctrip.xpipe.spring.AbstractExceptionHandler;
+
+/**
+ * @author wenchao.meng
+ *
+ * Aug 5, 2016
+ */
+@ControllerAdvice
+public class MetaExecptionHandler extends AbstractExceptionHandler{
+
+}
