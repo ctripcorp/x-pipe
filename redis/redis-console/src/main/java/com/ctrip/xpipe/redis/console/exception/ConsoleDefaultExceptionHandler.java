3db5d3ba2542e817f5670a635d34fbd96997928a
shyin <tjuyinsh@gmail.com>
2016-08-09 13:18:41 +0800

    Console Exception Handler

---
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/exception/ConsoleDefaultExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.ctrip.xpipe.redis.console.exception;
+
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.HttpMediaTypeException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.unidal.dal.jdbc.DalNotFoundException;
+
+import com.ctrip.xpipe.spring.AbstractExceptionHandler;
+
+/**
+ * @author shyin
+ *
+ * Aug 9, 2016
+ */
+
+@ControllerAdvice
+public class ConsoleDefaultExceptionHandler extends AbstractExceptionHandler {
+
+	// Dal Not Found Exception
+	@ExceptionHandler(DalNotFoundException.class)
+	public ResponseEntity<Map<String,Object>> dalNotFound(HttpServletRequest request, DalNotFoundException ex) {
+		return handleError(request,HttpStatus.NOT_FOUND, ex);
+	}
+	
+	@ExceptionHandler(DataNotFoundException.class)
+	public ResponseEntity<Map<String,Object>> dataNotFound(HttpServletRequest request, DataNotFoundException ex) {
+		return handleError(request,HttpStatus.NOT_FOUND, ex);
+	}
+	
+	@ExceptionHandler({HttpRequestMethodNotSupportedException.class, HttpMediaTypeException.class})
+	public ResponseEntity<Map<String, Object>> badRequest(HttpServletRequest request, ServletException ex) {
+	    return handleError(request, HttpStatus.BAD_REQUEST, ex);
+	  }
+
+}
--- redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/rest/metaserver/ConsoleController.java
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/rest/metaserver/ConsoleController.java
@@ -2,12 +2,22 @@ package com.ctrip.xpipe.redis.console.rest.metaserver;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.unidal.dal.jdbc.DalException;
 
+
+import com.ctrip.xpipe.api.codec.Codec;
+import com.ctrip.xpipe.codec.JsonCodec;
+import com.ctrip.xpipe.redis.console.exception.DataNotFoundException;
 import com.ctrip.xpipe.redis.console.service.MetaInfoService;
 import com.ctrip.xpipe.redis.core.entity.ClusterMeta;
 import com.ctrip.xpipe.redis.core.entity.DcMeta;
@@ -20,39 +30,84 @@ import com.ctrip.xpipe.redis.core.entity.ShardMeta;
 @RestController
 @RequestMapping("/api")
 public class ConsoleController {
+	private static Logger logger = LoggerFactory.getLogger(ConsoleController.class);
+	private static Codec coder = new JsonCodec();
 	
 	@Autowired
 	private MetaInfoService metaInfoService;
 	
-	@RequestMapping("/dc/{dcId}")
-	public DcMeta getDcMeta(@PathVariable String dcId) throws DalException {
-		return metaInfoService.getDcMeta(dcId);
+
+	@RequestMapping(value = "/dc/{dcId}", method = RequestMethod.GET, produces={MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public String getDcMeta(@PathVariable String dcId, @RequestParam(value="format", required = false) String format) {
+		
+		DcMeta result;
+		try {
+			result = metaInfoService.getDcMeta(dcId);
+		} catch (DalException e) {
+			logger.error(e.getMessage());
+			throw new DataNotFoundException(e.getMessage());
+		}
+		
+		return (format != null && format.equals("xml"))? result.toString() : coder.encode(result);
 	}
 	
-	@RequestMapping("/dc/{dcId}/cluster/{clusterId}")
-	public ClusterMeta getDcClusterMeta(@PathVariable String dcId,@PathVariable String clusterId) throws DalException {
-		return metaInfoService.getDcClusterMeta(dcId, clusterId);
+	@RequestMapping(value = "/dc/{dcId}/cluster/{clusterId}", method = RequestMethod.GET, produces={MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public String getDcClusterMeta(@PathVariable String dcId,@PathVariable String clusterId, @RequestParam(value="format", required = false) String format) {
+		
+		ClusterMeta result;
+		try {
+			result = metaInfoService.getDcClusterMeta(dcId, clusterId);
+		} catch (DalException e) {
+			logger.error(e.getMessage());
+			throw new DataNotFoundException(e.getMessage());
+		}
+		
+		return (format != null && format.equals("xml"))? result.toString() : coder.encode(result);
 	}
 	
-	@RequestMapping("/dc/{dcId}/cluster/{clusterId}/shard/{shardId}")
-	public ShardMeta getDcClusterShardMeta(@PathVariable String dcId,@PathVariable String clusterId,
-			@PathVariable String shardId) throws DalException {
-		return metaInfoService.getDcClusterShardMeta(dcId, clusterId, shardId);
+	@RequestMapping(value = "/dc/{dcId}/cluster/{clusterId}/shard/{shardId}", method = RequestMethod.GET, produces={MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public String getDcClusterShardMeta(@PathVariable String dcId,@PathVariable String clusterId,
+			@PathVariable String shardId, @RequestParam(value="format", required = false) String format){
+		
+		ShardMeta result;
+		try {
+			result = metaInfoService.getDcClusterShardMeta(dcId, clusterId, shardId);
+		} catch (DalException e) {
+			logger.error(e.getMessage());
+			throw new DataNotFoundException(e.getMessage());
+		}
+		
+		return (format != null && format.equals("xml"))? result.toString() : coder.encode(result);
 	}
 	
-	@RequestMapping("/dcids")
-	public List<String> getDcIds() throws DalException {
-		return metaInfoService.getAllDcIds();
+	@RequestMapping(value = "/dcids", method = RequestMethod.GET)
+	public List<String> getDcIds(){
+		try {
+			return metaInfoService.getAllDcIds();
+		} catch (DalException e) {
+			logger.error(e.getMessage());
+			throw new DataNotFoundException(e.getMessage());
+		}
 	}
 	
-	@RequestMapping("/clusterids")
-	public List<String> getClusterIds() throws DalException {
-		return metaInfoService.getAllClusterIds();
+	@RequestMapping(value = "/clusterids", method = RequestMethod.GET)
+	public List<String> getClusterIds(){
+		try {
+			return metaInfoService.getAllClusterIds();
+		} catch (DalException e) {
+			logger.error(e.getMessage());
+			throw new DataNotFoundException(e.getMessage());
+		}
 	}
 	
-	@RequestMapping("/cluster/{clusterId}/shardids")
-	public List<String> getShardIds(@PathVariable String clusterId) throws DalException {
-		return metaInfoService.getAllClusterShardIds(clusterId);
+	@RequestMapping(value = "/cluster/{clusterId}/shardids", method = RequestMethod.GET)
+	public List<String> getShardIds(@PathVariable String clusterId) {
+		try {
+			return metaInfoService.getAllClusterShardIds(clusterId);
+		} catch (DalException e) {
+			logger.error(e.getMessage());
+			throw new DataNotFoundException(e.getMessage());
+		}
 	}
 
 }
