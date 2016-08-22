ed9af4e29d2e70302d80a44f2a2688c99992bd60
wenchaomeng <oytmfc@gmail.com>
2016-08-22 20:33:42 +0800

    travis

--- core/src/main/java/com/ctrip/xpipe/utils/FileUtils.java
+++ core/src/main/java/com/ctrip/xpipe/utils/FileUtils.java
@@ -62,8 +62,10 @@ public class FileUtils {
 		
 		//try classpath
 		URL url = clazz.getResource(fileName);
+		logger.info("[tryResource]{}", url);
 		if(url == null){
 			url = clazz.getClassLoader().getResource(fileName);
+			logger.info("[tryResourceClassLoader]{}", url);
 		}
 		if(url != null){
 			try {
