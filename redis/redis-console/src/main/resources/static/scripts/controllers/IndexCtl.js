233d79a7124b29ec6149524a74fa41feaf1fa43c
shyin <tjuyinsh@gmail.com>
2016-08-30 16:57:27 +0800

    Frontend structure modified

--- redis/redis-console/src/main/resources/static/scripts/controllers/IndexCtl.js
+++ redis/redis-console/src/main/resources/static/scripts/controllers/IndexCtl.js
@@ -1,7 +1,7 @@
 index_module.controller('IndexCtl', ['$rootScope', '$scope',
     function ($rootScope, $scope) {
 
-        $rootScope.currentNav = '';
+        $rootScope.currentNav = '1-2';
         $rootScope.switchNav = switchNav;
         
         function switchNav(nav) {
