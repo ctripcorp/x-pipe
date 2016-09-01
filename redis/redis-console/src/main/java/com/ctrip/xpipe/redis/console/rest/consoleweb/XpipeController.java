ddb3edd1e2dea36d1085f8be96738567f0d03897
zhangle <zhanglea@ctrip.com>
2016-09-01 18:16:07 +0800

    redis page design
    shard page front

--- redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/rest/consoleweb/XpipeController.java
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/rest/consoleweb/XpipeController.java
@@ -2,12 +2,15 @@ package com.ctrip.xpipe.redis.console.rest.consoleweb;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 /**
  * @author zhangle 16/8/24
  */
+@RestController
+@RequestMapping("console")
 public class XpipeController {
 
   @RequestMapping(value = "/ips/{ip}/ports")
@@ -15,5 +18,6 @@ public class XpipeController {
 	  // TODO
     return null;
   }
+
 
 }
--- redis/redis-console/src/main/resources/static/scripts/controllers/ClusterDcShardUpdateCtl.js
+++ redis/redis-console/src/main/resources/static/scripts/controllers/ClusterDcShardUpdateCtl.js
@@ -1,142 +1,273 @@
-index_module.controller('ClusterDcShardUpdateCtl', ['$rootScope', '$scope', '$stateParams', '$window', '$location', 'toastr', 'AppUtil', 'ClusterService', 'ShardService',
-    function ($rootScope, $scope, $stateParams, $window, $location, toastr, AppUtil, ClusterService, ShardService) {
+index_module.controller('ClusterDcShardUpdateCtl',
+                        ['$rootScope', '$scope', '$stateParams', '$window', '$location', 'toastr', 'AppUtil',
+                         'ClusterService', 'ShardService', 'RedisService', 'KeeperContainerService',
+                         function ($rootScope, $scope, $stateParams, $window, $location, toastr, AppUtil,
+                                   ClusterService, ShardService, RedisService, KeeperContainerService) {
 
-        $scope.dcs, $scope.dcActiveTab, $scope.shards;
-        $scope.clusterName = $stateParams.clusterName;
-        $scope.shardName = $stateParams.shardName;
-        
-        $scope.switchDc = switchDc;
-        $scope.loadCluster = loadCluster;
-        $scope.loadShard = loadShard;
-        
-        $scope.preCreateRedis = preCreateRedis;
-        $scope.createRedis = createRedis;
-        $scope.preDeleteRedis = preDeleteRedis;
-        $scope.deleteRedis = deleteRedis;
+                             $scope.dcs, $scope.dcActiveTab,
+                                 $scope.hasMasterRedis = false, 
+                                 $scope.createKeeperErrorMsg = '';
+                             $scope.dcShards = {};
+                             $scope.clusterName = $stateParams.clusterName;
+                             $scope.shardName = $stateParams.shardName;
 
-        $scope.submitUpdates = submitUpdates;
+                             $scope.switchDc = switchDc;
+                             $scope.loadCluster = loadCluster;
+                             $scope.loadShard = loadShard;
+
+                             $scope.preCreateRedis = preCreateRedis;
+                             $scope.createRedis = createRedis;
+
+                             $scope.preCreateKeeper = preCreateKeeper;
+                             $scope.createKeeper = createKeeper;
+                             $scope.addCreateBackupKeeperForm = addCreateBackupKeeperForm;
+                             $scope.removeCreateBackupKeeperForm = removeCreateBackupKeeperForm;
+                             $scope.selectKeeperContainer = selectKeeperContainer;
+
+                             $scope.preDeleteRedis = preDeleteRedis;
+                             $scope.deleteRedis = deleteRedis;
+
+                             $scope.submitUpdates = submitUpdates;
+
+                             if ($scope.clusterName) {
+                                 loadCluster();
+                             }
+
+                             function findKeeperContainers(dcName) {
+                                 KeeperContainerService.findKeeperContainersByDc(dcName)
+                                     .then(function (result) {
+                                         $scope.keeperContainers = result;
+                                     })
+
+                             }
+
+                             function switchDc(dc) {
+                                 $scope.currentDcName = dc.dcName;
+                                 findKeeperContainers($scope.currentDcName);
+
+                                 var shard = $scope.dcShards[$scope.currentDcName];
+
+                                 if (!shard){
+                                     loadShard($scope.clusterName, dc.dcName, $scope.shardName);
+                                 }
+
+                             }
+
+                             function loadCluster() {
+                                 ClusterService.findClusterDCs($scope.clusterName)
+                                     .then(function (result) {
+                                         if (!result || result.length == 0) {
+                                             $scope.dcs = [];
+                                             return;
+                                         }
+                                         $scope.dcs = result;
+                                         $scope.currentDcName = $scope.dcs[0].dcName;
+                                         findKeeperContainers($scope.currentDcName);
+                                         loadShard($scope.clusterName, $scope.dcs[0].dcName, $scope.shardName);
+
+                                     }, function (result) {
+                                         toastr.error(AppUtil.errorMsg(result));
+                                     });
+
+                             }
+
+                             function loadShard(clusterName, dcName, shardName) {
+                                 ShardService.findClusterDcShard(clusterName, dcName, shardName)
+                                     .then(function (result) {
+                                         $scope.dcShards[dcName] = result;
+
+                                         refreshShardStatus();
+
+                                     }, function (result) {
+                                         toastr.error(AppUtil.errorMsg(result));
+                                     });
+                             }
+
+                             function preCreateRedis() {
+                                 $scope.toCreateRedis = {};
+
+                                 $('#createRedisModal').modal('show');
+                             }
+
+                             function createRedis() {
+                                 $scope.toCreateRedis.id = 0;
+                                 var shard = $scope.dcShards[$scope.currentDcName];
+                                 shard.redises.push($scope.toCreateRedis);
+                                 $scope.toCreateRedis = {};
+                                 $('#createRedisModal').modal('hide');
+
+                                 refreshShardStatus();
+                             }
+
+                             function addCreateBackupKeeperForm() {
+                                 $scope.toCreateBackupKeepers.push({});
+                             }
+
+                             function removeCreateBackupKeeperForm(index) {
+                                 $scope.toCreateBackupKeepers.splice(index, 1);
+                             }
+                             
+                             function selectKeeperContainer(selectedKeeperContainerId) {
+                                 var index = indexOfKeeperContainer(selectedKeeperContainerId, $scope.freeKeeperContainers);
+                                 if (index >= 0){
+                                     $scope.freeKeeperContainers = _.clone($scope.freeKeeperContainers);
+                                     $scope.freeKeeperContainers.splice(index, 1);
+                                 }
+                             }
+
+                             function preCreateKeeper() {
+                                 if (!$scope.hasActiveKeeper) {
+                                     $scope.toCreateActiveKeeper = {};
+                                     $scope.toCreateActiveKeeper.active = true;
+                                 }
+
+                                 // init backup container
+                                 $scope.toCreateBackupKeepers = [];
+                                 $scope.toCreateBackupKeepers.push({});
+
+                                 $('#createKeeperModal').modal('show');
+                             }
+
+                             function createKeeper() {
+                                 $scope.createKeeperErrorMsg = '';
+                                 var shard = $scope.dcShards[$scope.currentDcName];
+
+                                 if (!$scope.hasActiveKeeper){
+                                     if (!validKeeper($scope.toCreateActiveKeeper)){
+                                         $scope.createKeeperErrorMsg = "valid form content please check";
+                                         return;
+                                     }else {
+                                         shard.keepers.push($scope.toCreateActiveKeeper);
+                                     }
+                                 }
+
+                                 $scope.toCreateBackupKeepers.forEach(function (backupKeeper) {
+                                     if (!validKeeper(backupKeeper)){
+                                         $scope.createKeeperErrorMsg = "valid form content please check";
+                                         return;
+                                     }else {
+                                         shard.keepers.push(backupKeeper);
+                                     }
+                                 });
+
+                                 if (!$scope.createKeeperErrorMsg){
+                                     $('#createKeeperModal').modal('hide');
+                                     refreshShardStatus();
+                                 }
+
+                             }
+
+                             function validKeeper(keeper) {
+                                 return keeper && keeper.ip && keeper.port;
+                             }
+
+                             function preDeleteRedis(redis) {
+                                 $scope.toDeleteRedis = {};
+                                 $scope.toDeleteRedis = redis;
+                                 $('#deleteRedisConfirm').modal('show');
+                             }
+
+                             function deleteRedis() {
+                                 var shard = $scope.dcShards[$scope.currentDcName];
+                                 var index = -1;
+                                 for (var cnt_redis = 0; cnt_redis != shard.redises.length; ++cnt_redis) {
+                                     if ($scope.toDeleteRedis == shard.redises[cnt_redis]) {
+                                         index = cnt_redis;
+                                         break;
+                                     }
+                                 }
+                                 if (index != -1) {
+                                     shard.redises.splice(index, 1);
+                                     $scope.toDeleteRedis = {};
+                                     refreshShardStatus();
+                                     return;
+                                 }
+
+                                 for (var cnt_keeper = 0; cnt_keeper != shard.keepers.length; ++cnt_keeper) {
+                                     if ($scope.toDeleteRedis == shard.keepers[cnt_keeper]) {
+                                         index = cnt_keeper;
+                                         break;
+                                     }
+                                 }
+                                 if (index != -1) {
+                                     shard.keepers.splice(index, 1);
+                                     $scope.toDeleteRedis = {};
+                                     refreshShardStatus();
+                                     return;
+                                 }
+
+                             }
+
+                             function submitUpdates() {
+
+                                 var shard = $scope.dcShards[$scope.currentDcName];
+
+                                 //pre check
+                                 //must have and only have one active keeper
+                                 var activeKeeperCnt = 0;
+                                 shard.keepers.forEach(function (keeper) {
+                                     if (keeper.active){
+                                         activeKeeperCnt ++;
+                                     }
+                                 });
+                                 if (activeKeeperCnt != 1){
+                                    toastr.error("must have and only have one active keeper");
+                                     return;
+                                 }
+
+                                 RedisService.updateShardRedis($scope.clusterName, $scope.currentDcName, shard.id, shard)
+                                     .then(function (result) {
+                                         toastr.success("operator success");
+                                     }, function (result) {
+                                         toastr.error(AppUtil.errorMsg(result), "operator fail");
+                                     });
+
+                             }
+
+                             function refreshShardStatus() {
+                                 $scope.hasRedisMaster = false;
+
+                                 var shard = $scope.dcShards[$scope.currentDcName];
+
+                                 if (shard.redises && shard.redises.length) {
+                                     shard.redises.forEach(function (redis) {
+                                         if (redis.master == "" || redis.master == "true") {
+                                             $scope.hasRedisMaster = true;
+                                             redis.master = "true";
+                                         } else {
+                                             redis.master = "false";
+                                         }
+
+                                     })
+                                 }
+
+                                 $scope.freeKeeperContainers = $scope.keeperContainers;
+                                 $scope.hasActiveKeeper = false;
+                                 if (shard.keepers && shard.keepers.length > 0){
+                                     shard.keepers.forEach(function (keeper) {
+                                         var index = indexOfKeeperContainer(keeper.keeperContainerId, $scope.keeperContainers);
+                                         if (index >= 0){
+                                             $scope.freeKeeperContainers.splice(index, 1);
+                                         }
+                                         if (keeper.active){
+                                             $scope.hasActiveKeeper = true;
+                                         }
+                                     })
+                                 }
+
+                             }
+
+                             function indexOfKeeperContainer(keeperContainerId, keeperContainers) {
+                                 var index = -1;
+                                 keeperContainers.forEach(function (keeperContainer, i) {
+                                     if (keeperContainer.keepercontainerId == keeperContainerId){
+                                         index = i;
+                                         return;
+                                     }
+                                 })
+
+                                 return index;
+                             }
 
 
-        if ($scope.clusterName) {
-            loadCluster();
-        }
-        
-        function switchDc(dc) {
-            $scope.currentDcName = dc.dcName;
-            loadShard($scope.clusterName, dc.dcName, $scope.shardName);
-        }
-
-        function loadCluster() {
-            ClusterService.findClusterDCs($scope.clusterName)
-                .then(function (result) {
-                    if (!result || result.length == 0) {
-                        $scope.dcs = [];
-                        $scope.shards = [];
-                        return;
-                    }
-                    $scope.dcs = result;
-                    $scope.currentDcName = $scope.dcs[0].dcName;
-
-                    loadShard($scope.clusterName, $scope.dcs[0].dcName, $scope.shardName);
-
-                }, function (result) {
-                    toastr.error(AppUtil.errorMsg(result));
-                });
-
-
-        }
-
-        function loadShard(clusterName, dcName, shardName) {
-            ShardService.findClusterDcShard(clusterName, dcName, shardName)
-                .then(function (result) {
-                    $scope.shard = result;
-                }, function (result) {
-                    toastr.error(AppUtil.errorMsg(result));
-                });
-        }
-
-
-        function preCreateRedis(type) {
-            $scope.toCreateRedis = {};
-            $scope.toCreateRedis.redisRole = type;
-
-            $('#createRedisModal').modal('show');
-        }
-
-        function createRedis() {
-        	if('redis' === $scope.toCreateRedis.redisRole) {
-        		$scope.toCreateRedis.id = 0;
-        		$scope.shard.redises.push($scope.toCreateRedis);
-        		$scope.toCreateRedis = {};
-        		$('#createRedisModal').modal('hide');
-        	} 
-        	if ('keeper' === $scope.toCreateRedis.redisRole){
-        		$scope.toCreateRedis.id = 0;
-        		$scope.shard.keepers.push($scope.toCreateRedis);
-        		$scope.toCreateRedis = {};
-        		$('#createRedisModal').modal('hide');
-        	}
-        }
-        
-        function preDeleteRedis(redis) {
-        	$scope.toDeleteRedis = {};
-            $scope.toDeleteRedis = redis;
-            $('#deleteRedisConfirm').modal('show');
-        }
-
-        function deleteRedis() {
-        	var index = -1;
-        	for(var cnt_redis = 0 ; cnt_redis != $scope.shard.redises.length; ++cnt_redis) {
-        		if($scope.toDeleteRedis == $scope.shard.redises[cnt_redis]) {
-        			index = cnt_redis;
-        			break;
-        		}
-        	}
-        	if(index != -1) {
-        		$scope.shard.redises.splice(index,1);
-        		$scope.toDeleteRedis = {};
-        		return;
-        	}
-        	
-        	for(var cnt_keeper = 0; cnt_keeper != $scope.shard.keepers.length; ++cnt_keeper) {
-        		if($scope.toDeleteRedis == $scope.shard.keepers[cnt_keeper]) {
-        			index = cnt_keeper;
-        			break;
-        		}
-        	}
-        	if(index != -1) {
-        		$scope.shard.keepers.splice(index,1);
-        		$scope.toDeleteRedis = {};
-        		return;
-        	}
-        	$scope.toDeleteRedis = {};
-        }
-
-        function submitUpdates() {
-        	var toUpdateRedises = [];
-        	for(var cnt_redis = 0 ; cnt_redis != $scope.shard.redises.length; ++cnt_redis) {
-        		var proto = {};
-        		var toUpdateRedis = $scope.shard.redises[cnt_redis];
-        		proto.redisName = toUpdateRedis.id;
-        		proto.redisIp = toUpdateRedis.ip;
-        		proto.redisPort = toUpdateRedis.port;
-        		// master check & set
-        		
-        		toUpdateRedises.push(proto);
-        	}
-        	for(var cnt_keeper = 0 ; cnt_keeper != $scope.shard.keepers.length; ++cnt_keeper) {
-        		var proto = {};
-        		var toUpdateRedis = $scope.shard.keepers[cnt_keeper];
-        		proto.redisName = toUpdateRedis.id;
-        		proto.redisIp = toUpdateRedis.ip;
-        		proto.redisPort = toUpdateRedis.port;
-        		// master check & set
-        		proto.keeperContainerId = toUpdateRedis.keeperContainerId;
-        		
-        		toUpdateRedis.push(proto);
-        	}
-        	
-        	// call service to update
-        }
-        
-    }]);
+                         }]);
