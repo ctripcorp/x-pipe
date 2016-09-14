7a0c39f0028662928ea337b85d1e695439647950
liuyi007 <964193365@qq.com>
2016-09-14 13:05:41 +0800

    console api junit test

--- redis/redis-integrated-test/src/test/java/com/ctrip/xpipe/redis/integratedtest/consoleapi/ConsoleApiTest.java
+++ redis/redis-integrated-test/src/test/java/com/ctrip/xpipe/redis/integratedtest/consoleapi/ConsoleApiTest.java
@@ -3,7 +3,14 @@ package com.ctrip.xpipe.redis.integratedtest.consoleapi;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.ctrip.xpipe.redis.core.entity.ClusterMeta;
 import com.ctrip.xpipe.redis.integratedtest.consoleapi.util.ApiTestExecitorPool;
@@ -14,9 +21,11 @@ import com.ctrip.xpipe.redis.integratedtest.consoleapi.util.ApiTestExecitorPool;
  *         Sep 9, 2016
  */
 public class ConsoleApiTest {
-
+	private Logger logger = LoggerFactory.getLogger(getClass());
 	private static Properties p = new Properties();
-	static {
+	
+	@Before
+	public void loadProperties(){
 		try {
 			p.load(new FileInputStream("/opt/data/100004374/console.properties"));
 		} catch (FileNotFoundException e) {
@@ -25,9 +34,37 @@ public class ConsoleApiTest {
 			e.printStackTrace();
 		}
 	}
-
-	public static void main(String[] args) {
-		new ApiTestExecitorPool("apiName", p.getProperty("apiName"),
-				ClusterMeta.class, 5).doTest();
+	@Test
+	public void apiTest(){
+		//apiNames=apiName1,apiName2,apiName3
+		String[]apiNames=p.getProperty("apiNames").split(",");
+		List<ApiTestExecitorPool> apiTests=new ArrayList<ApiTestExecitorPool>();
+		for(String apiName:apiNames){
+			//apiName=url
+			apiTests.add(new ApiTestExecitorPool(apiName, p.getProperty(apiName),
+					ClusterMeta.class));
+		}
+		for(ApiTestExecitorPool apiTest:apiTests){
+			apiTest.doTest(1,10,100);
+		}
+		int successNum=0;
+		while(true){
+			for(ApiTestExecitorPool apiTest:apiTests){
+				if(apiTest.isOver){
+					if(apiTest.isPass){
+						successNum++;
+					}else{
+						apiTest.printErrorMessages();
+						logger.error("{} did not pass the test",apiTest.getApiName());
+						throw new RuntimeException(String.format("%s did not pass the test", apiTest.getApiName()));
+					}
+					
+				}
+			}
+			if(apiTests.size()==successNum){
+				break;
+			}
+			successNum=0;
+		}
 	}
 }
--- redis/redis-integrated-test/src/test/java/com/ctrip/xpipe/redis/integratedtest/consoleapi/util/ApiTestExecitorPool.java
+++ redis/redis-integrated-test/src/test/java/com/ctrip/xpipe/redis/integratedtest/consoleapi/util/ApiTestExecitorPool.java
@@ -1,13 +1,22 @@
 package com.ctrip.xpipe.redis.integratedtest.consoleapi.util;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Vector;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.concurrent.FutureCallback;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.client.RestTemplate;
 
 import com.ctrip.xpipe.redis.core.transform.DefaultSaxParser;
-import com.ctrip.xpipe.spring.RestTemplateFactory;
 
 /**
  * @author liu
@@ -17,16 +26,27 @@ import com.ctrip.xpipe.spring.RestTemplateFactory;
 public class ApiTestExecitorPool extends AbstractExecutorPool {
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
-	private AtomicInteger SUCCESS_NUM = new AtomicInteger(0);
-	private AtomicInteger FAIL_NUM = new AtomicInteger(0);
-	private int testNum;
+	private AtomicInteger successNum = new AtomicInteger(0);
+	private AtomicLong successTotalDelay = new AtomicLong(0);
+	private AtomicInteger failNum = new AtomicInteger(0);
+	private AtomicLong failTotalDelay = new AtomicLong(0);
+
+	private int threadNum = 1;
+	private int threadExecutionNum= 100;
+	private long threadSleepMsec = 100;
+
 	private String url;
 	private String apiName;
-	private RestTemplate restTemplate = RestTemplateFactory
-			.createCommonsHttpRestTemplate(10, 100, 5000, 5000);
-
 	@SuppressWarnings("rawtypes")
 	private Class type;
+	private Object defaultObj;
+	private List<String> errorMessages = new Vector<String>();
+
+	private CloseableHttpAsyncClient httpClient = HttpAsyncClients
+			.createDefault();
+
+	public boolean isOver = false;
+	public boolean isPass = false;
 
 	@SuppressWarnings("unused")
 	private ApiTestExecitorPool() {
@@ -34,75 +54,185 @@ public class ApiTestExecitorPool extends AbstractExecutorPool {
 	}
 
 	public ApiTestExecitorPool(String apiName, String url,
-			@SuppressWarnings("rawtypes") Class type, int testNum) {
+			@SuppressWarnings("rawtypes") Class type) {
 		super();
 		this.url = url;
-		this.testNum = testNum;
 		this.apiName = apiName;
 		this.type = type;
 	}
 
 	public void doTest() {
-		for (int i = 1; i <= testNum; i++) {
+		doTest(threadNum, threadExecutionNum, threadSleepMsec);
+	}
+
+	public void doTest(int threadNum, int threadExecutionNum, long threadSleepMsec) {
+		this.threadNum = threadNum;
+		this.threadExecutionNum = threadExecutionNum;
+		this.threadSleepMsec = threadSleepMsec;
+		httpClient.start();
+		for (long i = 0; i < threadNum; i++) {
 			this.addThread(getApiName());
 		}
 	}
 
+	private Object initDefaultObj(Object obj) {
+		if (defaultObj == null) {
+			synchronized (super.fixedThreadPool) {
+				if (defaultObj == null)
+					defaultObj = obj;
+			}
+		}
+		return defaultObj;
+	}
+
 	@Override
 	protected void test() {
-		boolean isOk = false;
-		try {
-			String result = restTemplate.getForObject(url, String.class);
-			@SuppressWarnings("unchecked")
-			Object obj = DefaultSaxParser.parseEntity(type, result);
-			if (obj != null)
-				isOk = true;
-		} catch (Exception e) {
-			logger.error("Exception", e);
-		} finally {
-			if (isOk) {
-				addSuccess();
-			} else {
-				addFail();
-			}
-			if ((getSuccess() + getFail()) == getTestNum()) {
-				logger.error("{} ，success:{}，fail:{}", getApiName(),
-						getSuccess(), getFail());
-				fixedThreadPool.shutdown();
+		HttpGet request = new HttpGet(url);
+		for (int i = 0; i < threadExecutionNum; i++) {
+			sendHttp(request);
+			try {
+				Thread.sleep(threadSleepMsec);
+			} catch (InterruptedException e) {
 			}
 		}
 	}
 
-	protected int getTestNum() {
-		return this.testNum;
+	public void handleHttpReturn(String result, long loseTime,
+			String errorMessage) {
+		boolean isOk = false;
+		try {
+			@SuppressWarnings("unchecked")
+			Object obj = result == null ? null : DefaultSaxParser.parseEntity(
+					type, result);
+			if (obj != null && obj.equals(initDefaultObj(obj))) {
+				isOk = true;
+			}
+		} catch (Exception e) {
+			if (errorMessage == null)
+				errorMessage = e.getMessage();
+		} finally {
+			int no = isOk ? addSuccess(loseTime) : addFail(loseTime);
+			logger.error("{}--->{} ，{}[delay:{}ms]", no, getApiName(),
+					isOk ? "success" : "failed", loseTime);
+			if (errorMessage != null) {
+				errorMessages
+						.add(String
+								.format("ErrorMessages%d--->%s ，Exception[delay:%dms,message:%s]",
+										no, getApiName(), loseTime,
+										errorMessage));
+			}
+			if (no >= this.threadNum * this.threadExecutionNum) {
+				logger.error(
+						"TOTAL--->{} ，success[count:{} , averageDelay:{}ms]，failed[count:{} , averageDelay:{}ms]",
+						getApiName(), getSuccess(), getSucAverageDelay(),
+						getFail(), getFaiAverageDelay());
+				logger.error(
+						"END--->{}>>>>============ BYEBYE ============<<<<",
+						getApiName());
+				try {
+					httpClient.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				fixedThreadPool.shutdown();
+				if (getFail() > 0) {
+					isPass = false;
+				}else{
+					isPass = true;
+				}
+				isOver = true;
+				// System.exit(0);
+			}
+		}
+	}
+
+	private void sendHttp(HttpGet request) {
+
+		httpClient.execute(request, new FutureCallback<HttpResponse>() {
+
+			public long startTime;
+
+			public void cancelled() {
+			}
+
+			public void completed(HttpResponse arg0) {
+				String content = null;
+				String errorMessage=null;
+				try {
+					content = EntityUtils.toString(arg0.getEntity(), "UTF-8");
+				} catch (Exception e) {
+					logger.error("completed exception",e);
+					errorMessage=e.getMessage();
+				}
+				handleHttpReturn(content, System.currentTimeMillis()
+						- startTime, errorMessage);
+			}
+
+			public void failed(Exception e) {
+				handleHttpReturn(null, System.currentTimeMillis() - startTime,
+						e.getMessage());
+			}
+
+			public FutureCallback<HttpResponse> setMystartTime(long startTime) {
+				this.startTime = startTime;
+				return this;
+			}
+		}.setMystartTime(System.currentTimeMillis()));
+	}
+
+	public void printErrorMessages() {
+		logger.error(
+				"ErrorMessages--->{}，start>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",
+				getApiName());
+		for (String errorMessage : errorMessages) {
+			logger.error(errorMessage);
+		}
+		logger.error(
+				"ErrorMessages--->{}，end>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",
+				getApiName());
 	}
 
 	@Override
 	protected int getPoolSize() {
-		return 5;
+		return 20;
 	}
 
 	protected String getUrl() {
 		return this.url;
 	}
 
-	protected void addSuccess() {
-		SUCCESS_NUM.addAndGet(1);
+	protected int addSuccess(long successDelay) {
+		successTotalDelay.addAndGet(successDelay);
+		return successNum.addAndGet(1);
 	}
 
-	protected void addFail() {
-		FAIL_NUM.addAndGet(1);
+	protected int addFail(long failDelay) {
+		failTotalDelay.addAndGet(failDelay);
+		return failNum.addAndGet(1);
 	}
 
 	protected int getSuccess() {
-		return SUCCESS_NUM.get();
+		return successNum.get();
 	}
 
 	protected int getFail() {
-		return FAIL_NUM.get();
+		return failNum.get();
+	}
+
+	protected long getSucAverageDelay() {
+		if (successNum.get() == 0)
+			return 0;
+		return successTotalDelay.get() / successNum.get();
+	}
+
+	protected long getFaiAverageDelay() {
+		if (failNum.get() == 0)
+			return 0;
+		return failTotalDelay.get() / failNum.get();
 	}
 
 	public String getApiName() {
 		return apiName;
 	}
+
 }
