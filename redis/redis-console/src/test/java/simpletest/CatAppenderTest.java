aa0cd6643e54e7064c68b070820ea4997ea48f12
shyin <tjuyinsh@gmail.com>
2016-09-22 14:51:23 +0800

    Use default Log4j2 appender detected

--- redis/redis-console/src/test/java/simpletest/CatAppenderTest.java
+++ redis/redis-console/src/test/java/simpletest/CatAppenderTest.java
@@ -1,17 +1,16 @@
 package simpletest;
 
 import org.junit.Test;
-
 import com.ctrip.xpipe.redis.console.AbstractConsoleTest;
 
 /**
  * @author shyin
  *
- * Aug 11, 2016
+ *         Aug 11, 2016
  */
 
-public class CatAppenderTest extends AbstractConsoleTest{
-	
+public class CatAppenderTest extends AbstractConsoleTest {
+
 	@Test
 	public void catAppenderTest() throws InterruptedException {
 		logger.error("CatAppender error message", new Exception("simple exception"));
