2489cd5ae1fc1199453052a7203255e7d1b126bb
shyin <tjuyinsh@gmail.com>
2016-09-27 18:01:11 +0800

    Retryable restoperations

---
+++ core/src/main/java/com/ctrip/xpipe/retry/RestOperationsRetryPolicy.java
@@ -0,0 +1,37 @@
+package com.ctrip.xpipe.retry;
+
+import org.springframework.web.client.ResourceAccessException;
+
+import com.ctrip.xpipe.api.retry.RetryPolicy;
+
+/**
+ * @author shyin
+ *
+ *         Sep 20, 2016
+ */
+public class RestOperationsRetryPolicy extends AbstractRetryPolicy implements RetryPolicy {
+
+	private int retryInterval;
+	
+	public RestOperationsRetryPolicy() {
+		this(5);
+	}
+
+	public RestOperationsRetryPolicy(int retryInterval) {
+		this.retryInterval = retryInterval;
+	}
+
+	@Override
+	public boolean retry(Throwable e) {
+		if(e instanceof ResourceAccessException) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	protected int getSleepTime(int currentRetryTime) {
+		return (retryInterval <= 0) ? 0 : retryInterval;
+	}
+
+}
---
+++ core/src/test/java/com/ctrip/xpipe/retry/RetryableRestOperationsTest.java
@@ -0,0 +1,117 @@
+package com.ctrip.xpipe.retry;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.mockito.Matchers.any;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner.Mode;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestOperations;
+
+import com.ctrip.xpipe.AbstractTest;
+import com.ctrip.xpipe.api.retry.RetryPolicy;
+import com.ctrip.xpipe.simpleserver.SimpleTestSpringServer;
+import com.ctrip.xpipe.spring.RestTemplateFactory;
+
+/**
+ * @author shyin
+ *
+ *         Sep 20, 2016
+ */
+public class RetryableRestOperationsTest extends AbstractTest {
+	ConfigurableApplicationContext ctx;
+	int port;
+	String targetResponse;
+
+	@Before
+	public void startUp() {
+		port = randomPort(8081, 9090);
+		targetResponse = randomString();
+		System.setProperty("server.port", String.valueOf(port));
+		System.setProperty("target-response", targetResponse);
+		SpringApplication app = new SpringApplication(SimpleTestSpringServer.class);
+		app.setBannerMode(Mode.OFF);
+		ctx = app.run("");
+		ctx.start();
+	}
+
+	@After
+	public void tearDown() {
+		if (null != ctx && ctx.isActive()) {
+			ctx.close();
+		}
+	}
+
+	@Test
+	public void retryableRestOperationsSuccessTest() throws Exception {
+		RestOperations restOperations = RestTemplateFactory.createCommonsHttpRestTemplate();
+		assertEquals(targetResponse, restOperations.getForObject(generateRequestURL(), String.class));
+	}
+
+	@Test
+	public void retryableRestOperationsFailWithMethodNotSupportedTest() {
+		RestOperations restOperations = RestTemplateFactory.createCommonsHttpRestTemplate();
+		try {
+			restOperations.put(generateRequestURL(), null);
+			fail();
+		} catch (Exception e) {
+			assertTrue(e instanceof HttpClientErrorException);
+		}
+	}
+	
+	@Test
+	public void retryableRestOperationsFailAndRetrySuccessTest() throws InterruptedException {
+		ctx.close();
+		RestOperations restOperations = RestTemplateFactory.createCommonsHttpRestTemplate(10, 100, 5000, 5000, 30,
+				RetryPolicyFactories.newRestOperationsRetryPolicyFactory(100));
+		Thread appStartThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				logger.info(remarkableMessage("New SpringApplication"));
+				SpringApplication app2 = new SpringApplication(SimpleTestSpringServer.class);
+				app2.setBannerMode(Mode.OFF);
+				ctx = app2.run("");
+				ctx.start();
+			}
+		});
+		appStartThread.start();
+		String response = restOperations.getForObject(generateRequestURL(), String.class);
+		assertEquals(targetResponse, response);
+		appStartThread.join();
+	}
+
+	@Test
+	public void retryableRestOperationsFailTest() {
+		ctx.close();
+
+		int retryTimes = 10;
+		RetryPolicyFactory mockedRetryPolicyFactory = Mockito.mock(RetryPolicyFactory.class);
+		RetryPolicy mockedRetryPolicy = Mockito.mock(RetryPolicy.class);
+		when(mockedRetryPolicyFactory.create()).thenReturn(mockedRetryPolicy);
+		when(mockedRetryPolicy.retry(any(Throwable.class))).thenReturn(true);
+		RestOperations restOperations = RestTemplateFactory.createCommonsHttpRestTemplate(10, 100, 5000, 5000,
+				retryTimes, mockedRetryPolicyFactory);
+		try {
+			restOperations.getForObject(generateRequestURL(), String.class);
+		} catch (Exception e) {
+			verify(mockedRetryPolicy, times(retryTimes)).retry(any(Throwable.class));
+			// check the type of original exception
+			assertTrue(e instanceof ResourceAccessException);
+		}
+
+	}
+
+	private String generateRequestURL() {
+		return "http://localhost:" + String.valueOf(port) + "/test";
+	}
+
+}
