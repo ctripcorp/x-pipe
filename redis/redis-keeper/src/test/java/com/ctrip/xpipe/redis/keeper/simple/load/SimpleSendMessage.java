5a20830e2f749dd79ad61fd946945eda7e5c3b5e
wenchaomeng <oytmfc@gmail.com>
2016-09-29 17:02:32 +0800

    direct pooled buffer

--- redis/redis-keeper/src/test/java/com/ctrip/xpipe/redis/keeper/simple/load/SimpleSendMessage.java
+++ redis/redis-keeper/src/test/java/com/ctrip/xpipe/redis/keeper/simple/load/SimpleSendMessage.java
@@ -1,62 +1,62 @@
 package com.ctrip.xpipe.redis.keeper.simple.load;
 
-
 import java.net.InetSocketAddress;
 
 import com.ctrip.xpipe.redis.keeper.simple.AbstractLoadRedis;
 
 import redis.clients.jedis.Jedis;
 
-
 /**
  * @author wenchao.meng
  *
- * May 23, 2016
+ *         May 23, 2016
  */
-public class SimpleSendMessage extends AbstractLoadRedis{
-	
+public class SimpleSendMessage extends AbstractLoadRedis {
+
 	private final int messageSize = 1 << 10;
-	
+
 	private String message;
-	
+
 	public SimpleSendMessage(InetSocketAddress master) {
 		super(master);
 		message = createMessage(messageSize);
 	}
 
-	
 	private String createMessage(int messageSize) {
-		
+
 		StringBuilder sb = new StringBuilder();
-		for(int i=0;i<messageSize;i++){
+		for (int i = 0; i < messageSize; i++) {
 			sb.append('c');
 		}
 		return sb.toString();
 	}
 
-
 	@Override
 	protected void doStart() throws Exception {
 		super.doStart();
-		
-		total = 100;
-		try(Jedis jedis = new Jedis(master.getHostName(), master.getPort())){
-			for(int i=0; i < total; i++){
-				jedis.set(String.valueOf(i), message);
+
+		try (Jedis jedis = new Jedis(master.getHostName(), master.getPort())) {
+			while (true) {
+				long index = increase();
+				if (index < 0) {
+					logger.info("[doStart][index < 0, break]{}", index);
+					break;
+				}
+				long keyIndex = getKeyIndex(index);
+				jedis.set(String.valueOf(keyIndex), message);
 			}
 		}
 		logger.info("[doStart][finish]");
 		System.exit(0);
-		
+
 	}
 
 	public static void main(String[] args) throws Exception {
-		
-		new SimpleSendMessage(
-				new InetSocketAddress(6379)
-				).start();
+
+		SimpleSendMessage simpleSendMessage = new SimpleSendMessage(new InetSocketAddress(6379));
+		simpleSendMessage.initialize();
+		simpleSendMessage.start();
 
 	}
-	
 
 }
