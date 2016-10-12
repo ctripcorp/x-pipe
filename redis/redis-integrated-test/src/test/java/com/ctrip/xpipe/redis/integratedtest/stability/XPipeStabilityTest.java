3e252e6ed25c0372d9dfcaae94772c6f38d01cee
shyin <tjuyinsh@gmail.com>
2016-10-12 18:51:26 +0800

    Refined stability test log metric

--- redis/redis-integrated-test/src/test/java/com/ctrip/xpipe/redis/integratedtest/stability/XPipeStabilityTest.java
+++ redis/redis-integrated-test/src/test/java/com/ctrip/xpipe/redis/integratedtest/stability/XPipeStabilityTest.java
@@ -14,6 +14,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
 import com.ctrip.xpipe.AbstractTest;
 import com.dianping.cat.Cat;
 
@@ -34,15 +35,18 @@ public class XPipeStabilityTest extends AbstractTest {
 	private ExecutorService expireCheckThreadPool;
 	private ExecutorService valueCheckThreadPool;
 	private ScheduledExecutorService qpsCheckThreadPool;
+	private ExecutorService catLogMetricThreadPool;
 	private JedisPool masterPool;
 	private JedisPool slavePool;
 
 	private ConcurrentHashMap<String, String> records = new ConcurrentHashMap<>(20000);
 	private ConcurrentLinkedQueue<Pair<String, String>> valueCheckQueue = new ConcurrentLinkedQueue<>();
 
-	private AtomicLong queryCount = new AtomicLong(0);
-	private Long historyQueryCount = new Long(0);
+	private AtomicLong queryCnt = new AtomicLong(0);
+	private Long historyQueryCnt = new Long(0);
 	private AtomicLong globalCnt = new AtomicLong(0);
+	private AtomicInteger catIntervalCnt = new AtomicInteger(0);
+	private AtomicLong catIntervalTotalDelay = new AtomicLong(0);
 
 	public static int MILLIS_TO_NANOTIME = 1000000;
 	public static int SECONDS_TO_MILLIS = 1000;
@@ -51,11 +55,13 @@ public class XPipeStabilityTest extends AbstractTest {
 	public int KEY_EXPIRE_SECONDS = Integer.parseInt(System.getProperty("key-expire-seconds", "500"));
 	public int QPS_COUNT_INTERVAL = Integer.parseInt(System.getProperty("qps-count-interval", "10"));
 	private int producerThreadNum = Integer.parseInt(System.getProperty("thread", "4"));
-	private int msgSize = Integer.parseInt(System.getProperty("msgSize", "100"));
+	private int msgSize = Integer.parseInt(System.getProperty("msg-size", "100"));
+	private int catIntervalSize = Integer.parseInt(System.getProperty("cat-interval-size", "100"));
+
 	private String masterAddress = System.getProperty("master", "127.0.0.1");
 	private int masterPort = Integer.parseInt(System.getProperty("master-port", "6379"));
 	private String slaveAddress = System.getProperty("slave", "127.0.0.1");
-	private int slavePort = Integer.parseInt(System.getProperty("master-port", "6379"));
+	private int slavePort = Integer.parseInt(System.getProperty("slave-port", "6379"));
 
 	@Before
 	public void setUp() {
@@ -66,6 +72,7 @@ public class XPipeStabilityTest extends AbstractTest {
 		expireCheckThreadPool = Executors.newFixedThreadPool(1);
 		valueCheckThreadPool = Executors.newFixedThreadPool(producerThreadNum * 3);
 		qpsCheckThreadPool = Executors.newScheduledThreadPool(1);
+		catLogMetricThreadPool = Executors.newFixedThreadPool(1);
 
 		masterPool = getJedisPool(masterAddress, masterPort, producerThreadNum * 2, 1);
 		slavePool = getJedisPool(slaveAddress, slavePort, producerThreadNum * 4, 1);
@@ -77,6 +84,8 @@ public class XPipeStabilityTest extends AbstractTest {
 		consumerThreadPool.shutdownNow();
 		expireCheckThreadPool.shutdownNow();
 		valueCheckThreadPool.shutdownNow();
+		qpsCheckThreadPool.shutdownNow();
+		catLogMetricThreadPool.shutdownNow();
 
 		masterPool.destroy();
 		slavePool.destroy();
@@ -87,7 +96,8 @@ public class XPipeStabilityTest extends AbstractTest {
 		startConsumerJob();
 		startProducerJob();
 		startQpsCheckJob();
-
+		startCatLogMetricJob();
+		
 		startExpireCheckJob();
 		startValueCheckJob();
 
@@ -114,7 +124,7 @@ public class XPipeStabilityTest extends AbstractTest {
 								master.setex(key, KEY_EXPIRE_SECONDS, value);
 								logger.debug("[startProducerJob][run]Insert: <{}>", key);
 
-								queryCount.incrementAndGet();
+								queryCnt.incrementAndGet();
 							} catch (JedisConnectionException e) {
 								logger.error("[startProducerJob][run]JedisConnectionException : {}", e);
 								records.remove(key);
@@ -175,20 +185,40 @@ public class XPipeStabilityTest extends AbstractTest {
 			records.remove(key);
 			logger.debug("[XPipeStabilityTestJedisPubSub][onPMessage]Remove: <{}>", key);
 
-			String nanotime_key = key.substring(key.indexOf("-") + 1);
-			long delay = System.nanoTime() - Long.valueOf(nanotime_key);
-			Cat.logMetricForSum("xpipe.redis.delay", delay);
+			catIntervalCnt.incrementAndGet();
+			long delay = System.nanoTime() - Long.valueOf(key.substring(key.indexOf("-") + 1));
+			catIntervalTotalDelay.set(catIntervalTotalDelay.get() + delay);
 		}
+	}
+
+	private void startCatLogMetricJob() {
+		catLogMetricThreadPool.submit(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					while (true) {
+						if (catIntervalCnt.get() == catIntervalSize) {
+							Cat.logMetricForSum("xpipe.redis.delay", catIntervalTotalDelay.get() / catIntervalSize);
+							catIntervalTotalDelay.set(0);
+							catIntervalCnt.set(0);
+						}
+						sleep(5);
+					}
+				} catch (Exception e) {
+					logger.error("[startCatLogMetricJob][run]Unexpected Exception : {}", e);
+				}
+			}
+		});
 	}
 
 	private void startQpsCheckJob() {
 		qpsCheckThreadPool.scheduleAtFixedRate(new Runnable() {
 			@Override
 			public void run() {
-				logger.info("[startQpsCheckJob][run]QPS : {}", (queryCount.get() - historyQueryCount) / QPS_COUNT_INTERVAL);
+				logger.info("[startQpsCheckJob][run]QPS : {}", (queryCnt.get() - historyQueryCnt) / QPS_COUNT_INTERVAL);
 				logger.info("[startQpsCheckJob][run]MapSize : {}", records.size());
 				logger.info("[startQpsCheckJob][run]QueueSize : {}", valueCheckQueue.size());
-				historyQueryCount = queryCount.get();
+				historyQueryCnt = queryCnt.get();
 			}
 		}, 1, QPS_COUNT_INTERVAL, TimeUnit.SECONDS);
 	}
