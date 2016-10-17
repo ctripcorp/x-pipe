1c6c52cb59a4899fddfcf6394436acbc462cfd1e
wenchaomeng <oytmfc@gmail.com>
2016-10-17 18:00:31 +0800

    reduce log

--- redis/redis-keeper/src/test/java/com/ctrip/xpipe/redis/keeper/store/DefaultReplicationStoreManagerTest.java
+++ redis/redis-keeper/src/test/java/com/ctrip/xpipe/redis/keeper/store/DefaultReplicationStoreManagerTest.java
@@ -35,7 +35,7 @@ public class DefaultReplicationStoreManagerTest extends AbstractRedisKeeperTest{
 		
 		final DefaultReplicationStoreManager mgr = (DefaultReplicationStoreManager) createReplicationStoreManager();
 		
-		for(int i=0;i<1000;i++){
+		for(int i=0;i<10;i++){
 			
 			logger.info("[testGc]{}", i);
 			
