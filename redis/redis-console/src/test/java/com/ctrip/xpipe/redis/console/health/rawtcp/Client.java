02ebef7fe93253f71814dfe4fb03f37273e6059e
marsqing <q_gu@ctrip.com>
2016-12-09 16:24:53 +0800

    add raw tcp and netty latency test

---
+++ redis/redis-console/src/test/java/com/ctrip/xpipe/redis/console/health/rawtcp/Client.java
@@ -0,0 +1,41 @@
+package com.ctrip.xpipe.redis.console.health.rawtcp;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+
+/**
+ * @author marsqing
+ *
+ *         Dec 9, 2016 4:02:23 PM
+ */
+public class Client {
+	public static void main(String[] args) throws Exception {
+		Socket s = new Socket("127.0.0.1", 7777);
+		s.setTcpNoDelay(true);
+
+		OutputStream out = s.getOutputStream();
+		InputStream in = s.getInputStream();
+		byte[] buf = new byte[8];
+		while (true) {
+			out.write(longToBytes(System.nanoTime()));
+			in.read(buf);
+			System.out.println((System.nanoTime() - bytesToLong(buf)) / 1000);
+			Thread.sleep(1000);
+		}
+	}
+
+	public static byte[] longToBytes(long x) {
+		ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+		buffer.putLong(x);
+		return buffer.array();
+	}
+
+	public static long bytesToLong(byte[] bytes) {
+		ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+		buffer.put(bytes);
+		buffer.flip();// need flip
+		return buffer.getLong();
+	}
+}
