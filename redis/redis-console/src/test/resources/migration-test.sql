a17e145e849cf1690af77c35bf15bdfc37a31339
shyin <tjuyinsh@gmail.com>
2016-12-28 18:53:28 +0800

    Different active dc while migrating in DcMeta

--- redis/redis-console/src/test/resources/migration-test.sql
+++ redis/redis-console/src/test/resources/migration-test.sql
@@ -1,18 +1,20 @@
-insert into CLUSTER_TBL (cluster_name,activedc_id,cluster_description,cluster_last_modified_time,status) values ('cluster1',1,'Cluster:cluster1 , ActiveDC : A','0000000000000000','Normal');
-insert into CLUSTER_TBL (cluster_name,activedc_id,cluster_description,cluster_last_modified_time,status) values ('cluster2',1,'Cluster:cluster2 , ActiveDC : A','0000000000000000','Migrating');
+insert into CLUSTER_TBL (id,cluster_name,activedc_id,cluster_description,cluster_last_modified_time,status,is_xpipe_interested) values (1,'cluster1',1,'Cluster:cluster1 , ActiveDC : A','0000000000000000','Normal',1);
+insert into CLUSTER_TBL (id,cluster_name,activedc_id,cluster_description,cluster_last_modified_time,status,is_xpipe_interested) values (2,'cluster2',1,'Cluster:cluster2 , ActiveDC : A','0000000000000000','Migrating',1);
 
-insert into DC_CLUSTER_TBL (dc_id,cluster_id,dc_cluster_phase,metaserver_id) values (1,1,1,0);
-insert into DC_CLUSTER_TBL (dc_id,cluster_id,dc_cluster_phase,metaserver_id) values (2,1,1,0);
-insert into DC_CLUSTER_TBL (dc_id,cluster_id,dc_cluster_phase,metaserver_id) values (1,2,1,0);
-insert into DC_CLUSTER_TBL (dc_id,cluster_id,dc_cluster_phase,metaserver_id) values (2,2,1,0);
+insert into DC_CLUSTER_TBL (dc_cluster_id,dc_id,cluster_id,dc_cluster_phase,metaserver_id) values (1,1,1,1,0);
+insert into DC_CLUSTER_TBL (dc_cluster_id,dc_id,cluster_id,dc_cluster_phase,metaserver_id) values (2,2,1,1,0);
+insert into DC_CLUSTER_TBL (dc_cluster_id,dc_id,cluster_id,dc_cluster_phase,metaserver_id) values (3,1,2,1,0);
+insert into DC_CLUSTER_TBL (dc_cluster_id,dc_id,cluster_id,dc_cluster_phase,metaserver_id) values (4,2,2,1,0);
 
-insert into SHARD_TBL (shard_name,cluster_id) values('shard1',1);
-insert into SHARD_TBL (shard_name,cluster_id) values('shard2',1);
+insert into SHARD_TBL (id,shard_name,cluster_id) values(1,'shard1',1);
+insert into SHARD_TBL (id,shard_name,cluster_id) values(2,'shard2',1);
 insert into SHARD_TBL (id,shard_name,cluster_id) values(3,'shard1',2);
 insert into SHARD_TBL (id,shard_name,cluster_id) values(4,'shard2',2);
 
-insert into DC_CLUSTER_SHARD_TBL (dc_cluster_id,shard_id,setinel_id,dc_cluster_shard_phase) values (1,1,1,1);
-insert into DC_CLUSTER_SHARD_TBL (dc_cluster_id,shard_id,setinel_id,dc_cluster_shard_phase) values (2,1,2,1);
+insert into DC_CLUSTER_SHARD_TBL (dc_cluster_shard_id,dc_cluster_id,shard_id,setinel_id,dc_cluster_shard_phase) values (1,1,1,1,1);
+insert into DC_CLUSTER_SHARD_TBL (dc_cluster_shard_id,dc_cluster_id,shard_id,setinel_id,dc_cluster_shard_phase) values (2,2,1,2,1);
+insert into DC_CLUSTER_SHARD_TBL (dc_cluster_shard_id,dc_cluster_id,shard_id,setinel_id,dc_cluster_shard_phase) values (3,3,3,1,1);
+insert into DC_CLUSTER_SHARD_TBL (dc_cluster_shard_id,dc_cluster_id,shard_id,setinel_id,dc_cluster_shard_phase) values (4,4,3,2,1);
 
 insert into REDIS_TBL (run_id,dc_cluster_shard_id,redis_ip,redis_port,redis_role,master,redis_master,keepercontainer_id) values('ffffffffffffffffffffffffffffffffffffffff',1,'127.0.0.1',6000,'keeper',0,-1,1);
 insert into REDIS_TBL (run_id,dc_cluster_shard_id,redis_ip,redis_port,redis_role,master,redis_master,keepercontainer_id) values('ffffffffffffffffffffffffffffffffffffffff',1,'127.0.0.1',6000,'keeper',0,-1,2);
@@ -24,6 +26,17 @@ insert into REDIS_TBL (run_id,dc_cluster_shard_id,redis_ip,redis_port,redis_role
 insert into REDIS_TBL (run_id,dc_cluster_shard_id,redis_ip,redis_port,redis_role,master,redis_master,keepercontainer_id) values('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',2,'127.0.0.1',6000,'keeper',0,-1,5);
 insert into REDIS_TBL (run_id,dc_cluster_shard_id,redis_ip,redis_port,redis_role,master,redis_master,keepercontainer_id) values('unknown',2,'127.0.0.1',6379,'redis',0,-1,null);
 insert into REDIS_TBL (run_id,dc_cluster_shard_id,redis_ip,redis_port,redis_role,master,redis_master,keepercontainer_id) values('unknown',2,'127.0.0.1',6379,'redis',0,-1,null);
+
+insert into REDIS_TBL (run_id,dc_cluster_shard_id,redis_ip,redis_port,redis_role,master,redis_master,keepercontainer_id) values('bfffffffffffffffffffffffffffffffffffffff',3,'127.0.0.1',6000,'keeper',0,-1,1);
+insert into REDIS_TBL (run_id,dc_cluster_shard_id,redis_ip,redis_port,redis_role,master,redis_master,keepercontainer_id) values('bfffffffffffffffffffffffffffffffffffffff',3,'127.0.0.1',6000,'keeper',0,-1,2);
+insert into REDIS_TBL (run_id,dc_cluster_shard_id,redis_ip,redis_port,redis_role,master,redis_master,keepercontainer_id) values('unknown',3,'127.0.0.1',6379,'redis',1,0,null);
+insert into REDIS_TBL (run_id,dc_cluster_shard_id,redis_ip,redis_port,redis_role,master,redis_master,keepercontainer_id) values('unknown',3,'127.0.0.1',6379,'redis',0,-1,null);
+insert into REDIS_TBL (run_id,dc_cluster_shard_id,redis_ip,redis_port,redis_role,master,redis_master,keepercontainer_id) values('unknown',3,'127.0.0.1',6379,'redis',0,-1,null);
+
+insert into REDIS_TBL (run_id,dc_cluster_shard_id,redis_ip,redis_port,redis_role,master,redis_master,keepercontainer_id) values('beeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',4,'127.0.0.1',6000,'keeper',0,-1,4);
+insert into REDIS_TBL (run_id,dc_cluster_shard_id,redis_ip,redis_port,redis_role,master,redis_master,keepercontainer_id) values('beeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',4,'127.0.0.1',6000,'keeper',0,-1,5);
+insert into REDIS_TBL (run_id,dc_cluster_shard_id,redis_ip,redis_port,redis_role,master,redis_master,keepercontainer_id) values('unknown',4,'127.0.0.1',6379,'redis',0,-1,null);
+insert into REDIS_TBL (run_id,dc_cluster_shard_id,redis_ip,redis_port,redis_role,master,redis_master,keepercontainer_id) values('unknown',4,'127.0.0.1',6379,'redis',0,-1,null);
 
 insert into MIGRATION_EVENT_TBL (id,event_tag) values (2,'xpipe-test-event-2');
 
