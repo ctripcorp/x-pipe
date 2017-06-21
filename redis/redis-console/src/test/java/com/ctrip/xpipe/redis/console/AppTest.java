7add6b9ada00936aced88f695e5b847380fce855
wenchaomeng <oytmfc@gmail.com>
2017-06-21 17:14:32 +0800

    find keeper container without keepers

--- redis/redis-console/src/test/java/com/ctrip/xpipe/redis/console/AppTest.java
+++ redis/redis-console/src/test/java/com/ctrip/xpipe/redis/console/AppTest.java
@@ -26,7 +26,7 @@ public class AppTest extends AbstractConsoleH2DbTest {
 	public void startUp() {
 
 		System.setProperty(AbstractProfile.PROFILE_KEY, AbstractProfile.PROFILE_NAME_TEST);
-		System.setProperty(HealthChecker.ENABLED, "true");
+		System.setProperty(HealthChecker.ENABLED, "false");
 		System.setProperty(CatConfig.CAT_ENABLED_KEY, "false");
 	}
 
