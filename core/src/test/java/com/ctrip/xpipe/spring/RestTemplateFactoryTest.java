dabd29b24eaad2dc31700db57eeea9ec1b194ba3
wenchaomeng <oytmfc@gmail.com>
2017-09-20 15:07:50 +0800

    retry when io error or 402

--- core/src/test/java/com/ctrip/xpipe/spring/RestTemplateFactoryTest.java
+++ core/src/test/java/com/ctrip/xpipe/spring/RestTemplateFactoryTest.java
@@ -4,6 +4,7 @@ import com.ctrip.xpipe.AbstractTest;
 import com.ctrip.xpipe.codec.Person;
 import com.ctrip.xpipe.testutils.SpringApplicationStarter;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestOperations;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author wenchao.meng
@@ -22,21 +26,61 @@ public class RestTemplateFactoryTest extends AbstractTest {
     private RestOperations restOperations;
     private SpringApplicationStarter springApplicationStarter;
     private int port = 8080;
+    private int retryTimes = 2;
+    private static int countInvoke = 0;
 
     @Before
     public void beforeRestTemplateFactoryTest() throws Exception {
 
-        restOperations = RestTemplateFactory.createCommonsHttpRestTemplate(2, 1000, 1000, 5000);
+        restOperations = RestTemplateFactory.createCommonsHttpRestTemplate(2
+                , 1000
+                , 1000
+                , 5000
+                , retryTimes);
 
         port = randomPort();
         springApplicationStarter = new SpringApplicationStarter(TestServer.class, port);
         springApplicationStarter.start();
     }
 
+    @Test
+    public void testRetry502(){
 
-    @After
-    public void afterRestTemplateFactoryTest() throws Exception {
-        springApplicationStarter.stop();
+        int beginCount =  countInvoke;
+        try{
+            Person person = restOperations.getForObject("http://localhost:" + port + "/502", Person.class);
+        }catch (Exception e){
+            logger.error("[testRetry502]", e);
+        }
+        int endCount =  countInvoke;
+        Assert.assertEquals(retryTimes + 1, endCount - beginCount);
+    }
+
+    @Test
+    public void testNoRetry505(){
+
+        int beginCount =  countInvoke;
+        try{
+            Person person = restOperations.getForObject("http://localhost:" + port + "/505", Person.class);
+        }catch (Exception e){
+            logger.error("[testRetry502]", e);
+        }
+        int endCount =  countInvoke;
+        Assert.assertEquals(1, endCount - beginCount);
+    }
+
+    @Test
+    public void testNotry404(){
+
+        int beginCount =  countInvoke;
+        try{
+            Person person = restOperations.getForObject("http://localhost:" + port + "/404", Person.class);
+        }catch (Exception e){
+            logger.error("[testRetry404]", e);
+        }
+        int endCount =  countInvoke;
+        Assert.assertEquals(1, endCount - beginCount);
+
     }
 
 
@@ -58,6 +102,32 @@ public class RestTemplateFactoryTest extends AbstractTest {
         public String person() {
             return "{\"sex\":\"FEMALE\",\"age\":1010, \"other\":11}";
         }
+
+        @RequestMapping("/502")
+        @ResponseBody
+        public void person502(HttpServletRequest request, HttpServletResponse response){
+            countInvoke++;
+            response.setStatus(HttpServletResponse.SC_BAD_GATEWAY);
+        }
+
+        @RequestMapping("/505")
+        @ResponseBody
+        public void person505(HttpServletRequest request, HttpServletResponse response){
+            countInvoke++;
+            response.setStatus(HttpServletResponse.SC_HTTP_VERSION_NOT_SUPPORTED);
+        }
+
+        @RequestMapping("/404")
+        @ResponseBody
+        public void person404(HttpServletRequest request, HttpServletResponse response){
+            countInvoke++;
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+
     }
 
+    @After
+    public void afterRestTemplateFactoryTest() throws Exception {
+        springApplicationStarter.stop();
+    }
 }
