d6f98322bcba8392b837ed291ac18e97946ab619
NickNYU <cz739@nyu.edu>
2017-11-16 17:55:01 +0800

    urgent fix

--- redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/build/ComponentsConfigurator.java
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/build/ComponentsConfigurator.java
@@ -1,6 +1,5 @@
 package com.ctrip.xpipe.redis.console.build;
 
-import com.ctrip.xpipe.redis.console.dal.XpipeDalDataObjectAssembly;
 import com.ctrip.xpipe.redis.console.dal.XpipeDalTransactionManager;
 import com.ctrip.xpipe.redis.console.ds.XpipeDataSourceProvider;
 import org.unidal.dal.jdbc.datasource.DataSourceProvider;
@@ -29,7 +28,6 @@ public class ComponentsConfigurator extends AbstractResourceConfigurator {
                 .config(E("datasourceFile").value("datasources.xml"),
                         E("baseDirRef").value(KEY_XPIPE_LOCATION)));
         all.add(A(XpipeDalTransactionManager.class));
-        all.add(A(XpipeDalDataObjectAssembly.class));
         return all;
     }
 
