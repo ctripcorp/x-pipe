7b370c462fa7bb54dfeceb1795233fc6bed190b1
wenchao.meng <oytmfc@gmail.com>
2018-01-03 15:13:32 +0800

    simple bug fix

--- redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/resources/DefaultMetaCache.java
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/resources/DefaultMetaCache.java
@@ -107,7 +107,7 @@ public class DefaultMetaCache implements MetaCache {
 
     @Override
     public XpipeMeta getXpipeMeta() {
-        return meta.getKey();
+        return meta == null ? null : meta.getKey();
     }
 
 
