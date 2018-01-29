bdee9bce27e2d1b68b3baa25cdd9de1549e22f03
wenchao.meng <oytmfc@gmail.com>
2018-01-29 17:34:26 +0800

    tcp port check async command

---
+++ core/src/main/java/com/ctrip/xpipe/netty/TcpPortCheckCommand.java
@@ -0,0 +1,83 @@
+package com.ctrip.xpipe.netty;
+
+import com.ctrip.xpipe.command.AbstractCommand;
+import com.ctrip.xpipe.utils.XpipeThreadFactory;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author wenchao.meng
+ *         <p>
+ *         Jan 26, 2018
+ */
+public class TcpPortCheckCommand extends AbstractCommand<Boolean> {
+
+    private static final int CHECK_TIMEOUT_MILLI = 10000;
+    private String host;
+    private int port;
+    private static Logger logger = LoggerFactory.getLogger(TcpPortCheckCommand.class);
+    private static NioEventLoopGroup nioEventLoopGroup = new NioEventLoopGroup(1, XpipeThreadFactory.create("tcp-port-check", true));
+    private Bootstrap b = new Bootstrap();
+
+    public TcpPortCheckCommand(String host, int port) {
+        this.host = host;
+        this.port = port;
+        this.b.group(nioEventLoopGroup);
+        this.b.option(ChannelOption.ALLOCATOR, UnpooledByteBufAllocator.DEFAULT);
+        this.b.channel(NioSocketChannel.class);
+        this.b.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, CHECK_TIMEOUT_MILLI);
+
+        this.b.handler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel ch) throws Exception {
+                ChannelPipeline pipeline = ch.pipeline();
+                pipeline.addLast(new LoggingHandler(LogLevel.DEBUG));
+                pipeline.addLast(new ChannelDuplexHandler() {
+                    @Override
+                    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                        ctx.channel().close();
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public String getName() {
+        return "[TcpPortCheckCommand]";
+    }
+
+    @Override
+    protected void doExecute() throws Exception {
+
+        ChannelFuture connect = b.connect(new InetSocketAddress(host, port));
+        connect.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) throws Exception {
+
+                if (future.isSuccess()) {
+                    future().setSuccess(true);
+                } else {
+                    logger.warn("[doExecute]", future.cause());
+                    future().setFailure(future.cause());
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void doReset() {
+
+    }
+}
---
+++ core/src/test/java/com/ctrip/xpipe/netty/TcpPortCheckCommandTest.java
@@ -0,0 +1,84 @@
+package com.ctrip.xpipe.netty;
+
+import com.ctrip.xpipe.AbstractTest;
+import com.ctrip.xpipe.api.command.CommandFuture;
+import com.ctrip.xpipe.simpleserver.Server;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author wenchao.meng
+ *         <p>
+ *         Jan 26, 2018
+ */
+public class TcpPortCheckCommandTest extends AbstractTest {
+
+    private int count = 10;
+
+    @Test
+    public void testOk() throws Exception {
+
+        Server server = startEchoServer();
+
+        for (int i = 0; i < count; i++) {
+            TcpPortCheckCommand checkCommand = new TcpPortCheckCommand("localhost", server.getPort());
+            CommandFuture<Boolean> future = checkCommand.execute();
+            Assert.assertTrue(future.get(500, TimeUnit.SECONDS));
+        }
+
+
+    }
+
+    @Test
+    public void testFail() throws InterruptedException, ExecutionException, TimeoutException {
+
+        int port = randomPort();
+
+        for (int i = 0; i < count; i++) {
+            TcpPortCheckCommand checkCommand = new TcpPortCheckCommand("localhost", port);
+            CommandFuture<Boolean> future = checkCommand.execute();
+            try {
+                future.get(500, TimeUnit.SECONDS);
+                Assert.fail();
+            } catch (Exception e) {
+            }
+        }
+
+    }
+
+//    @Test
+    public void testResource() throws Exception {
+
+        Server server = startEchoServer();
+        int i = 0;
+        try {
+            for (; i < (1 << 10); i++) {
+                TcpPortCheckCommand checkCommand = new TcpPortCheckCommand("localhost", server.getPort());
+                CommandFuture<Boolean> future = checkCommand.execute();
+                Assert.assertTrue(future.get(500, TimeUnit.SECONDS));
+            }
+        } catch (Exception e) {
+            logger.error("[testResource]" + i, e);
+        }
+
+
+    }
+
+    //    @Test
+    public void testTimeout() throws IOException {
+
+        TcpPortCheckCommand checkCommand = new TcpPortCheckCommand("10.2.24.217", 2181);
+
+        logger.info("[begin]");
+        checkCommand.execute();
+        logger.info("[end]");
+
+        waitForAnyKey();
+    }
+
+}
