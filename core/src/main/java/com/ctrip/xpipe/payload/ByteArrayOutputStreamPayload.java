9c49cf3e607dfa4e363e12a7fca0a0c6584a9446
NickNYU <cz739@nyu.edu>
2018-03-05 11:27:24 +0800

    fix bug will lead IndexOutOfBound Exception

--- core/src/main/java/com/ctrip/xpipe/payload/ByteArrayOutputStreamPayload.java
+++ core/src/main/java/com/ctrip/xpipe/payload/ByteArrayOutputStreamPayload.java
@@ -50,11 +50,17 @@ public class ByteArrayOutputStreamPayload extends AbstractInOutPayload{
 	private void makeSureSize(int size) {
 		
 		if(pos.get() + size > data.length){
-			byte []newData = new byte[data.length * 2];
+			byte []newData = new byte[ensureSize(size)];
 			System.arraycopy(data, 0, newData, 0, data.length);
 			data = newData;
 		}
 	}
+
+	private int ensureSize(int size) {
+		int predictSize = data.length * 2, requireSize = size + pos.get();
+		return predictSize > requireSize ? predictSize : requireSize;
+	}
+
 	@Override
 	public long doOut(WritableByteChannel writableByteChannel) throws IOException {
 		
