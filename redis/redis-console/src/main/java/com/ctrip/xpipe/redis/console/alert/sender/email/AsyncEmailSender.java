5c77278c7ed10b498f783f0f343770feb5b71468
NickNYU <cz739@nyu.edu>
2018-04-24 13:55:46 +0800

    modify email service

--- redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/alert/sender/email/AsyncEmailSender.java
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/alert/sender/email/AsyncEmailSender.java
@@ -1,20 +1,27 @@
 package com.ctrip.xpipe.redis.console.alert.sender.email;
 
+import com.ctrip.xpipe.api.codec.Codec;
 import com.ctrip.xpipe.api.command.CommandFuture;
 import com.ctrip.xpipe.api.command.CommandFutureListener;
+import com.ctrip.xpipe.api.email.EmailResponse;
 import com.ctrip.xpipe.api.email.EmailService;
+import com.ctrip.xpipe.api.foundation.FoundationService;
 import com.ctrip.xpipe.redis.console.alert.AlertMessageEntity;
 import com.ctrip.xpipe.redis.console.alert.sender.AbstractSender;
 import com.ctrip.xpipe.redis.console.alert.sender.email.listener.AsyncEmailSenderCallback;
 import com.ctrip.xpipe.redis.console.alert.sender.email.listener.CompositeEmailSenderCallback;
 import com.ctrip.xpipe.redis.console.alert.sender.email.listener.EmailSendErrorReporter;
 import com.ctrip.xpipe.redis.console.alert.sender.email.listener.EmailSentCounter;
+import com.ctrip.xpipe.redis.console.model.EventModel;
+import com.ctrip.xpipe.redis.console.service.impl.AlertEventService;
 import com.ctrip.xpipe.utils.VisibleForTesting;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 
 import static com.ctrip.xpipe.spring.AbstractSpringConfigContext.GLOBAL_EXECUTOR;
@@ -35,6 +42,9 @@ public class AsyncEmailSender extends AbstractSender {
     @Resource(name = GLOBAL_EXECUTOR)
     private ExecutorService executor;
 
+    @Autowired
+    private AlertEventService alertEventService;
+
     @Override
     public String getId() {
         return ID;
@@ -50,22 +60,25 @@ public class AsyncEmailSender extends AbstractSender {
 
     @Override
     public boolean send(AlertMessageEntity message) {
-        CommandFuture<Void> future = EmailService.DEFAULT.sendEmailAsync(createEmail(message), executor);
-        future.addListener(new CommandFutureListener<Void>() {
-            @Override
-            public void operationComplete(CommandFuture<Void> commandFuture) throws Exception {
-                if(commandFuture.isSuccess()) {
-                    callbackFunction.success();
-                } else {
-                    callbackFunction.fail(commandFuture.cause());
-                }
-
-            }
+        CommandFuture<EmailResponse> future = EmailService.DEFAULT.sendEmailAsync(createEmail(message), executor);
+        future.addListener(commandFuture -> {
+            EmailResponse response = commandFuture.getNow();
+            alertEventService.insert(createEventModel(message, response));
         });
         if(future.isDone() && !future.isSuccess()) {
             return false;
         }
         return true;
+    }
+
+    @VisibleForTesting
+    @SuppressWarnings("unchecked")
+    protected EventModel createEventModel(AlertMessageEntity message, EmailResponse response) {
+        EventModel model = new EventModel();
+        model.setEventType(EventModel.EventType.ALERT_EMAIL).setEventOperator(FoundationService.DEFAULT.getLocalIp())
+                .setEventOperation(message.getAlert().getAlertType().name()).setEventDetail(message.getTitle())
+                .setEventProperty(Codec.DEFAULT.encode(response));
+        return model;
     }
 
     @VisibleForTesting
