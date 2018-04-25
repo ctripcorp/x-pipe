f5bf1b5b00c104b35567cf5570f75860207f5654
NickNYU <cz739@nyu.edu>
2018-04-25 14:47:36 +0800

    scheduled alert velocity might cause nullpointer exception by concurrenthashmap

--- redis/redis-console/src/test/java/com/ctrip/xpipe/redis/console/alert/message/subscriber/RepeatAlertEntitySubscriberManualTest.java
+++ redis/redis-console/src/test/java/com/ctrip/xpipe/redis/console/alert/message/subscriber/RepeatAlertEntitySubscriberManualTest.java
@@ -6,6 +6,7 @@ import com.ctrip.xpipe.redis.console.alert.ALERT_TYPE;
 import com.ctrip.xpipe.redis.console.alert.AlertEntity;
 import com.ctrip.xpipe.redis.console.alert.manager.AlertPolicyManager;
 import com.ctrip.xpipe.redis.console.model.ConfigModel;
+import com.ctrip.xpipe.redis.console.service.ConfigService;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,9 @@ public class RepeatAlertEntitySubscriberManualTest extends AbstractConsoleIntegr
     @Autowired
     private AlertPolicyManager policyManager;
 
+    @Autowired
+    private ConfigService configService;
+
     private AlertEntity alert;
 
     @Before
@@ -48,17 +52,30 @@ public class RepeatAlertEntitySubscriberManualTest extends AbstractConsoleIntegr
     }
 
     @Test
-    public void testScheduledReport() throws InterruptedException {
+    public void testScheduledReport() throws InterruptedException, DalException {
         subscriber.processData(alert);
         subscriber.processData(alert);
         subscriber.processData(alert);
         subscriber.processData(alert);
+        AlertEntity alert2 = new AlertEntity(new HostPort("192.168.2.10", 6390), dcNames[0],
+                "clusterId2", "shardId2", "test message2", ALERT_TYPE.CLIENT_INSTANCE_NOT_OK);
+        AlertEntity alert3 = new AlertEntity(new HostPort("192.168.3.10", 6390), dcNames[0],
+                "clusterId3", "shardId3", "test message3", ALERT_TYPE.CLIENT_INSTANCE_NOT_OK);
+        AlertEntity alert4 = new AlertEntity(new HostPort("192.168.4.10", 6390), dcNames[0],
+                "clusterId4", "shardId4", "test message4", ALERT_TYPE.CLIENT_INSTANCE_NOT_OK);
+        subscriber.processData(alert2);
+        subscriber.processData(alert3);
+        subscriber.processData(alert4);
+
+        configService.stopAlertSystem(new ConfigModel(), 1);
+
         subscriber.scheduledReport();
         Thread.sleep(2000);
     }
 
     @Test
     public void doProcessAlert() {
+        AlertEntity alertEntity = new AlertEntity(new HostPort(), dcNames[0], "cluster", "shard", "instance is not valid", ALERT_TYPE.CLIENT_INSTANCE_NOT_OK);
         subscriber.processData(alert);
     }
 
