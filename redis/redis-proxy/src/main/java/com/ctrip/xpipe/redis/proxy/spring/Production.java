75e87eb63fd2aed6b1c0d3ba4cfff520467b8725
NickNYU <cz739@nyu.edu>
2018-06-07 17:32:40 +0800

    when endpoint unhealthy more than one time, remove it from all endpoint. in case all endpoints grows too big

--- redis/redis-proxy/src/main/java/com/ctrip/xpipe/redis/proxy/spring/Production.java
+++ redis/redis-proxy/src/main/java/com/ctrip/xpipe/redis/proxy/spring/Production.java
@@ -11,12 +11,17 @@ import com.ctrip.xpipe.redis.proxy.monitor.impl.DebugTunnelMonitorManager;
 import com.ctrip.xpipe.spring.AbstractProfile;
 import com.ctrip.xpipe.utils.OsUtils;
 import com.ctrip.xpipe.utils.XpipeThreadFactory;
+import com.google.common.util.concurrent.MoreExecutors;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author chen.zhu
@@ -37,6 +42,8 @@ public class Production extends AbstractProfile {
     public static final String BACKEND_EVENTLOOP_GROUP = "backendEventLoopGroup";
 
     public static final String TUNNEL_MONITOR_MANAGER = "globalTunnelMonitorManager";
+
+    public static final String GLOBAL_SCHEDULED = "globalScheduled";
 
     @Autowired
     private ProxyConfig config;
@@ -64,5 +71,14 @@ public class Production extends AbstractProfile {
     @Bean(name = TUNNEL_MONITOR_MANAGER)
     public TunnelMonitorManager tunnelMonitorManager () {
         return new DebugTunnelMonitorManager();
+    }
+
+    @Bean(name = GLOBAL_SCHEDULED)
+    public ScheduledExecutorService getScheduled() {
+        int corePoolSize = OsUtils.getCpuCount();
+        return MoreExecutors.getExitingScheduledExecutorService(
+                new ScheduledThreadPoolExecutor(corePoolSize, XpipeThreadFactory.create(GLOBAL_SCHEDULED)),
+                1, TimeUnit.SECONDS
+        );
     }
 }
