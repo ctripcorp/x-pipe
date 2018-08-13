ab791db0a2c02eca76a67c37c1e3beb191201889
NickNYU <cz739@nyu.edu>
2018-08-13 15:09:08 +0800

    Subscribe Command modification

--- redis/redis-core/src/main/java/com/ctrip/xpipe/redis/core/protocal/cmd/pubsub/SubscribeCommand.java
+++ redis/redis-core/src/main/java/com/ctrip/xpipe/redis/core/protocal/cmd/pubsub/SubscribeCommand.java
@@ -22,9 +22,9 @@ public class SubscribeCommand extends AbstractSubscribe {
 
     public SubscribeCommand(Endpoint endpoint, ScheduledExecutorService scheduled, String channel) {
         super(endpoint.getHost(), endpoint.getPort(), scheduled, channel, MESSAGE_TYPE.MESSAGE);
-        if(isProxyEnabled(endpoint)) {
-            setCommandTimeoutMilli(PROXYED_REDIS_CONNECTION_COMMAND_TIME_OUT_MILLI);
-        }
+//        if(isProxyEnabled(endpoint)) {
+//            setCommandTimeoutMilli(PROXYED_REDIS_CONNECTION_COMMAND_TIME_OUT_MILLI);
+//        }
     }
 
     public SubscribeCommand(SimpleObjectPool<NettyClient> clientPool, ScheduledExecutorService scheduled, String channel) {
