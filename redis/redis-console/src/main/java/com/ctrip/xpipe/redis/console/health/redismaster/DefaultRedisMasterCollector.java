4e14126241155c406699b251afe56ff246b2da90
tt.tu <tt.tu@Ctrip.com>
2018-08-14 17:24:59 +0800

    fix bug of redis master monitor

--- redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/health/redismaster/DefaultRedisMasterCollector.java
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/health/redismaster/DefaultRedisMasterCollector.java
@@ -58,7 +58,7 @@ public class DefaultRedisMasterCollector implements RedisMasterCollector{
             correct(plan);
         }else {
             Map.Entry<HostPort, InstanceRedisMasterResult> next = hostPort2SampleResult.entrySet().iterator().next();
-            if(next.getValue().roleIsSlave() || next.getValue().roleIsUnknow()){
+            if(!next.getValue().roleIsMaster()){
                 logger.info("[collect][role not right]{}, {}", plan, next);
                 correct(plan);
             }
