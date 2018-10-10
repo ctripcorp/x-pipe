f54a301473e9f320b5bd94f35d5da0587f9a20c8
vzc祝辰 <zhuchen@ctrip.com>
2018-10-10 12:08:55 +0800

    add alertTypes() for VersionHealthActionFactory

--- redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/healthcheck/redisconf/version/VersionCheckActionFactory.java
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/healthcheck/redisconf/version/VersionCheckActionFactory.java
@@ -1,11 +1,15 @@
 package com.ctrip.xpipe.redis.console.healthcheck.redisconf.version;
 
+import com.ctrip.xpipe.redis.console.alert.ALERT_TYPE;
 import com.ctrip.xpipe.redis.console.healthcheck.RedisHealthCheckInstance;
 import com.ctrip.xpipe.redis.console.healthcheck.redisconf.AbstractCDLAHealthCheckActionFactory;
 import com.ctrip.xpipe.redis.console.healthcheck.redisconf.CrossDcLeaderAwareHealthCheckAction;
 import com.ctrip.xpipe.redis.console.resources.MetaCache;
+import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * @author chen.zhu
@@ -26,5 +30,10 @@ public class VersionCheckActionFactory extends AbstractCDLAHealthCheckActionFact
     @Override
     public Class<? extends CrossDcLeaderAwareHealthCheckAction> support() {
         return VersionCheckAction.class;
+    }
+
+    @Override
+    protected List<ALERT_TYPE> alertTypes() {
+        return Lists.newArrayList(ALERT_TYPE.XREDIS_VERSION_NOT_VALID);
     }
 }
