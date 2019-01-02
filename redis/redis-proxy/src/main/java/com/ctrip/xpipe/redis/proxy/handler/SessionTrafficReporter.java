e118cc6f38e25e9fc68927c8161bd49a809c37da
NickNYU <cz739@nyu.edu>
2019-01-02 17:35:15 +0800

    add session stats to proxy

--- redis/redis-proxy/src/main/java/com/ctrip/xpipe/redis/proxy/handler/SessionTrafficReporter.java
+++ redis/redis-proxy/src/main/java/com/ctrip/xpipe/redis/proxy/handler/SessionTrafficReporter.java
@@ -36,14 +36,18 @@ public class SessionTrafficReporter extends ChannelTrafficStatisticsHandler {
     @Override
     protected void doChannelRead(ChannelHandlerContext ctx, Object msg) {
         if(msg instanceof ByteBuf) {
-//            session.getSessionMonitor().getSessionStats().increaseInputBytes(((ByteBuf) msg).readableBytes());
+            if(session != null && session.getSessionMonitor() != null) {
+                session.getSessionMonitor().getSessionStats().increaseInputBytes(((ByteBuf) msg).readableBytes());
+            }
         }
     }
 
     @Override
     protected void doWrite(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) {
         if(msg instanceof ByteBuf) {
-//            session.getSessionMonitor().getSessionStats().increaseOutputBytes(((ByteBuf) msg).readableBytes());
+            if(session != null && session.getSessionMonitor() != null) {
+                session.getSessionMonitor().getSessionStats().increaseOutputBytes(((ByteBuf) msg).readableBytes());
+            }
         }
     }
 
