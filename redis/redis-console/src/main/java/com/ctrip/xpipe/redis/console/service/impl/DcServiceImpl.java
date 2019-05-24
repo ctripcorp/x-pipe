3cd693a883a62603d7fcffc3dddc6c271835aa2b
tt.tu <tt.tu@ctrip.com>
2019-05-24 16:57:40 +0800

    normalize restful develop

--- redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/service/impl/DcServiceImpl.java
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/service/impl/DcServiceImpl.java
@@ -160,13 +160,13 @@ public class DcServiceImpl extends AbstractConsoleService<DcTblDao> implements D
 	}
 
 	@Override
-	public synchronized void insert(long zone_id, String dc_name, String description) {
+	public synchronized void insertWithPartField(long zone_id, String dc_name, String description) {
 		queryHandler.handleInsert(new DalQuery<Integer>() {
 			@Override
 			public Integer doQuery() throws DalException {
-				return dao.insert(dao.createLocal().setZoneId(zone_id)
+				return dao.insertWithPartField(dao.createLocal().setZoneId(zone_id)
 						.setDcName(dc_name).setDcDescription(description)
-				.setDcLastModifiedTime("0000000000000000"));
+				);
 			}
 		});
 	}
