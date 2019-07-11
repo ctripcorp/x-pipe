3648426fc31daa187ad098f702ef19122d81ba8c
NickNYU <cz739@nyu.edu>
2019-07-11 21:55:18 +0800

    upgrade fireman to 0.0.200

---
+++ redis/redis-console/src/test/java/com/ctrip/xpipe/redis/console/FastjsonTest.java
@@ -0,0 +1,97 @@
+package com.ctrip.xpipe.redis.console;
+
+import com.ctrip.xpipe.api.codec.Codec;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class FastjsonTest {
+
+    public static class Node {
+        private String key;
+
+        private String val;
+
+        private int anInt;
+
+        private long aLong;
+
+        public Node(String key, String val, int anInt, long aLong) {
+            this.key = key;
+            this.val = val;
+            this.anInt = anInt;
+            this.aLong = aLong;
+        }
+
+        public Node(){}
+
+        public String getKey() {
+            return key;
+        }
+
+        public Node setKey(String key) {
+            this.key = key;
+            return this;
+        }
+
+        public String getVal() {
+            return val;
+        }
+
+        public Node setVal(String val) {
+            this.val = val;
+            return this;
+        }
+
+        public int getAnInt() {
+            return anInt;
+        }
+
+        public Node setAnInt(int anInt) {
+            this.anInt = anInt;
+            return this;
+        }
+
+        public long getaLong() {
+            return aLong;
+        }
+
+        public Node setaLong(long aLong) {
+            this.aLong = aLong;
+            return this;
+        }
+    }
+
+    static class NodeGroup {
+        private List<Node> nodes = new ArrayList<>();
+
+        public NodeGroup(List<Node> nodes) {
+            this.nodes = nodes;
+        }
+
+        public List<Node> getNodes() {
+            return nodes;
+        }
+
+        public NodeGroup setNodes(List<Node> nodes) {
+            this.nodes = nodes;
+            return this;
+        }
+
+        public NodeGroup addNode(Node node) {
+            this.nodes.add(node);
+            return this;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        for(int i = 0; i < Integer.MAX_VALUE; i++) {
+            Node node = new Node("hello"+i, "world"+i, i, i);
+            byte[] serilized = Codec.DEFAULT.encodeAsBytes(node);
+            Node deserilized = Codec.DEFAULT.decode(serilized, Node.class);
+        }
+    }
+}
--- services/ctrip-service/src/test/java/com/ctrip/xpipe/service/AllTests.java
+++ services/ctrip-service/src/test/java/com/ctrip/xpipe/service/AllTests.java
@@ -1,13 +1,11 @@
 package com.ctrip.xpipe.service;
 
 import com.ctrip.xpipe.service.fireman.XPipeFiremanDependencyTest;
-import com.ctrip.xpipe.service.fireman.XPipeTemporaryDependencyTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses(value = {
-        XPipeTemporaryDependencyTest.class,
         XPipeFiremanDependencyTest.class
 })
 public class AllTests {
