692bd25c245a94cc9bb56d154803ce6bd04889a0
NickNYU <cz739@nyu.edu>
2019-09-11 17:05:25 +0800

    to pass ignite

--- redis/redis-keeper/src/main/java/com/ctrip/xpipe/redis/keeper/container/KeeperContainerController.java
+++ redis/redis-keeper/src/main/java/com/ctrip/xpipe/redis/keeper/container/KeeperContainerController.java
@@ -4,12 +4,17 @@ import com.ctrip.xpipe.redis.core.entity.KeeperInstanceMeta;
 import com.ctrip.xpipe.redis.core.entity.KeeperTransMeta;
 import com.ctrip.xpipe.redis.keeper.RedisKeeperServer;
 import com.ctrip.xpipe.spring.AbstractController;
+import com.ctrip.xpipe.utils.IpUtils;
 import com.google.common.base.Function;
 import com.google.common.collect.FluentIterable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Jason Song(song_s@ctrip.com)
@@ -66,5 +71,10 @@ public class KeeperContainerController extends AbstractController {
 
         logger.info("[stop]{},{}", clusterName, shardName);
         keeperContainerService.stop(clusterName, shardName);
+    }
+
+    @RequestMapping(value = "/health", method = RequestMethod.GET)
+    public boolean getHealthState(HttpServletRequest request, HttpServletResponse response) {
+        return true;
     }
 }
