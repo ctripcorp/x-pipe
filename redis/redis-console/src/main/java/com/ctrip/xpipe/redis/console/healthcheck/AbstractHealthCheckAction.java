56104d174819096c90b6963e2c30aa06f7eb56e7
lishanglin <sl_li@Ctrip.com>
2020-01-14 11:30:01 +0800

    console - dynamic sentinel check interval

--- redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/healthcheck/AbstractHealthCheckAction.java
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/healthcheck/AbstractHealthCheckAction.java
@@ -25,7 +25,7 @@ public abstract class AbstractHealthCheckAction<T extends ActionContext> extends
 
     protected ScheduledExecutorService scheduled;
 
-    protected ScheduledFuture future;
+    private ScheduledFuture future;
 
     protected ExecutorService executors;
 
@@ -97,7 +97,7 @@ public abstract class AbstractHealthCheckAction<T extends ActionContext> extends
         return future;
     }
 
-    protected void scheduleTask(int baseInterval) {
+    private void scheduleTask(int baseInterval) {
         long checkInterval = getCheckTimeInterval(baseInterval);
         future = scheduled.scheduleWithFixedDelay(new AbstractExceptionLogTask() {
 
