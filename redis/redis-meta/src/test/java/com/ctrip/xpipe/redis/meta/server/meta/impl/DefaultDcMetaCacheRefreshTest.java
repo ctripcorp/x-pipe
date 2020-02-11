2c27189a49efd414c3aeb0850a6c0f4a69a034b6
lishanglin <sl_li@Ctrip.com>
2020-02-11 16:45:34 +0800

    metaserver - unit test for refresh meta with DC change

---
+++ redis/redis-meta/src/test/java/com/ctrip/xpipe/redis/meta/server/meta/impl/DefaultDcMetaCacheRefreshTest.java
@@ -0,0 +1,124 @@
+package com.ctrip.xpipe.redis.meta.server.meta.impl;
+
+import com.ctrip.xpipe.redis.core.console.ConsoleService;
+import com.ctrip.xpipe.redis.core.entity.*;
+import com.ctrip.xpipe.redis.core.meta.MetaClone;
+import com.ctrip.xpipe.redis.core.protocal.pojo.MasterInfo;
+import com.ctrip.xpipe.redis.meta.server.AbstractMetaServerTest;
+import com.ctrip.xpipe.redis.meta.server.dcchange.ExecutionLog;
+import com.ctrip.xpipe.redis.meta.server.dcchange.impl.BecomePrimaryAction;
+import com.ctrip.xpipe.redis.meta.server.meta.DcMetaCache;
+import com.ctrip.xpipe.tuple.Pair;
+import com.ctrip.xpipe.utils.StringUtil;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DefaultDcMetaCacheRefreshTest extends AbstractMetaServerTest {
+
+    private DefaultDcMetaCache dcMetaCache;
+
+    @Mock
+    ConsoleService mockConsoleService;
+
+    @Before
+    public void beforeDefaultDcMetaCacheTest() throws Exception {
+        dcMetaCache = new DefaultDcMetaCache();
+
+        XpipeMeta xpipeMeta = getXpipeMeta();
+        DcMeta dcMeta = (DcMeta) xpipeMeta.getDcs().values().toArray()[0];
+        Mockito.when(mockConsoleService.getDcMeta(Mockito.anyString())).thenReturn(dcMeta);
+
+        injectConsoleServiceInto(dcMetaCache);
+        dcMetaCache.initialize();
+
+        System.out.println(dcMetaCache.getDcMeta());
+    }
+
+    private void injectConsoleServiceInto(DefaultDcMetaCache dcMetaCache) throws Exception {
+        Field consoleField = DefaultDcMetaCache.class.getDeclaredField("consoleService");
+        consoleField.setAccessible(true);
+        consoleField.set(dcMetaCache, mockConsoleService);
+    }
+
+    @Test
+    public void refreshDcMetaWithDcChangeTest() throws Exception {
+        DcMeta origin = dcMetaCache.getDcMeta().getDcMeta();
+        ClusterMeta clusterMeta = (ClusterMeta) origin.getClusters().values().toArray()[0];
+        ShardMeta shardMeta = (ShardMeta) clusterMeta.getShards().values().toArray()[0];
+        String newPrimaryDc = "newPrimaryDc";
+        String newBackUpDcs = StringUtil.isEmpty(clusterMeta.getBackupDcs()) ? clusterMeta.getActiveDc()
+                : clusterMeta.getActiveDc() + "," + clusterMeta.getBackupDcs();
+        MasterInfo masterInfo = new MasterInfo();
+
+        DcMeta expected = MetaClone.clone(origin);
+        expected.getClusters().get(clusterMeta.getId()).setActiveDc(newPrimaryDc.trim().toLowerCase())
+                .setBackupDcs(newBackUpDcs);
+
+        Mockito.when(mockConsoleService.getDcMeta(Mockito.anyString())).thenAnswer(invocationOnMock -> {
+            sleep(1000);
+            return origin;
+        });
+
+        ExecutionLog executionLog = new ExecutionLog("refreshDcMetaWithDcChangeTest");
+        BecomePrimaryAction becomePrimaryAction = new CustomBecomePrimaryAction(dcMetaCache, executionLog);
+
+        CountDownLatch latch = new CountDownLatch(2);
+        new Thread(() -> {
+            dcMetaCache.run();
+            latch.countDown();
+        }).start();
+
+        new Thread(() -> {
+            sleep(200); // wait for dcMetaCache run
+            becomePrimaryAction.changePrimaryDc(clusterMeta.getId(), shardMeta.getId(), newPrimaryDc, masterInfo);
+            latch.countDown();
+        }).start();
+
+        latch.await(3000, TimeUnit.MILLISECONDS);
+        Assert.assertEquals(expected.toString(), dcMetaCache.getDcMeta().getDcMeta().toString());
+    }
+
+
+
+
+    private static class CustomBecomePrimaryAction extends BecomePrimaryAction {
+        public CustomBecomePrimaryAction(DcMetaCache dcMetaCache,  ExecutionLog executionLog) {
+            super(dcMetaCache, null, null, null, executionLog,
+                    null, null, null, null);
+        }
+
+        @Override
+        protected Pair<String, Integer> chooseNewMaster(String clusterId, String shardId) {
+            return new Pair<>("127.0.0.1", 6379);
+        }
+
+        @Override
+        protected List<RedisMeta> getAllSlaves(Pair<String, Integer> newMaster, List<RedisMeta> shardRedises) {
+            return Collections.emptyList();
+        }
+
+        @Override
+        protected void makeRedisesOk(Pair<String, Integer> newMaster, List<RedisMeta> slaves) {
+        }
+
+        @Override
+        protected void makeKeepersOk(String clusterId, String shardId, Pair<String, Integer> newMaster) {
+        }
+
+        @Override
+        protected void changeSentinel(String clusterId, String shardId, Pair<String, Integer> newMaster) {
+        }
+    }
+
+}
