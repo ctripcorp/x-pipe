26484e31f56431a6ec4f81da7dc9834776d55b1a
NickNYU <cz739@nyu.edu>
2020-03-18 14:14:08 +0800

    not log too much

--- redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/healthcheck/actions/delay/DelayAction.java
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/healthcheck/actions/delay/DelayAction.java
@@ -49,12 +49,13 @@ public class DelayAction extends AbstractHealthCheckAction<DelayActionContext> {
 
     @Override
     protected void doTask() {
-        logger.info("[doTask][begin][{}]", instance.getRedisInstanceInfo().getClusterShardHostport());
+        //TODO: log only when it's too long to not execute
+//        logger.info("[doTask][begin][{}]", instance.getRedisInstanceInfo().getClusterShardHostport());
         reportDelay();
         RedisSession session = instance.getRedisSession();
         session.subscribeIfAbsent(CHECK_CHANNEL, callback);
         if(instance.getRedisInstanceInfo().isMaster()) {
-            logger.info("[doTask][pub][{}]", instance.getRedisInstanceInfo().getClusterShardHostport());
+//            logger.info("[doTask][pub][{}]", instance.getRedisInstanceInfo().getClusterShardHostport());
             session.publish(CHECK_CHANNEL, Long.toHexString(System.nanoTime()));
         }
     }
