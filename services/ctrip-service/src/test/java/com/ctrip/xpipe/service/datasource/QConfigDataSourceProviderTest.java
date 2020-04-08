ceabd9a2f736a4a1e07c08d5b5583c58bbb3e714
NickNYU <cz739@nyu.edu>
2020-04-08 21:46:39 +0800

    remove apollo totally except maven dependency control

--- services/ctrip-service/src/test/java/com/ctrip/xpipe/service/datasource/QConfigDataSourceProviderTest.java
+++ services/ctrip-service/src/test/java/com/ctrip/xpipe/service/datasource/QConfigDataSourceProviderTest.java
@@ -1,9 +1,5 @@
 package com.ctrip.xpipe.service.datasource;
 
-import com.ctrip.framework.apollo.ConfigFile;
-import com.ctrip.framework.apollo.ConfigService;
-import com.ctrip.framework.apollo.core.enums.ConfigFileFormat;
-import com.ctrip.framework.foundation.Foundation;
 import com.ctrip.xpipe.AbstractTest;
 import org.junit.Assert;
 import org.junit.Test;
@@ -21,28 +17,5 @@ public class QConfigDataSourceProviderTest extends AbstractTest {
     public void testQConfigDataSource() {
         TypedConfig<String> dataSourceConfig = TypedConfig.get("datasources.xml", new QConfigDataSourceProvider.CustomParser());
         logger.info("{}", dataSourceConfig.current());
-    }
-
-    @Test
-    public void testApolloDataSource() {
-        ConfigFile file = ConfigService.getConfigFile("datasources", ConfigFileFormat.XML);
-
-        if (file != null && file.hasContent()) {
-            String content = file.getContent();
-            logger.info("{}", content);
-        }
-    }
-
-    @Test
-    public void diff() {
-        ConfigFile file = ConfigService.getConfigFile("datasources", ConfigFileFormat.XML);
-
-        String content = file.getContent();
-        logger.info("{}", content);
-
-        TypedConfig<String> dataSourceConfig = TypedConfig.get("datasources.xml", new QConfigDataSourceProvider.CustomParser());
-        logger.info("{}", dataSourceConfig.current());
-
-        Assert.assertEquals(file.getContent(), dataSourceConfig.current());
     }
 }
