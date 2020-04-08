82b03d470dcc61f52d2662ec72c5b382e1d7f223
NickNYU <cz739@nyu.edu>
2020-04-08 19:16:23 +0800

    move to qconfig

--- redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/ds/XpipeDataSourceProvider.java
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/ds/XpipeDataSourceProvider.java
@@ -23,16 +23,16 @@ public class XpipeDataSourceProvider implements DataSourceProvider, LogEnabled, 
     private DataSourceProvider m_delegate;
     private org.slf4j.Logger logger = LoggerFactory.getLogger(getClass());
 
-    private static final String apolloDataSourceProviderClass =
-            "com.ctrip.framework.apollo.ds.ApolloDataSourceProvider";
-    private static boolean apolloDataSourceProviderPresent =
-            ClassUtils.isPresent(apolloDataSourceProviderClass, XpipeDataSourceProvider.class.getClassLoader());
+    private static final String qconfigDataSourceProviderClass =
+            "com.ctrip.xpipe.service.datasource.DataSourceProvider";
+    private static boolean qconfigDataSourceProviderPresent =
+            ClassUtils.isPresent(qconfigDataSourceProviderClass, XpipeDataSourceProvider.class.getClassLoader());
 
     @Override
     public void initialize() throws InitializationException {
-        if (apolloDataSourceProviderPresent) {
+        if (qconfigDataSourceProviderPresent) {
             try {
-                m_delegate = (DataSourceProvider)(Class.forName(apolloDataSourceProviderClass).newInstance());
+                m_delegate = (DataSourceProvider)(Class.forName(qconfigDataSourceProviderClass).newInstance());
             } catch (Throwable ex) {
                 m_logger.error("Loading apollo datasource provider failed", ex);
             }
