c0750c9c6bc9c17f68ca27d5a221b5e3ffe79697
lishanglin <sl_li@Ctrip.com>
2020-05-27 13:47:54 +0800

    health check for db variables

---
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/healthcheck/nonredis/dbvariables/checker/UnreachableMajorityTimeoutChecker.java
@@ -0,0 +1,23 @@
+package com.ctrip.xpipe.redis.console.healthcheck.nonredis.dbvariables.checker;
+
+import com.ctrip.xpipe.redis.console.alert.AlertManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UnreachableMajorityTimeoutChecker extends AbstractVariableChecker {
+
+    @Autowired
+    public UnreachableMajorityTimeoutChecker(AlertManager alertManager) {
+        super(alertManager);
+    }
+
+    boolean checkValue(Object value) {
+        return null != value && Integer.parseInt(value.toString()) > 0;
+    }
+
+    String getName() {
+        return "group_replication_unreachable_majority_timeout";
+    }
+
+}
