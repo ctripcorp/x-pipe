80c151c305b70a549d74c7f157ea12bcb11bbd1d
NickNYU <cz739@nyu.edu>
2020-06-17 20:46:06 +0800

    [new-api]support synchronizely delete xpipe cluster

--- redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/service/DcClusterService.java
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/service/DcClusterService.java
@@ -12,4 +12,5 @@ public interface DcClusterService {
 	DcClusterTbl addDcCluster(String dcName, String clusterName);
 	List<DcClusterTbl> findByClusterIds(List<Long> clusterIds);
 	List<DcClusterTbl> findAllByDcId(long dcId);
+	List<DcClusterTbl> findClusterRelated(long clusterId);
 }
