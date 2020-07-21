71d0cb883a12644d1b86942991e66ecd72643637
NickNYU <cz739@nyu.edu>
2020-07-21 13:41:52 +0800

    [bugfix]netty client use fixed allocator now

--- redis/redis-core/src/main/java/com/ctrip/xpipe/redis/core/protocal/cmd/pubsub/SubscribeCommand.java
+++ redis/redis-core/src/main/java/com/ctrip/xpipe/redis/core/protocal/cmd/pubsub/SubscribeCommand.java
@@ -3,6 +3,7 @@ package com.ctrip.xpipe.redis.core.protocal.cmd.pubsub;
 import com.ctrip.xpipe.api.endpoint.Endpoint;
 import com.ctrip.xpipe.api.pool.SimpleObjectPool;
 import com.ctrip.xpipe.netty.commands.NettyClient;
+import com.ctrip.xpipe.utils.VisibleForTesting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,10 +18,12 @@ public class SubscribeCommand extends AbstractSubscribe {
 
     private static final Logger logger = LoggerFactory.getLogger(SubscribeCommand.class);
 
+    @VisibleForTesting
     public SubscribeCommand(String host, int port, ScheduledExecutorService scheduled, String... channel) {
         super(host, port, scheduled, MESSAGE_TYPE.MESSAGE, channel);
     }
 
+    @VisibleForTesting
     public SubscribeCommand(Endpoint endpoint, ScheduledExecutorService scheduled, String... channel) {
         super(endpoint.getHost(), endpoint.getPort(), scheduled, MESSAGE_TYPE.MESSAGE, channel);
     }
