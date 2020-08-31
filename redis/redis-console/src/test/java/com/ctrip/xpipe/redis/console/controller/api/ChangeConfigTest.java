6c9ac3a2b427559b54b88cf30d47132da7622869
lishanglin <sl_li@Ctrip.com>
2020-08-31 10:58:36 +0800

    expose param "maintain minutes" for api stop sentinel check

---
+++ redis/redis-console/src/test/java/com/ctrip/xpipe/redis/console/controller/api/ChangeConfigTest.java
@@ -0,0 +1,69 @@
+package com.ctrip.xpipe.redis.console.controller.api;
+
+import com.ctrip.xpipe.redis.console.config.ConsoleConfig;
+import com.ctrip.xpipe.redis.console.model.ClusterTbl;
+import com.ctrip.xpipe.redis.console.service.ClusterService;
+import com.ctrip.xpipe.redis.console.service.ConfigService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ChangeConfigTest {
+
+    @Mock
+    private ConfigService configService;
+
+    @Mock
+    private ConsoleConfig consoleConfig;
+
+    @Mock
+    private ClusterService clusterService;
+
+    @InjectMocks
+    private ChangeConfig controller;
+
+    private String clusterName = "cluster1";
+
+    private int noAlarmMinutesForClusterUpdate = 15;
+
+    private int configDefaultRestoreHours = 1;
+
+    @Before
+    public void setupChangeConfigTest() {
+        Mockito.when(clusterService.find(clusterName)).thenReturn(new ClusterTbl());
+        Mockito.when(consoleConfig.getConfigDefaultRestoreHours()).thenReturn(configDefaultRestoreHours);
+        Mockito.when(consoleConfig.getNoAlarmMinutesForClusterUpdate()).thenReturn(noAlarmMinutesForClusterUpdate);
+    }
+
+    @Test
+    public void testStopSentinelCheck() throws Exception {
+        AtomicInteger expectedMinutes = new AtomicInteger(0);
+        Mockito.doAnswer(invocation -> {
+            Integer minutes = invocation.getArgumentAt(1, Integer.class);
+            Assert.assertEquals(minutes.intValue(), expectedMinutes.get());
+            return null;
+        }).when(configService).stopSentinelCheck(Mockito.any(), Mockito.anyInt());
+
+        expectedMinutes.set(noAlarmMinutesForClusterUpdate);
+        controller.stopSentinelCheck(Mockito.mock(HttpServletRequest.class), clusterName, null);
+        Mockito.verify(configService, Mockito.times(1)).stopSentinelCheck(Mockito.any(), Mockito.anyInt());
+
+        expectedMinutes.set(30);
+        controller.stopSentinelCheck(Mockito.mock(HttpServletRequest.class), clusterName, 30);
+        Mockito.verify(configService, Mockito.times(2)).stopSentinelCheck(Mockito.any(), Mockito.anyInt());
+
+        expectedMinutes.set(configDefaultRestoreHours * 60);
+        controller.stopSentinelCheck(Mockito.mock(HttpServletRequest.class), clusterName, configDefaultRestoreHours * 60 + 10);
+        Mockito.verify(configService, Mockito.times(3)).stopSentinelCheck(Mockito.any(), Mockito.anyInt());
+    }
+
+}
