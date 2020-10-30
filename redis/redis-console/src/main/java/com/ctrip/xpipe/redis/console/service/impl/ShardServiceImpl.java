1675c1327c97ead9ecea59b4b8f468c6cbd443a7
Cheng Chen （陈诚）(FU-IT) <cchen6@trip.com>
2020-10-30 16:13:17 +0800

    finish sync delete multi shard

--- redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/service/impl/ShardServiceImpl.java
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/service/impl/ShardServiceImpl.java
@@ -1,7 +1,7 @@
 package com.ctrip.xpipe.redis.console.service.impl;
 
 import com.ctrip.xpipe.cluster.ClusterType;
-import com.ctrip.xpipe.endpoint.HostPort;
+import com.ctrip.xpipe.redis.console.annotation.DalTransaction;
 import com.ctrip.xpipe.redis.console.dao.ShardDao;
 import com.ctrip.xpipe.redis.console.exception.ServerException;
 import com.ctrip.xpipe.redis.console.healthcheck.actions.delay.DelayService;
@@ -31,7 +31,7 @@ import java.util.stream.Collectors;
 
 @Service
 public class ShardServiceImpl extends AbstractConsoleService<ShardTblDao> implements ShardService {
-	
+
 	@Autowired
 	private DcService dcService;
 	@Autowired
@@ -56,7 +56,7 @@ public class ShardServiceImpl extends AbstractConsoleService<ShardTblDao> implem
 
 	@Resource(name = ConsoleContextConfig.GLOBAL_EXECUTOR)
 	private ExecutorService executors;
-	
+
 	@Override
 	public ShardTbl find(final long shardId) {
 		return queryHandler.handleQuery(new DalQuery<ShardTbl>() {
@@ -166,12 +166,52 @@ public class ShardServiceImpl extends AbstractConsoleService<ShardTblDao> implem
 				shardEvent.onEvent();
 			}
     	}
-    	
+
     	/** Notify meta server **/
 		List<DcTbl> relatedDcs = dcService.findClusterRelatedDc(clusterName);
     	if(null != relatedDcs) {
     		notifier.notifyClusterUpdate(clusterName, relatedDcs.stream().map(DcTbl::getDcName).collect(Collectors.toList()));
     	}
+	}
+
+	@Override
+	public void deleteShards(ClusterTbl cluster, List<String> shardNames) {
+		if(cluster != null) {
+			String clusterName = cluster.getClusterName();
+			List<ShardTbl> shards = queryHandler.handleQuery(new DalQuery<List<ShardTbl>>() {
+				@Override
+				public List<ShardTbl> doQuery() throws DalException {
+					return dao.findByShardNames(clusterName, shardNames, ShardTblEntity.READSET_ID_NAME_AND_MONITOR_NAME);
+				}
+			});
+
+			if (null != shards && !shards.isEmpty()) {
+				try {
+					shardDao.deleteShardsBatch(shards);
+				} catch (Exception e) {
+					throw new ServerException(e.getMessage());
+				}
+
+				for (ShardTbl shard : shards) {
+					// Call shard event
+					Map<Long, SetinelTbl> sentinels = sentinelService.findByShard(shard.getId());
+					ShardEvent shardEvent = null;
+					if (sentinels != null && !sentinels.isEmpty()) {
+						shardEvent = createShardDeleteEvent(clusterName, cluster, shard.getShardName(), shard, sentinels);
+					}
+					if (shardEvent != null) {
+						// RejectedExecutionException
+						shardEvent.onEvent();
+					}
+				}
+
+				/** Notify meta server **/
+				List<DcTbl> relatedDcs = dcService.findClusterRelatedDc(clusterName);
+				if (null != relatedDcs) {
+					notifier.notifyClusterUpdate(clusterName, relatedDcs.stream().map(DcTbl::getDcName).collect(Collectors.toList()));
+				}
+			}
+		}
 	}
 
 	@Override
