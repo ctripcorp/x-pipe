ed1db400f4df5ebf747035d7f5a0c925890a8f93
lishanglin <sl_li@trip.com>
2021-01-25 20:37:08 +0800

    integration test for normal migration

---
+++ redis/redis-integration-test/src/test/java/com/ctrip/xpipe/redis/integratedtest/dr/cmd/RedisKillCmd.java
@@ -0,0 +1,37 @@
+package com.ctrip.xpipe.redis.integratedtest.dr.cmd;
+
+import java.util.concurrent.ExecutorService;
+
+/**
+ * @author lishanglin
+ * date 2021/1/25
+ */
+public class RedisKillCmd extends AbstractForkProcessCmd {
+
+    private int port;
+
+    public RedisKillCmd(int port, ExecutorService executors) {
+        super(executors);
+        this.port = port;
+    }
+
+    @Override
+    protected void doExecute() throws Exception {
+        String[] cmd = new String[] {
+                "/bin/sh",
+                "-c",
+                String.format("kill -9 `ps -ef | grep 'redis-serve' | grep \"%d\" | awk '{print $2}'`", port)
+        };
+        execCmd(cmd);
+    }
+
+    @Override
+    protected void doReset() {
+    }
+
+    @Override
+    public String getName() {
+        return "RedisKillCmd-" + port;
+    }
+
+}
---
+++ redis/redis-integration-test/src/test/java/com/ctrip/xpipe/redis/integratedtest/dr/cmd/ServerStartCmd.java
@@ -0,0 +1,61 @@
+package com.ctrip.xpipe.redis.integratedtest.dr.cmd;
+
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+
+/**
+ * @author lishanglin
+ * date 2021/1/21
+ */
+public class ServerStartCmd extends AbstractForkProcessCmd {
+
+    private String mainClass;
+
+    private Map<String, String> args;
+
+    private String id;
+
+    public ServerStartCmd(String id, String mainClass, Map<String, String> args, ExecutorService executors) {
+        super(executors);
+        this.id = id;
+        this.mainClass = mainClass;
+        this.args = args;
+    }
+
+    @Override
+    protected void doExecute() throws Exception {
+        String javaHome = System.getProperty("java.home");
+        String javaExecutable = javaHome +
+                File.separator + "bin" +
+                File.separator + "java";
+
+        URL[] urls = ((URLClassLoader) Thread.currentThread().getContextClassLoader()).getURLs();
+        String classPath = ".:";
+        for (URL url: urls) {
+            classPath += url.getPath() + ":";
+        }
+        classPath = classPath.substring(0, classPath.length() - 1);
+
+        String cmd = javaExecutable + " -classpath \"" + classPath + "\" ";
+        StringBuilder argBuilder = new StringBuilder();
+        args.forEach((key, value) -> {
+            argBuilder.append(String.format("-D%s=%s ", key, value));
+        });
+
+        execCmd(cmd + argBuilder.toString() + mainClass);
+    }
+
+    @Override
+    protected void doReset() {
+
+    }
+
+    @Override
+    public String getName() {
+        return "ServerStartCmd-" + id + "-" + mainClass;
+    }
+
+}
