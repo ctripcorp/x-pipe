90951b16b2a4cfc8a3c3c13c206cdab41ec209d4
lishanglin <sl_li@trip.com>
2021-01-27 10:57:32 +0800

    alert for crdt instance back streaming

---
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/healthcheck/actions/redisstats/backstreaming/BackStreamingActionFactory.java
@@ -0,0 +1,49 @@
+package com.ctrip.xpipe.redis.console.healthcheck.actions.redisstats.backstreaming;
+
+import com.ctrip.xpipe.redis.console.alert.ALERT_TYPE;
+import com.ctrip.xpipe.redis.console.healthcheck.BiDirectionSupport;
+import com.ctrip.xpipe.redis.console.healthcheck.RedisHealthCheckInstance;
+import com.ctrip.xpipe.redis.console.healthcheck.actions.redisstats.RedisStatsCheckController;
+import com.ctrip.xpipe.redis.console.healthcheck.actions.redisstats.conflic.ConflictCheckAction;
+import com.ctrip.xpipe.redis.console.healthcheck.actions.redisstats.conflic.ConflictCheckListener;
+import com.ctrip.xpipe.redis.console.healthcheck.leader.AbstractLeaderAwareHealthCheckActionFactory;
+import com.ctrip.xpipe.redis.console.healthcheck.leader.AbstractRedisLeaderAwareHealthCheckActionFactory;
+import com.ctrip.xpipe.redis.console.healthcheck.leader.SiteLeaderAwareHealthCheckAction;
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author lishanglin
+ * date 2021/1/26
+ */
+@Component
+public class BackStreamingActionFactory extends AbstractRedisLeaderAwareHealthCheckActionFactory implements BiDirectionSupport {
+
+    @Autowired
+    private List<ConflictCheckListener> listeners;
+
+    @Autowired
+    private RedisStatsCheckController checkController;
+
+    @Override
+    public BackStreamingAction create(RedisHealthCheckInstance instance) {
+        BackStreamingAction action = new BackStreamingAction(scheduled, instance, executors);
+        action.addListeners(listeners);
+        action.addController(checkController);
+        return action;
+    }
+
+    @Override
+    public Class<? extends SiteLeaderAwareHealthCheckAction> support() {
+        return BackStreamingAction.class;
+    }
+
+    @Override
+    protected List<ALERT_TYPE> alertTypes() {
+        return Lists.newArrayList(ALERT_TYPE.CRDT_BACKSTREAMING);
+    }
+
+}
