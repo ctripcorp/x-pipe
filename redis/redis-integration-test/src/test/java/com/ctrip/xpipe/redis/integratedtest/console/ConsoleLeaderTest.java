91ae074c34ecf1808a182c836648e2204c149683
lishanglin <sl_li@trip.com>
2021-02-03 20:57:34 +0800

    integration test for console leader change

---
+++ redis/redis-integration-test/src/test/java/com/ctrip/xpipe/redis/integratedtest/console/ConsoleLeaderTest.java
@@ -0,0 +1,114 @@
+package com.ctrip.xpipe.redis.integratedtest.console;
+
+import com.ctrip.xpipe.redis.console.controller.api.HealthController;
+import com.ctrip.xpipe.redis.core.foundation.IdcUtil;
+import com.ctrip.xpipe.redis.integratedtest.dr.AbstractXPipeClusterTest;
+import com.ctrip.xpipe.redis.integratedtest.dr.ForkProcessCmd;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+
+import java.io.IOException;
+import java.util.*;
+
+import static com.ctrip.xpipe.redis.console.cluster.ConsoleLeaderElector.KEY_CONSOLE_ID;
+
+/**
+ * @author lishanglin
+ * date 2021/2/3
+ */
+public class ConsoleLeaderTest extends AbstractXPipeClusterTest {
+
+    private String dcJQ = "jq";
+
+    private String zkJQ;
+
+    private List<String> consoles;
+
+    private String consoleId8080 = "jq8080";
+    private String consoleId8081 = "jq8081";
+
+    private ForkProcessCmd jqConsole8080;
+    private ForkProcessCmd jqConsole8081;
+
+    @Override
+    protected String prepareDatas() throws IOException {
+        return prepareDatasFromFile("src/test/resources/single-dc.sql");
+    }
+
+    @Before
+    public void setupConsoleLeaderTest() {
+        zkJQ = "127.0.0.1:" + IdcUtil.JQ_ZK_PORT;
+        consoles = new ArrayList<>();
+        consoles.add("127.0.0.1:8080");
+        consoles.add("127.0.0.1:8081");
+    }
+
+    @After
+    public void afterDRTest() throws IOException {
+        cleanupAllSubProcesses();
+        killAllRedisServers();
+        cleanupConf();
+    }
+
+    @Test
+    public void testOriginLeaderRestart() throws Exception {
+        startJqConsoleClusters();
+
+        boolean is8080Leader = isLeader(8080);
+        boolean is8081Leader = isLeader(8081);
+
+        Assert.assertTrue(is8080Leader ^ is8081Leader);
+
+        ForkProcessCmd originLeaderConsole = is8080Leader ? jqConsole8080 : jqConsole8081;
+        int newLeaderPort = is8080Leader ? 8081 : 8080;
+        int newFollowerPort = is8080Leader ? 8080 : 8081;
+
+        stopServer(originLeaderConsole);
+        waitConditionUntilTimeOut(() -> isLeader(newLeaderPort), 10000, 1000);
+
+        startConsole(newFollowerPort, dcJQ, zkJQ, consoles, Collections.emptyMap(), Collections.emptyMap(), Collections.singletonMap(KEY_CONSOLE_ID, dcJQ + newFollowerPort));
+
+        waitForServerAck(String.format("http://localhost:%d/health", newFollowerPort), Map.class, 120000);
+
+        Assert.assertTrue(isLeader(newLeaderPort));
+        Assert.assertFalse(isLeader(newFollowerPort));
+
+        HealthController.RedisHealthCheckInstanceModel checkInstanceOfLeader = getCheckInstance(newLeaderPort);
+        HealthController.RedisHealthCheckInstanceModel checkInstanceOfFollower = getCheckInstance(newFollowerPort);
+
+        Assert.assertTrue(checkInstanceOfLeader.getActions().size() > checkInstanceOfFollower.getActions().size());
+    }
+
+    private boolean isLeader(int port) {
+        Map<String, Object> healthState = restTemplate.exchange(String.format("http://localhost:%d/health", port),
+                HttpMethod.GET, null, new ParameterizedTypeReference<Map<String, Object>>() {}).getBody();
+
+        return (Boolean) healthState.get("isLeader");
+    }
+
+    private HealthController.RedisHealthCheckInstanceModel getCheckInstance(int port) throws Exception {
+        String url = String.format("http://localhost:%d/api/health/check/instance/127.0.0.1/6379", port);
+        waitForServerAck(url, HealthController.RedisHealthCheckInstanceModel.class, 30000);
+        return restTemplate.getForObject(url, HealthController.RedisHealthCheckInstanceModel.class);
+    }
+
+    private void startJqConsoleClusters() throws Exception {
+        startZk(IdcUtil.JQ_ZK_PORT);
+
+        startH2Server();
+        setUpTestDataSource();
+
+        startRedis(6379);
+
+        jqConsole8080 = startConsole(8080, dcJQ, zkJQ, consoles, Collections.emptyMap(), Collections.emptyMap(), Collections.singletonMap(KEY_CONSOLE_ID, consoleId8080));
+        jqConsole8081 = startConsole(8081, dcJQ, zkJQ, consoles, Collections.emptyMap(), Collections.emptyMap(), Collections.singletonMap(KEY_CONSOLE_ID, consoleId8081));
+
+        waitForServerAck("http://localhost:8080/health", Map.class, 120000);
+        waitForServerAck("http://localhost:8081/health", Map.class, 60000);
+    }
+
+}
--- redis/redis-integration-test/src/test/java/com/ctrip/xpipe/redis/integratedtest/dr/DRTest.java
+++ redis/redis-integration-test/src/test/java/com/ctrip/xpipe/redis/integratedtest/dr/DRTest.java
@@ -28,6 +28,7 @@ import com.ctrip.xpipe.utils.FileUtils;
 import com.lambdaworks.redis.models.role.RedisInstance;
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.web.client.RestOperations;
 
@@ -49,9 +50,7 @@ import static com.ctrip.xpipe.redis.meta.server.config.DefaultMetaServerConfig.K
  * @author lishanglin
  * date 2021/1/21
  */
-public class DRTest extends AbstractConsoleH2DbTest {
-
-    private RestOperations restTemplate;
+public class DRTest extends AbstractXPipeClusterTest {
 
     private Map<String, DcInfo> dcInfos;
 
@@ -59,18 +58,15 @@ public class DRTest extends AbstractConsoleH2DbTest {
 
     private Map<String, String> metaservers;
 
-    private List<ForkProcessCmd> subProcessCmds;
+    private ForkProcessCmd jqMetaServer;
+    private ForkProcessCmd oyMetaServer;
 
     private String zkJQ;
 
     private String zkOY;
 
-    @Override
-    public void before() {
-        restTemplate = RestTemplateFactory.createRestTemplate();
-
-        subProcessCmds = new ArrayList<>();
-
+    @Before
+    public void setupDRTest() {
         dcInfos = new HashMap<>();
         dcInfos.put("jq", new DcInfo("http://127.0.0.1:" + IdcUtil.JQ_METASERVER_PORT));
         dcInfos.put("oy", new DcInfo("http://127.0.0.1:" + IdcUtil.OY_METASERVER_PORT));
@@ -89,8 +85,8 @@ public class DRTest extends AbstractConsoleH2DbTest {
 
     @After
     public void afterDRTest() throws IOException {
-        cleanupSubProcesses();
-        killRedisServers();
+        cleanupAllSubProcesses();
+        killAllRedisServers();
         cleanupConf();
     }
 
@@ -114,21 +110,15 @@ public class DRTest extends AbstractConsoleH2DbTest {
     }
 
     @Test
-    public void testMigrationOnOriginDcMetaserverDown() throws Exception {
+    public void testMigrationWhenOriginDcMetaserverDown() throws Exception {
         startSimpleXPipeDR();
 
         // check migration system
         waitForServerRespAsExpected("http://localhost:8081/api/migration/migration/system/health/status", RetMessage.class, RetMessage.createSuccessMessage(), 60000);
 
-        Optional<ForkProcessCmd> jqMetaserver = subProcessCmds.stream().filter(subProcess -> {
-            return subProcess instanceof ServerStartCmd && ((ServerStartCmd) subProcess).getId().equals("jq" + IdcUtil.JQ_METASERVER_PORT);
-        }).findFirst();
-        if (!jqMetaserver.isPresent()) Assert.fail("no sub process for jq metaserver");
+        stopServer(jqMetaServer);
 
-        subProcessCmds.remove(jqMetaserver.get());
-        jqMetaserver.get().killProcess();
-
-        if (jqMetaserver.get().isProcessAlive()) Assert.fail("jq metaserver is still alive after kill");
+        if (jqMetaServer.isProcessAlive()) Assert.fail("jq metaserver is still alive after kill");
 
         // do migration
         tryMigration("http://localhost:8081", "cluster-dr", "jq", "oy");
@@ -138,7 +128,7 @@ public class DRTest extends AbstractConsoleH2DbTest {
         waitForRedisRole("127.0.0.1", 7379, Server.SERVER_ROLE.MASTER, 15000);
 
         // recover origin primary dc metaserver
-        subProcessCmds.add(startMetaServer("jq", "http://127.0.0.1:8080", zkJQ, IdcUtil.JQ_METASERVER_PORT, dcInfos));
+        jqMetaServer = startMetaServer("jq", "http://127.0.0.1:8080", zkJQ, IdcUtil.JQ_METASERVER_PORT, dcInfos);
 
         // wait for repl recover
         waitForServerRespAsExpected("http://localhost:8081/api/health/127.0.0.1/6379", String.class, "\"HEALTHY\"", 120000);
@@ -151,240 +141,32 @@ public class DRTest extends AbstractConsoleH2DbTest {
         startH2Server();
         setUpTestDataSource(); // init data in h2
 
-        subProcessCmds.add(startRedis(6379));
-        subProcessCmds.add(startRedis(7379));
-        subProcessCmds.add(startConsole(8080, "jq", zkJQ, consoles, metaservers));
-        subProcessCmds.add(startConsole(8081, "oy", zkOY, consoles, metaservers));
+        startRedis(6379);
+        startRedis(7379);
+
+        Map<String, String> extraOptions = Collections.singletonMap(KEY_CLUSTER_SHARD_FOR_MIGRATE_SYS_CHECK, "cluster-dr,cluster-dr-shard1");
+        startConsole(8080, "jq", zkJQ, Collections.singletonList("127.0.0.1:8080"), consoles, metaservers, extraOptions);
+        startConsole(8081, "oy", zkOY, Collections.singletonList("127.0.0.1:8081"), consoles, metaservers, extraOptions);
 
         String userDir = System.getProperty("user.dir");
-        subProcessCmds.add(startKeepercontainer("jq", zkJQ, 7080, userDir + "/src/test/tmp/keepercontainer7080"));
-        subProcessCmds.add(startKeepercontainer("jq", zkJQ, 7081, userDir + "/src/test/tmp/keepercontainer7081"));
-        subProcessCmds.add(startKeepercontainer("oy", zkOY, 7180, userDir + "/src/test/tmp/keepercontainer7180"));
-        subProcessCmds.add(startKeepercontainer("oy", zkOY, 7181, userDir + "/src/test/tmp/keepercontainer7181"));
+        startKeepercontainer("jq", zkJQ, 7080, userDir + "/src/test/tmp/keepercontainer7080");
+        startKeepercontainer("jq", zkJQ, 7081, userDir + "/src/test/tmp/keepercontainer7081");
+        startKeepercontainer("oy", zkOY, 7180, userDir + "/src/test/tmp/keepercontainer7180");
+        startKeepercontainer("oy", zkOY, 7181, userDir + "/src/test/tmp/keepercontainer7181");
 
         // wait for console init
-        waitForServerInit("http://localhost:8080/api/dc/jq", DcMeta.class, 120000);
-        waitForServerInit("http://localhost:8081/api/dc/oy", DcMeta.class, 60000);
+        waitForServerAck("http://localhost:8080/api/dc/jq", DcMeta.class, 120000);
+        waitForServerAck("http://localhost:8081/api/dc/oy", DcMeta.class, 60000);
 
         checkAllProcessAlive();
 
-        subProcessCmds.add(startMetaServer("jq", "http://127.0.0.1:8080", zkJQ, IdcUtil.JQ_METASERVER_PORT, dcInfos));
-        subProcessCmds.add(startMetaServer("oy", "http://127.0.0.1:8081", zkOY, IdcUtil.OY_METASERVER_PORT, dcInfos));
+        jqMetaServer = startMetaServer("jq", "http://127.0.0.1:8080", zkJQ, IdcUtil.JQ_METASERVER_PORT, dcInfos);
+        oyMetaServer = startMetaServer("oy", "http://127.0.0.1:8081", zkOY, IdcUtil.OY_METASERVER_PORT, dcInfos);
 
         // repl online
         waitForServerRespAsExpected("http://localhost:8080/api/health/127.0.0.1/7379", String.class, "\"HEALTHY\"", 120000);
 
         checkAllProcessAlive();
-    }
-
-    protected RedisStartCmd startRedis(int port) {
-        RedisStartCmd redis = new RedisStartCmd(port, executors);
-        redis.execute(executors).addListener(redisFuture -> {
-            if (redisFuture.isSuccess()) {
-                logger.info("[startRedis] redis{} end {}", port, redisFuture.get());
-            } else {
-                logger.info("[startRedis] redis{} fail", port, redisFuture.cause());
-            }
-        });
-
-        return redis;
-    }
-
-    protected ServerStartCmd startConsole(int port, String idc, String zk, Map<String, String> consoles, Map<String, String> metaservers) {
-        ServerStartCmd consoleServer = new ServerStartCmd(idc + port, ConsoleApp.class.getName(), new HashMap<String, String>() {{
-            put(HealthChecker.ENABLED, "true");
-            put("server.port", String.valueOf(port));
-            put("cat.client.enabled", "false");
-            put("spring.profiles.active", AbstractProfile.PROFILE_NAME_PRODUCTION);
-            put(DATA_CENTER_KEY, idc);
-            put(KEY_ZK_ADDRESS, zk);
-            put(KEY_CLUSTER_SHARD_FOR_MIGRATE_SYS_CHECK, "cluster-dr,cluster-dr-shard1");
-            put(KEY_METASERVERS, JsonCodec.INSTANCE.encode(metaservers));
-            put("console.domains", JsonCodec.INSTANCE.encode(consoles));
-        }}, executors);
-        consoleServer.execute(executors).addListener(consoleFuture -> {
-            if (consoleFuture.isSuccess()) {
-                logger.info("[startConsoleJQ] console {}-{} end {}", idc, port, consoleFuture.get());
-            } else {
-                logger.info("[startConsoleJQ] console {}-{} fail", idc, port, consoleFuture.cause());
-            }
-
-        });
-
-        return consoleServer;
-    }
-
-    protected ServerStartCmd startMetaServer(String idc, String console, String zk, int port, Map<String, DcInfo> dcInfos) {
-        ServerStartCmd metaserver = new ServerStartCmd(idc + port, MetaserverApp.class.getName(), new HashMap<String, String>() {{
-            put("server.port", String.valueOf(port));
-            put("cat.client.enabled", "false");
-            put("spring.profiles.active", AbstractProfile.PROFILE_NAME_PRODUCTION);
-            put(DATA_CENTER_KEY, idc);
-            put(KEY_CONSOLE_ADDRESS, console);
-            put(KEY_ZK_ADDRESS, zk);
-            put(DefaultMetaServerConfig.KEY_DC_INFOS, JsonCodec.INSTANCE.encode(dcInfos));
-        }}, executors);
-        metaserver.execute(executors).addListener(metaserverFuture -> {
-            if (metaserverFuture.isSuccess()) {
-                logger.info("[startMetaServer] metaserver {}-{} end {}", idc, port, metaserverFuture.get());
-            } else {
-                logger.info("[startMetaServer] metaserver {}-{} fail", idc, port, metaserverFuture.cause());
-            }
-        });
-
-        return metaserver;
-    }
-
-    protected ServerStartCmd startKeepercontainer(String idc, String zk, int port, String storeDir) {
-        ServerStartCmd keepercontainer = new ServerStartCmd(idc + port, KeeperContainerApplication.class.getName(), new HashMap<String, String>() {{
-            put("server.port", String.valueOf(port));
-            put("cat.client.enabled", "false");
-            put("spring.profiles.active", AbstractProfile.PROFILE_NAME_PRODUCTION);
-            put(DATA_CENTER_KEY, idc);
-            put(KEY_ZK_ADDRESS, zk);
-            put(REPLICATION_STORE_DIR, storeDir);
-            put(KEY_REPLICATION_STORE_COMMANDFILE_SIZE, "104857600");
-            put(KEY_REPLICATION_STORE_MAX_COMMANDS_TO_TRANSFER_BEFORE_CREATE_RDB, "524288000");
-        }}, executors);
-        keepercontainer.execute(executors).addListener(keepercontainerFuture -> {
-            if (keepercontainerFuture.isSuccess()) {
-                logger.info("[startKeepercontainer] metaserver {}-{} end {}", idc, port, keepercontainerFuture.get());
-            } else {
-                logger.info("[startKeepercontainer] metaserver {}-{} fail", idc, port, keepercontainerFuture.cause());
-            }
-        });
-
-        return keepercontainer;
-    }
-
-    protected void tryMigration(String console, String cluster, String src, String dest) {
-        CheckPrepareRequest prepareRequest = new CheckPrepareRequest();
-        prepareRequest.setClusters(Collections.singletonList(cluster));
-        prepareRequest.setFromIdc(src);
-        prepareRequest.setToIdc(dest);
-        prepareRequest.setIsForce(false);
-
-        CheckPrepareResponse prepareResp = restTemplate.postForObject(console + "/api/migration/checkandprepare", prepareRequest, CheckPrepareResponse.class);
-        if (!prepareResp.getResults().get(0).isSuccess()) Assert.fail("migration prepare fail " + prepareResp.getResults().get(0).getFailReason());
-
-        DoRequest doRequest = new DoRequest();
-        doRequest.setTicketId(prepareResp.getTicketId());
-        DoResponse doResp = restTemplate.postForObject(console + "/api/migration/domigration", doRequest, DoResponse.class);
-
-        if (!doResp.isSuccess()) Assert.fail("do migration fail " + doResp.getMsg());
-    }
-
-    /* --------- wait condition --------- */
-
-    protected void waitForServerInit(String healthUrl, Class<?> respType, int waitTimeMilli) throws Exception {
-        waitConditionUntilTimeOut(() -> {
-            try {
-                Object resp = restTemplate.getForObject(healthUrl, respType);
-                logger.info("[waitForServerInit] resp for {}, {}", healthUrl, resp);
-                return true;
-            } catch (Throwable th) {
-                return false;
-            }
-        }, waitTimeMilli, 2000);
-    }
-
-    protected void waitForServerRespAsExpected(String healthUrl, Class<?> respType, Object expected, int waitTimeMilli) throws Exception {
-        waitConditionUntilTimeOut(() -> {
-            try {
-                Object resp = restTemplate.getForObject(healthUrl, respType);
-                logger.info("[waitForServerRespAsExpected] resp for {}, {}", healthUrl, resp);
-                return expected.equals(resp);
-            } catch (Throwable th) {
-                return false;
-            }
-        }, waitTimeMilli, 2000);
-    }
-
-    protected void waitForRedisRole(String host, int port, Server.SERVER_ROLE expected, int waitTimeMilli) throws Exception {
-        waitConditionUntilTimeOut(() -> {
-            try {
-                Role current = new RoleCommand(host, port, scheduled).execute().get();
-                logger.info("[waitForRedisRole][{}][{}] current role {}", host, port, current);
-                return current.getServerRole().equals(expected);
-            } catch (Throwable th) {
-                logger.info("[waitForRedisRole][{}][{}] role cmd fail", host, port, th);
-                return false;
-            }
-        }, waitTimeMilli, 2000);
-    }
-
-    /* --------- cleanup --------- */
-
-    protected void cleanupKeeper() {
-        String userDir = System.getProperty("user.dir");
-        IntStream.of(7080, 7081, 7082, 7180, 7181, 7182).forEach(port -> {
-            File dir = new File(userDir + "/src/test/tmp/keepercontainer" + port);
-            if (dir.exists()) {
-                try {
-                    FileUtils.recursiveDelete(dir);
-                } catch (Throwable th) {
-                    logger.info("[cleanupKeeper][{}] delete dir fail", port, th);
-                }
-            } else {
-                logger.info("[cleanupKeeper][{}] no rsd dir {}", port, dir.getAbsolutePath());
-            }
-        });
-    }
-
-    protected void cleanupRDB() {
-        String userDir = System.getProperty("user.dir");
-        IntStream.of(6379, 7379).forEach(port -> {
-            File rdb = new File(userDir + "/src/test/tmp/dump" + port + ".rdb");
-            try {
-                if (rdb.exists()) rdb.delete();
-                else logger.info("[cleanupRDB][{}] no rdb {}", port, rdb.getAbsolutePath());
-            } catch (Throwable th) {
-                logger.info("[cleanupRDB][{}] delete rdb fail", port, th);
-            }
-        });
-    }
-
-    protected void cleanupConf() {
-        String userDir = System.getProperty("user.dir");
-        IntStream.of(6379, 7379).forEach(port -> {
-            File conf = new File(userDir + "/src/test/tmp/redis" + port + ".conf");
-            try {
-                if (conf.exists()) conf.delete();
-                else logger.info("[cleanupConf][{}] no conf {}", port, conf.getAbsolutePath());
-            } catch (Throwable th) {
-                logger.info("[cleanupConf][{}] delete conf fail", port, th);
-            }
-        });
-    }
-
-    protected void cleanupSubProcesses() {
-        for (ForkProcessCmd subProcess: subProcessCmds) {
-            try {
-                logger.info("[cleanupSubProcesses][{}]", subProcess);
-                subProcess.killProcess();
-            } catch (Throwable th) {
-                logger.info("[cleanupSubProcesses][{}] kill thread fail", subProcess, th);
-            }
-        }
-    }
-
-    protected void checkAllProcessAlive() {
-        for (ForkProcessCmd subProcess: subProcessCmds) {
-            logger.info("[checkAllProcessAlive][{}]", subProcess);
-            if (!subProcess.isProcessAlive()) {
-                throw new IllegalStateException("sub process " + subProcess + " is down");
-            }
-        }
-    }
-
-    protected void killRedisServers() {
-        IntStream.of(6379, 7379).forEach(port -> {
-            try {
-                new RedisKillCmd(port, executors).execute().get();
-            } catch (Throwable th) {
-                logger.info("[killRedisServers][{}] fail", port, th);
-            }
-        });
     }
 
 }
