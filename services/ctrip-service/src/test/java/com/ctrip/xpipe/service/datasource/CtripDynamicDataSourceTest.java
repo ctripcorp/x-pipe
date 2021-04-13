a898febcf374ac21e3316587dc73d6dabd7e2b49
lishanglin <sl_li@trip.com>
2021-04-13 20:09:40 +0800

    import dal dynamic datasource

---
+++ services/ctrip-service/src/test/java/com/ctrip/xpipe/service/datasource/CtripDynamicDataSourceTest.java
@@ -0,0 +1,42 @@
+package com.ctrip.xpipe.service.datasource;
+
+import org.apache.tomcat.jdbc.pool.PoolExhaustedException;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.sql.Connection;
+
+/**
+ * @author lishanglin
+ * date 2021/4/13
+ */
+@Ignore
+public class CtripDynamicDataSourceTest {
+
+    // 1、start mysqld
+    // 2、prepare local config /opt/config/100004374/qconfig/datasource.properties,
+    // /opt/config/100004374/dal/local-databases.properties, /opt/config/100004374/qconfig/application.properties
+
+    private CtripDynamicDataSource dataSource;
+
+    @Before
+    public void setupCtripDynamicDataSourceTest() {
+        this.dataSource = new CtripDynamicDataSource();
+        this.dataSource.initialize(null);
+    }
+
+    @Test
+    public void testAccessLocalMySQL() throws Exception {
+        Connection connection = dataSource.getConnection();
+        connection.close();
+    }
+
+    // set maxActive=1, maxIdle=1 in local datasource.properties
+    @Test(expected = PoolExhaustedException.class)
+    public void testMaxActive1() throws Exception {
+        dataSource.getConnection();
+        dataSource.getConnection();
+    }
+
+}
