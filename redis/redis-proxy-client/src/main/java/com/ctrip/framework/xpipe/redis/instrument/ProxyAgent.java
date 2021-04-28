79b73fa9a41096e4fc890fddfdc09cf07940ae54
md_li <md_li@trip.com>
2021-04-28 23:40:59 +0800

    remove duplicate loaded class check

--- redis/redis-proxy-client/src/main/java/com/ctrip/framework/xpipe/redis/instrument/ProxyAgent.java
+++ redis/redis-proxy-client/src/main/java/com/ctrip/framework/xpipe/redis/instrument/ProxyAgent.java
@@ -9,28 +9,22 @@ import com.ctrip.framework.xpipe.redis.instrument.adapter.SocketChannelImplAdapt
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class ProxyAgent implements ClassFileTransformer {
 
-    private static Map<String, String> transformedClasses = new ConcurrentHashMap<>();
-
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
         className = className.replace("/", ".");
-        if (className.equals("java.net.Socket") && !transformedClasses.containsKey(className)) {
+        if (className.equals("java.net.Socket")) {
             ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
             SocketAdapter socketAdapter = new SocketAdapter(classWriter);
             ClassReader classReader = new ClassReader(classfileBuffer);
             classReader.accept(socketAdapter, 0);
-            transformedClasses.put(className, className);
             return classWriter.toByteArray();
-        } else if (className.equals("sun.nio.ch.SocketChannelImpl") && !transformedClasses.containsKey(className)) {
+        } else if (className.equals("sun.nio.ch.SocketChannelImpl")) {
             ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
             SocketChannelImplAdapter socketChannelImplAdapter = new SocketChannelImplAdapter(classWriter);
             ClassReader classReader = new ClassReader(classfileBuffer);
             classReader.accept(socketChannelImplAdapter, 0);
-            transformedClasses.put(className, className);
             return classWriter.toByteArray();
         }
         return null;
