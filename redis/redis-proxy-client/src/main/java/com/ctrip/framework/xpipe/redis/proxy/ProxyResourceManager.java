dee0192bd793bcfe75cb0326c8f896894c0679b1
md_li <md_li@trip.com>
2021-04-28 09:55:37 +0800

    work without log

--- redis/redis-proxy-client/src/main/java/com/ctrip/framework/xpipe/redis/proxy/ProxyResourceManager.java
+++ redis/redis-proxy-client/src/main/java/com/ctrip/framework/xpipe/redis/proxy/ProxyResourceManager.java
@@ -1,14 +1,10 @@
 package com.ctrip.framework.xpipe.redis.proxy;
 
-import com.ctrip.xpipe.api.lifecycle.Startable;
-import com.ctrip.xpipe.api.lifecycle.Stoppable;
-import com.ctrip.xpipe.proxy.ProxyEndpoint;
-import com.ctrip.xpipe.redis.core.exception.NoResourceException;
-import io.netty.buffer.ByteBuf;
+import java.net.InetSocketAddress;
 
-public interface ProxyResourceManager extends Startable, Stoppable {
+public interface ProxyResourceManager {
 
-    ByteBuf getProxyConnectProtocol();
+    byte[] getProxyConnectProtocol();
 
-    ProxyEndpoint nextHop() throws NoResourceException;
+    InetSocketAddress nextHop();
 }
---
+++ redis/redis-proxy-client/src/main/java/com/ctrip/framework/xpipe/redis/proxy/RouteOptionParser.java
@@ -0,0 +1,69 @@
+package com.ctrip.framework.xpipe.redis.proxy;
+
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class RouteOptionParser {
+
+    public static final String ELEMENT_SPLITTER = "\\h";
+    public static final String WHITE_SPACE = " ";
+    public static final String ARRAY_SPLITTER = "\\s*,\\s*";
+    public static final String ROUTE = "ROUTE";
+
+    private String[] nodes;
+
+    private String[] nextNodes;
+
+    protected String originOptionString;
+
+    protected String output;
+
+    private AtomicBoolean nextNodesRemoved = new AtomicBoolean(false);
+
+    public List<InetSocketAddress> getNextEndpoints() {
+        if (nextNodes == null || nextNodes.length == 0) {
+            return null;
+        }
+        List<InetSocketAddress> result = new ArrayList<>();
+        for (String rawUri : nextNodes) {
+            try {
+                URI uri = new URI(rawUri);
+                result.add(new InetSocketAddress(uri.getHost(), uri.getPort()));
+            } catch (Exception e) {
+            }
+        }
+        return result;
+    }
+
+    public RouteOptionParser read(String option) {
+        if (option == null || option.isEmpty() || option.length() <= ROUTE.length() + 1) {
+            return this;
+        }
+        this.originOptionString = option.substring(ROUTE.length() + 1);
+        this.nodes = originOptionString.split(ELEMENT_SPLITTER);
+        this.nextNodes = nodes[0].split(ARRAY_SPLITTER);
+        return this;
+    }
+
+    public String output() {
+        removeNextNodes();
+        StringBuilder sb = new StringBuilder();
+        sb.append(ROUTE);
+        for (String node : nodes) {
+            sb.append(WHITE_SPACE).append(node);
+        }
+        return sb.toString();
+    }
+
+    public void removeNextNodes() {
+        if (nextNodesRemoved.compareAndSet(false, true) && nodes.length > 0) {
+            String[] newNodes = new String[nodes.length - 1];
+            System.arraycopy(nodes, 1, newNodes, 0, newNodes.length);
+            nodes = newNodes;
+        }
+    }
+
+}
