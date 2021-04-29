304a3f9589dd40bc6cc2d93cd381a67d89a0b99c
md_li <md_li@trip.com>
2021-04-29 13:30:20 +0800

    catch exception when path parse error

--- redis/redis-proxy-client/src/main/java/com/ctrip/framework/xpipe/redis/instrument/ProxyAgentTool.java
+++ redis/redis-proxy-client/src/main/java/com/ctrip/framework/xpipe/redis/instrument/ProxyAgentTool.java
@@ -3,6 +3,7 @@ package com.ctrip.framework.xpipe.redis.instrument;
 import com.ctrip.framework.xpipe.redis.utils.Tools;
 
 import java.lang.reflect.Method;
+import java.net.URI;
 import java.nio.file.Paths;
 import java.security.CodeSource;
 
@@ -16,40 +17,45 @@ public class ProxyAgentTool {
 
     private static volatile boolean isLoaded = false;
 
-    public static synchronized void startUp() throws Exception {
+    public static synchronized void startUp() {
         if (!isLoaded) {
             loadAgent();
             isLoaded = true;
         }
     }
 
-    private static void loadAgent() throws Exception {
+    private static void loadAgent() {
 
-        Object VM;
-        Method loadAgentMethod;
-        Method detachMethod;
-        Class<?> vmClass = Tools.loadJDKToolClass(VirtualMachineClassName);
-        Class<?> hotspotVMClass = Tools.loadJDKToolClass(HotspotVMName);
-        String pid = Tools.currentPID();
-        if (VmProviderClass != null) {
-            Object vmProvider = VmProviderClass.newInstance();
-            VM = VmProviderClass.getMethod("attachVirtualMachine", String.class).invoke(vmProvider, pid);
-            loadAgentMethod = VM.getClass().getMethod("loadAgent", String.class, String.class);
-            detachMethod = VM.getClass().getMethod("detach");
-        } else {
-            Method attacheMethod = vmClass.getMethod("attach", String.class);
-            VM = attacheMethod.invoke(null, pid);
-            VmProviderClass = vmClass.getMethod("provider").invoke(VM).getClass();
-            loadAgentMethod = hotspotVMClass.getMethod("loadAgent", String.class, String.class);
-            detachMethod = vmClass.getMethod("detach");
+        String msg = null;
+        try {
+            Object VM;
+            Method loadAgentMethod;
+            Method detachMethod;
+            Class<?> vmClass = Tools.loadJDKToolClass(VirtualMachineClassName);
+            Class<?> hotspotVMClass = Tools.loadJDKToolClass(HotspotVMName);
+            String pid = Tools.currentPID();
+            if (VmProviderClass != null) {
+                Object vmProvider = VmProviderClass.newInstance();
+                VM = VmProviderClass.getMethod("attachVirtualMachine", String.class).invoke(vmProvider, pid);
+                loadAgentMethod = VM.getClass().getMethod("loadAgent", String.class, String.class);
+                detachMethod = VM.getClass().getMethod("detach");
+            } else {
+                Method attacheMethod = vmClass.getMethod("attach", String.class);
+                VM = attacheMethod.invoke(null, pid);
+                VmProviderClass = vmClass.getMethod("provider").invoke(VM).getClass();
+                loadAgentMethod = hotspotVMClass.getMethod("loadAgent", String.class, String.class);
+                detachMethod = vmClass.getMethod("detach");
+            }
+            CodeSource src = AgentMain.class.getProtectionDomain().getCodeSource();
+            URI uri = src.getLocation().toURI();
+            msg = String.format("[AgentMain] proxy uri:%s", uri);
+            String proxyFile = uri.getSchemeSpecificPart();
+            String jarPath = Paths.get(uri).toString();
+            loadAgentMethod.invoke(VM, jarPath, proxyFile);
+            detachMethod.invoke(VM);
+        } catch (Exception e) {
+            throw new RuntimeException(msg, e);
         }
-        CodeSource src = AgentMain.class.getProtectionDomain().getCodeSource();
-
-        String jarPath = Paths.get(src.getLocation().toURI()).toString();
-        String proxyFile = src.getLocation().toURI().getSchemeSpecificPart();
-        loadAgentMethod.invoke(VM, jarPath, proxyFile);
-        System.out.println(String.format("[AgentMain] jar path:%s, proxy file:%s", jarPath, proxyFile));
-        detachMethod.invoke(VM);
     }
 
 }
