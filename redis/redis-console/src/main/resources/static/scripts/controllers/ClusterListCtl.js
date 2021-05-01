f33e89af65caf7af21dee00bb54bd947ab3c74db
wcc吴骋成 <wucc@trip.com>
2021-05-01 19:39:22 +0800

    init webpack, starting to modify angularjs into standard-style

--- redis/redis-console/src/main/resources/static/scripts/controllers/ClusterListCtl.js
+++ redis/redis-console/src/main/resources/static/scripts/controllers/ClusterListCtl.js
@@ -1,31 +1,124 @@
-index_module.controller('ClusterListCtl', ['$rootScope', '$scope', '$window', '$stateParams', 'AppUtil', 'toastr', 'ClusterService','DcService', 'NgTableParams', 'ClusterType',
-    function ($rootScope, $scope, $window, $stateParams, AppUtil, toastr, ClusterService, DcService, NgTableParams, ClusterType) {
+angular
+    .module('index')
+    .controller('ClusterListCtl', ClusterListCtl);
 
-        $rootScope.currentNav = '1-2';
-        $scope.dcs = {};
-        $scope.clusterId = $stateParams.clusterId;
-        $scope.clusterName = $stateParams.clusterName;
-        $scope.containerId = $stateParams.keepercontainer;
-        $scope.getClusterActiveDc = getClusterActiveDc;
-        $scope.getTypeName = getTypeName;
-        $scope.preDeleteCluster = preDeleteCluster;
-        $scope.deleteCluster = deleteCluster;
-        $scope.preResetClusterStatus = preResetClusterStatus;
-        $scope.resetClusterStatus = resetClusterStatus;
-        $scope.showUnhealthyClusterOnly = false;
-        $scope.showErrorMigratingClusterOnly = false;
-        $scope.dcName = $stateParams.dcName;
-        $scope.type = $stateParams.type;
-        $scope.clusterTypes = ClusterType.selectData()
+ClusterListCtl.$inject = ['$rootScope', '$scope', '$window', '$stateParams', 'AppUtil',
+    'toastr', 'ClusterService','DcService', 'NgTableParams', 'ClusterType'];
 
-        $scope.sourceClusters = [];
-        if($scope.clusterName) {
-        	ClusterService.load_cluster($scope.clusterName)
-            .then(function (data) {
-                loadTable([data])
-            });
+function ClusterListCtl($rootScope, $scope, $window, $stateParams, AppUtil,
+                        toastr, ClusterService, DcService, NgTableParams, ClusterType) {
+
+    $rootScope.currentNav = '1-2';
+    $scope.dcs = {};
+    $scope.clusterId = $stateParams.clusterId;
+    $scope.clusterName = $stateParams.clusterName;
+    $scope.containerId = $stateParams.keepercontainer;
+    $scope.getClusterActiveDc = getClusterActiveDc;
+    $scope.getTypeName = getTypeName;
+    $scope.preDeleteCluster = preDeleteCluster;
+    $scope.deleteCluster = deleteCluster;
+    $scope.preResetClusterStatus = preResetClusterStatus;
+    $scope.resetClusterStatus = resetClusterStatus;
+    $scope.showUnhealthyClusterOnly = false;
+    $scope.showErrorMigratingClusterOnly = false;
+    $scope.dcName = $stateParams.dcName;
+    $scope.type = $stateParams.type;
+    $scope.clusterTypes = ClusterType.selectData()
+
+    $scope.sourceClusters = [];
+    if($scope.clusterName) {
+    	ClusterService.load_cluster($scope.clusterName)
+        .then(function (data) {
+            loadTable([data])
+        });
+    }
+    else if ($scope.dcName){
+        if ($scope.type === "activeDC"){
+            showClustersByActiveDc($scope.dcName);
+        }else if ($scope.type === "bindDC"){
+            showClustersBindDc($scope.dcName);
         }
-        else if ($scope.dcName){
+    }
+    else if ($scope.containerId) {
+        showClustersByContainer($scope.containerId)
+    }
+    else {
+        showClusters();
+    }
+
+    DcService.loadAllDcs()
+    	.then(function(data) {
+    		for(var i = 0 ; i < data.length; ++i) {
+    			var dc = data[i];
+    			$scope.dcs[dc.id] = dc.dcName;
+    		}
+    	});
+
+
+    function getClusterActiveDc(cluster) {
+        var clusterType = ClusterType.lookup(cluster.clusterType)
+        if (clusterType && clusterType.multiActiveDcs) {
+            return "-"
+        }
+
+        return $scope.dcs[cluster.activedcId] || "Unbind";
+    }
+
+    function getTypeName(type) {
+        var clusterType = ClusterType.lookup(type)
+        if (clusterType) return clusterType.name
+        else return '未知类型'
+    }
+
+    function preDeleteCluster(clusterName) {
+    	$scope.clusterName = clusterName;
+		$('#deleteClusterConfirm').modal('show');
+	}
+	function deleteCluster() {
+		ClusterService.deleteCluster($scope.clusterName)
+			.then(function (result) {
+				$('#deleteClusterConfirm').modal('hide');
+				toastr.success('删除成功');
+				setTimeout(function () {
+					// TODO [marsqing] reload ng-table instead of reload window
+					$window.location.reload();
+				},1000);
+	         }, function (result) {
+				toastr.error(AppUtil.errorMsg(result), '删除失败');
+			})
+		}
+
+    function preResetClusterStatus(clusterName, clusterId) {
+        $scope.clusterId = clusterId;
+        $scope.clusterName = clusterName;
+        console.log($scope);
+        console.log(clusterName, clusterId);
+        $('#resetClusterStatusConfirm').modal('show');
+    }
+    function resetClusterStatus() {
+        ClusterService.resetClusterStatus($scope.clusterId)
+            .then(function (result) {
+                $('#resetClusterStatusConfirm').modal('hide');
+                toastr.success('重置成功');
+                setTimeout(function () {
+                    // TODO [marsqing] reload ng-table instead of reload window
+                    $window.location.reload();
+                },1000);
+            }, function (result) {
+                toastr.error(AppUtil.errorMsg(result), '重置失败');
+            })
+    }
+
+    $scope.refresh = function() {
+        showClusters();
+    }
+
+    function showClusters() {
+        if ($scope.showUnhealthyClusterOnly === true) {
+            showUnhealthyClusters();
+        } else if ($scope.showErrorMigratingClusterOnly === true){
+            showErrorMigratingClusters();
+        } else if ($scope.dcName){
             if ($scope.type === "activeDC"){
                 showClustersByActiveDc($scope.dcName);
             }else if ($scope.type === "bindDC"){
@@ -36,133 +129,45 @@ index_module.controller('ClusterListCtl', ['$rootScope', '$scope', '$window', '$
             showClustersByContainer($scope.containerId)
         }
         else {
-            showClusters();
+            showAllClusters();
         }
-        
-        DcService.loadAllDcs()
-        	.then(function(data) {
-        		for(var i = 0 ; i < data.length; ++i) {
-        			var dc = data[i];
-        			$scope.dcs[dc.id] = dc.dcName;
-        		}
-        	});
+    }
 
+    function showUnhealthyClusters() {
+        ClusterService.getUnhealthyClusters()
+            .then(loadTable);
+    }
 
-        function getClusterActiveDc(cluster) {
-            var clusterType = ClusterType.lookup(cluster.clusterType)
-            if (clusterType && clusterType.multiActiveDcs) {
-                return "-"
-            }
+    function showErrorMigratingClusters() {
+        ClusterService.getErrorMigratingClusters()
+            .then(loadTable);
+    }
 
-            return $scope.dcs[cluster.activedcId] || "Unbind";
-        }
+    function showAllClusters() {
+        ClusterService.findAllClusters()
+            .then(loadTable);
+    }
 
-        function getTypeName(type) {
-            var clusterType = ClusterType.lookup(type)
-            if (clusterType) return clusterType.name
-            else return '未知类型'
-        }
-        
-        function preDeleteCluster(clusterName) {
-        	$scope.clusterName = clusterName;
-			$('#deleteClusterConfirm').modal('show');
-		}
-		function deleteCluster() {
-			ClusterService.deleteCluster($scope.clusterName)
-				.then(function (result) {
-					$('#deleteClusterConfirm').modal('hide');
-					toastr.success('删除成功');
-					setTimeout(function () {
-						// TODO [marsqing] reload ng-table instead of reload window
-						$window.location.reload();
-					},1000);
-		         }, function (result) {
-					toastr.error(AppUtil.errorMsg(result), '删除失败');
-				})
-			}
+    function showClustersBindDc(dcName) {
+        ClusterService.findClustersByDcNameBind(dcName).then(loadTable);
+    }
 
-        function preResetClusterStatus(clusterName, clusterId) {
-            $scope.clusterId = clusterId;
-            $scope.clusterName = clusterName;
-            console.log($scope);
-            console.log(clusterName, clusterId);
-            $('#resetClusterStatusConfirm').modal('show');
-        }
-        function resetClusterStatus() {
-            ClusterService.resetClusterStatus($scope.clusterId)
-                .then(function (result) {
-                    $('#resetClusterStatusConfirm').modal('hide');
-                    toastr.success('重置成功');
-                    setTimeout(function () {
-                        // TODO [marsqing] reload ng-table instead of reload window
-                        $window.location.reload();
-                    },1000);
-                }, function (result) {
-                    toastr.error(AppUtil.errorMsg(result), '重置失败');
-                })
-        }
+    function showClustersByActiveDc(dcName) {
+        ClusterService.findClustersByDcName(dcName).then(loadTable);
+    }
 
-        $scope.refresh = function() {
-            showClusters();
-        }
+    function showClustersByContainer(containerId) {
+        ClusterService.findAllByKeeperContainer(containerId).then(loadTable);
+    }
 
-        function showClusters() {
-            if ($scope.showUnhealthyClusterOnly === true) {
-                showUnhealthyClusters();
-            } else if ($scope.showErrorMigratingClusterOnly === true){
-                showErrorMigratingClusters();
-            } else if ($scope.dcName){
-                if ($scope.type === "activeDC"){
-                    showClustersByActiveDc($scope.dcName);
-                }else if ($scope.type === "bindDC"){
-                    showClustersBindDc($scope.dcName);
-                }
-            }
-            else if ($scope.containerId) {
-                showClustersByContainer($scope.containerId)
-            }
-            else {
-                showAllClusters();
-            }
-        }
-
-        function showUnhealthyClusters() {
-            ClusterService.getUnhealthyClusters()
-                .then(loadTable);
-        }
-
-        function showErrorMigratingClusters() {
-            ClusterService.getErrorMigratingClusters()
-                .then(loadTable);
-        }
-
-        function showAllClusters() {
-            ClusterService.findAllClusters()
-                .then(loadTable);
-        }
-
-        function showClustersBindDc(dcName) {
-            ClusterService.findClustersByDcNameBind(dcName).then(loadTable);
-        }
-
-        function showClustersByActiveDc(dcName) {
-            ClusterService.findClustersByDcName(dcName).then(loadTable);
-        }
-
-        function showClustersByContainer(containerId) {
-            ClusterService.findAllByKeeperContainer(containerId).then(loadTable);
-        }
-
-        function loadTable(data) {
-            $scope.sourceClusters = data;
-            $scope.tableParams = new NgTableParams({
-                page : 1,
-                count : 10
-            }, {
-                filterDelay:100,
-                dataset: $scope.sourceClusters,
-            });
-        }
-
-
-    }]);
+    function loadTable(data) {
+        $scope.sourceClusters = data;
+        $scope.tableParams = new NgTableParams({
+            page : 1,
+            count : 10
+        }, {
+            filterDelay:100,
+            dataset: $scope.sourceClusters,
+        });
+    }
+}
--- redis/redis-console/src/main/resources/static/scripts/controllers/ShardListCtl.js
+++ redis/redis-console/src/main/resources/static/scripts/controllers/ShardListCtl.js
@@ -1,71 +1,76 @@
-index_module.controller('ShardListCtl', ['$rootScope', '$scope', '$window', '$stateParams', 'AppUtil', 'toastr', 'ClusterService','DcService', 'NgTableParams', 'ClusterType',
-    function ($rootScope, $scope, $window, $stateParams, AppUtil, toastr, ClusterService, DcService, NgTableParams, ClusterType) {
+angular
+    .module('index')
+    .controller('ShardListCtl', ShardListCtl);
 
-        $rootScope.currentNav = '1-2';
-        $scope.dcs = {};
-        $scope.getActiveDcName = getActiveDcName;
-        $scope.getTypeName = getTypeName
-        $scope.showUnhealthyShardOnly = true;
-        $scope.clusterTypes = ClusterType.selectData()
+ShardListCtl.$inject = ['$rootScope', '$scope', '$window', '$stateParams', 'AppUtil',
+    'toastr', 'ClusterService','DcService', 'NgTableParams', 'ClusterType'];
 
-        $scope.sourceClusters = [];
+function ShardListCtl($rootScope, $scope, $window, $stateParams, AppUtil,
+                      toastr, ClusterService, DcService, NgTableParams, ClusterType) {
+
+    $rootScope.currentNav = '1-2';
+    $scope.dcs = {};
+    $scope.getActiveDcName = getActiveDcName;
+    $scope.getTypeName = getTypeName
+    $scope.showUnhealthyShardOnly = true;
+    $scope.clusterTypes = ClusterType.selectData()
+
+    $scope.sourceClusters = [];
+    showClusters();
+
+    DcService.loadAllDcs()
+        .then(function(data) {
+            for(var i = 0 ; i < data.length; ++i) {
+                var dc = data[i];
+                $scope.dcs[dc.id] = dc.dcName;
+            }
+        });
+
+    function getActiveDcName(shard) {
+        var clusterType = ClusterType.lookup(shard.clusterType)
+        if (clusterType && clusterType.multiActiveDcs) {
+            return "-"
+        }
+        return $scope.dcs[shard.activedcId] || "Unbind";
+    }
+
+    function getTypeName(type) {
+        var clusterType = ClusterType.lookup(type)
+        if (clusterType) return clusterType.name
+        else return '未知类型'
+    }
+
+    $scope.refresh = function() {
         showClusters();
+    }
 
-        DcService.loadAllDcs()
-            .then(function(data) {
-                for(var i = 0 ; i < data.length; ++i) {
-                    var dc = data[i];
-                    $scope.dcs[dc.id] = dc.dcName;
-                }
-            });
-
-        function getActiveDcName(shard) {
-            var clusterType = ClusterType.lookup(shard.clusterType)
-            if (clusterType && clusterType.multiActiveDcs) {
-                return "-"
-            }
-            return $scope.dcs[shard.activedcId] || "Unbind";
+    function showClusters() {
+        if ($scope.showUnhealthyShardOnly) {
+            showUnhealthyShards()
         }
-
-        function getTypeName(type) {
-            var clusterType = ClusterType.lookup(type)
-            if (clusterType) return clusterType.name
-            else return '未知类型'
+        else {
+            showAllClusters();
         }
+    }
 
-        $scope.refresh = function() {
-            showClusters();
-        }
+    function showUnhealthyShards() {
+        ClusterService.getUnhealthyShards()
+            .then(loadTable);
+    }
 
-        function showClusters() {
-            if ($scope.showUnhealthyShardOnly) {
-                showUnhealthyShards()
-            }
-            else {
-                showAllClusters();
-            }
-        }
+    function showAllClusters() {
+        ClusterService.findAllClusters()
+            .then(loadTable);
+    }
 
-        function showUnhealthyShards() {
-            ClusterService.getUnhealthyShards()
-                .then(loadTable);
-        }
-
-        function showAllClusters() {
-            ClusterService.findAllClusters()
-                .then(loadTable);
-        }
-
-        function loadTable(data) {
-            $scope.sourceClusters = data;
-            $scope.tableParams = new NgTableParams({
-                page : 1,
-                count : 10
-            }, {
-                filterDelay:100,
-                dataset: $scope.sourceClusters,
-            });
-        }
-
-
-    }]);
+    function loadTable(data) {
+        $scope.sourceClusters = data;
+        $scope.tableParams = new NgTableParams({
+            page : 1,
+            count : 10
+        }, {
+            filterDelay:100,
+            dataset: $scope.sourceClusters,
+        });
+    }
+}
