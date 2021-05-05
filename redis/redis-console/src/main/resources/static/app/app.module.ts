8faa8b6363d9d82d6cdde72fb86773865c105e0f
wcc吴骋成 <wucc@trip.com>
2021-05-05 16:22:45 +0800

    constructing cluster.list.component, not used

--- redis/redis-console/src/main/resources/static/app/app.module.ts
+++ redis/redis-console/src/main/resources/static/app/app.module.ts
@@ -1,17 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { UpgradeModule, setAngularJSGlobal } from '@angular/upgrade/static';
+import { UpgradeModule, setAngularJSGlobal, downgradeComponent } from '@angular/upgrade/static';
 
 import * as angular from 'angular'; setAngularJSGlobal(angular);
 import 'zone.js/dist/zone';
 import 'core-js'
 
-import '../scripts/app';
+import { UIRouterUpgradeModule } from '@uirouter/angular-hybrid';
+import { UIRouterModule } from '@uirouter/angular';
+
+import '../scripts/app.angularjs.module';
+
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+
+import { ClusterListComponent } from "./cluster.list.component";
+
+angular
+    .module('directive')
+    .directive(
+        'clusterList',
+        downgradeComponent({ component: ClusterListComponent }) as angular.IDirectiveFactory
+    );
 
 @NgModule({
-    imports: [ BrowserModule, UpgradeModule ]
+    imports: [
+        BrowserModule,
+        UpgradeModule,
+        UIRouterUpgradeModule,
+        Ng2SmartTableModule,
+    ],
+    declarations: [
+        ClusterListComponent
+    ],
+    entryComponents: [
+        ClusterListComponent
+    ]
 })
-
 export class AppModule {
     constructor(private upgrade: UpgradeModule) { }
 
