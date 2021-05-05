4e17d12de31fa2db40aa4eb3a8bc937c42a0b31c
lishanglin <sl_li@trip.com>
2021-05-05 14:33:05 +0800

    fix migration state hang on processing PartialSuccessState serially

--- redis/redis-console/src/test/java/com/ctrip/xpipe/redis/console/migration/status/migration/MigrationPartialSuccessStateTest.java
+++ redis/redis-console/src/test/java/com/ctrip/xpipe/redis/console/migration/status/migration/MigrationPartialSuccessStateTest.java
@@ -1,13 +1,18 @@
 package com.ctrip.xpipe.redis.console.migration.status.migration;
 
+import com.ctrip.xpipe.redis.console.migration.model.MigrationShard;
+import com.ctrip.xpipe.redis.console.migration.model.ShardMigrationResult;
+import com.ctrip.xpipe.redis.console.migration.model.ShardMigrationStep;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static org.mockito.Matchers.isA;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 /**
  * @author wenchao.meng
@@ -27,15 +32,50 @@ public class MigrationPartialSuccessStateTest extends AbstractMigrationStateTest
     }
 
     @Test
-    public void testSuccessAction(){
+    public void testAlreadySuccessAction(){
 
         shardResult(0);
 
         partialSuccessState.getStateActionState().tryAction();
 
-        partialSuccessState.refresh();
-        partialSuccessState.refresh();
+        verify(migrationCluster).updateStat(isA(MigrationPublishState.class));
+        verify(migrationCluster).process();
+    }
 
+    @Test
+    public void testSingleThreadProcess() throws Exception {
+        // single-thread executor with CallerRunsPolicy will run serially eventually
+        when(migrationCluster.getMigrationExecutor()).thenReturn(new ThreadPoolExecutor(1, 1,
+                0L, TimeUnit.MILLISECONDS,
+                new ArrayBlockingQueue<>(1), new ThreadPoolExecutor.CallerRunsPolicy()));
+
+        int failCnt = 4;
+        shardResult(failCnt);
+        AtomicInteger retryCnt = new AtomicInteger(0);
+
+        for (MigrationShard migrationShard : migrationCluster.getMigrationShards()) {
+            ShardMigrationResult result = migrationShard.getShardMigrationResult();
+            doAnswer(invocation -> {
+                ShardMigrationStep step = invocation.getArgumentAt(0, ShardMigrationStep.class);
+                when(result.stepTerminated(step)).thenReturn(false);
+                when(result.stepTerminated(step)).thenReturn(false);
+                return null;
+            }).when(result).stepRetry(any(ShardMigrationStep.class));
+
+            doAnswer(invocation -> {
+                ShardMigrationResult newResult = mock(ShardMigrationResult.class);
+                when(migrationShard.getShardMigrationResult()).thenReturn(newResult);
+                when(newResult.stepTerminated(any())).thenReturn(true);
+                when(newResult.stepSuccess(any())).thenReturn(true);
+                partialSuccessState.refresh();
+                retryCnt.incrementAndGet();
+                return null;
+            }).when(migrationShard).doMigrate();
+        }
+
+        partialSuccessState.getStateActionState().tryAction();
+
+        waitConditionUntilTimeOut(() -> retryCnt.get() >= failCnt);
         verify(migrationCluster).updateStat(isA(MigrationPublishState.class));
         verify(migrationCluster).process();
     }
