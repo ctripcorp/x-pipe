23ac0e784af9ffbf72bcf843070cd225282729cc
wcc吴骋成 <wucc@trip.com>
2021-05-13 18:29:17 +0800

    XpipeMetaManager is now read write safe

---
+++ redis/redis-core/src/main/java/com/ctrip/xpipe/redis/core/meta/ReadWriteSafe.java
@@ -0,0 +1,41 @@
+package com.ctrip.xpipe.redis.core.meta;
+
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.function.Supplier;
+
+/**
+ * @author Slight
+ * <p>
+ * May 13, 2021 4:52 PM
+ */
+public interface ReadWriteSafe {
+
+    ReadWriteLock getReadWriteLock();
+
+    default <T> T read(Supplier<T> supplier) {
+        getReadWriteLock().readLock().lock();
+        try {
+            return supplier.get();
+        } finally {
+            getReadWriteLock().readLock().unlock();
+        }
+    }
+
+    default void write(Runnable runnable) {
+        getReadWriteLock().writeLock().lock();
+        try {
+            runnable.run();
+        } finally {
+            getReadWriteLock().writeLock().unlock();
+        }
+    }
+
+    default <T> T write(Supplier<T> supplier) {
+        getReadWriteLock().writeLock().lock();
+        try {
+            return supplier.get();
+        } finally {
+            getReadWriteLock().writeLock().unlock();
+        }
+    }
+}
