cc7848f4350491f7853b50cabc24c4aa697f09b5
md_li <md_li@trip.com>
2021-05-30 10:27:58 +0800

    add upgrade_jdk.sh

---
+++ redis/redis-proxy/src/test/resources/env_set/upgrade_jdk.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+yum remove jdk11
+
+rpm -i http://git.dev.sh.ctripcorp.com/baseimage/components/raw/master/jdk11-0.4-1.x86_64.rpm
+
+/usr/java/jdk11/bin/java -version
+
+# enable app bind port at 80 and 443
+if [[ -z "$JAVA_HOME" && -d /usr/java/jdk11/ ]]; then
+	setcap 'cap_net_bind_service=+ep' /usr/java/jdk11/bin/java
+elif [[ -z "$JAVA_HOME" && -d /usr/java/latest/ ]]; then
+	setcap 'cap_net_bind_service=+ep' /usr/java/latest/bin/java
+elif [[ -n "$JAVA_HOME" ]]; then
+	setcap 'cap_net_bind_service=+ep' $JAVA_HOME/bin/java
+fi
+
+touch /etc/ld.so.conf.d/java.conf
+
+sed -i '1,$d' /etc/ld.so.conf.d/java.conf
+
+echo '/usr/java/jdk11/lib/jli/' >> /etc/ld.so.conf.d/java.conf
+
+ldconfig | grep libjli
+
+/usr/java/jdk11/bin/java -version
