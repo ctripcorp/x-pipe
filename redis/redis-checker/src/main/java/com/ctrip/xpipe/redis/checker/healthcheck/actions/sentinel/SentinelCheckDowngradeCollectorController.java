1759b0636136243ed41baba6a3cdb057ecc2bfef
llj李龙姣 <lilj@trip.com>
2021-05-30 12:25:57 +0800

    add transaction log for sentinel check notify

--- redis/redis-checker/src/main/java/com/ctrip/xpipe/redis/checker/healthcheck/actions/sentinel/SentinelCheckDowngradeCollectorController.java
+++ redis/redis-checker/src/main/java/com/ctrip/xpipe/redis/checker/healthcheck/actions/sentinel/SentinelCheckDowngradeCollectorController.java
@@ -1,5 +1,7 @@
 package com.ctrip.xpipe.redis.checker.healthcheck.actions.sentinel;
 
+import com.ctrip.xpipe.api.monitor.Task;
+import com.ctrip.xpipe.api.monitor.TransactionMonitor;
 import com.ctrip.xpipe.redis.checker.healthcheck.HealthCheckAction;
 import com.ctrip.xpipe.redis.checker.healthcheck.OneWaySupport;
 import com.ctrip.xpipe.redis.checker.healthcheck.RedisHealthCheckInstance;
@@ -11,6 +13,8 @@ import com.ctrip.xpipe.redis.core.entity.XpipeMeta;
 import com.ctrip.xpipe.redis.core.meta.MetaCache;
 import com.ctrip.xpipe.utils.StringUtil;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static com.ctrip.xpipe.redis.checker.healthcheck.actions.sentinel.SentinelHelloCheckAction.LOG_TITLE;
@@ -36,29 +40,43 @@ public class SentinelCheckDowngradeCollectorController extends AbstractAggregati
 
     @Override
     public void onAction(SentinelActionContext context) {
+        TransactionMonitor transaction = TransactionMonitor.DEFAULT;
         RedisInstanceInfo info = context.instance().getCheckInfo();
-        if (!info.getClusterId().equalsIgnoreCase(clusterId) || !info.getShardId().equalsIgnoreCase(shardId)) return;
-        if (!shouldCheckFromRedis(context.instance())) return;
+        transaction.logTransactionSwallowException("sentinel.check.notify", info.getClusterId(), new Task() {
+            @Override
+            public void go() throws Exception {
+                if (!info.getClusterId().equalsIgnoreCase(clusterId) || !info.getShardId().equalsIgnoreCase(shardId))
+                    return;
+                if (!shouldCheckFromRedis(context.instance())) return;
 
-        // only deal with success result when downgrade
-        if (!context.isFail() && info.isInActiveDc() && activeDcCollected.compareAndSet(false, true)) {
-            logger.info("[{}-{}+{}]active dc {} redis {} sub finish", LOG_TITLE, clusterId, shardId, info.getDcId(), info.getHostPort());
-            handleAllActiveDcHellos(context.instance(), context.getResult());
-            return;
-        }
+                // only deal with success result when downgrade
+                if (!context.isFail() && info.isInActiveDc() && activeDcCollected.compareAndSet(false, true)) {
+                    logger.info("[{}-{}+{}]active dc {} redis {} sub finish", LOG_TITLE, clusterId, shardId, info.getDcId(), info.getHostPort());
+                    handleAllActiveDcHellos(context.instance(), context.getResult());
+                    return;
+                }
 
-        // handle backup dc hello when all right
-        if (info.isInActiveDc()) return;
-        if (collectHello(context) >= countBackDcRedis()) {
-            if (checkFinishedInstance.size() == checkFailInstance.size()) {
-                logger.warn("[{}-{}+{}]backup dc {} all sub failed, try to sub from active dc", LOG_TITLE, clusterId, shardId, info.getDcId());
-                beginDowngrade();
-                return;
+                // handle backup dc hello when all right
+                if (info.isInActiveDc()) return;
+                if (collectHello(context) >= countBackDcRedis()) {
+                    if (checkFinishedInstance.size() == checkFailInstance.size()) {
+                        logger.warn("[{}-{}+{}]backup dc {} all sub failed, try to sub from active dc", LOG_TITLE, clusterId, shardId, info.getDcId());
+                        beginDowngrade();
+                        return;
+                    }
+                    needDowngrade.compareAndSet(true, false);
+                    logger.debug("[{}-{}+{}]backup dc {} sub finish", LOG_TITLE, clusterId, shardId, info.getDcId());
+                    handleAllBackupDcHellos(context.instance());
+                }
             }
-            needDowngrade.compareAndSet(true, false);
-            logger.debug("[{}-{}+{}]backup dc {} sub finish", LOG_TITLE, clusterId, shardId, info.getDcId());
-            handleAllBackupDcHellos(context.instance());
-        }
+
+            @Override
+            public Map<String, Object> getData() {
+                Map<String, Object> transactionData = new HashMap<>();
+                transactionData.put("checkRedisInstances", info);
+                return transactionData;
+            }
+        });
     }
 
     @Override
