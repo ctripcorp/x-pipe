6d66c56f5e9cca255ae0199449337087d2c0484e
lishanglin <sl_li@trip.com>
2021-06-24 19:07:52 +0800

    fix model deserialize fail

--- redis/redis-console/src/test/java/com/ctrip/xpipe/redis/console/model/consoleportal/UnhealthyInfoModelTest.java
+++ redis/redis-console/src/test/java/com/ctrip/xpipe/redis/console/model/consoleportal/UnhealthyInfoModelTest.java
@@ -1,10 +1,13 @@
 package com.ctrip.xpipe.redis.console.model.consoleportal;
 
+import com.ctrip.xpipe.api.codec.Codec;
 import com.ctrip.xpipe.endpoint.HostPort;
 import com.ctrip.xpipe.redis.console.AbstractConsoleTest;
 import com.google.common.collect.Sets;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Collections;
 
 public class UnhealthyInfoModelTest extends AbstractConsoleTest {
 
@@ -29,6 +32,19 @@ public class UnhealthyInfoModelTest extends AbstractConsoleTest {
         Assert.assertEquals(Sets.newHashSet(new UnhealthyInfoModel.RedisHostPort(new HostPort("127.0.0.1", 6379), true)
                 , new UnhealthyInfoModel.RedisHostPort(new HostPort("127.0.0.2", 6379), false)),
                 origin.getUnhealthyInstance().get("cluster1").get(new UnhealthyInfoModel.DcShard("jq", "shard1")));
+    }
+
+    @Test
+    public void testModelSerialize() {
+        UnhealthyInfoModel model = new UnhealthyInfoModel();
+        model.addUnhealthyInstance("cluster1", "jq", "shard1", new HostPort("127.0.0.1", 6379), true);
+        String json = Codec.DEFAULT.encode(model);
+        UnhealthyInfoModel data = Codec.DEFAULT.decode(json, UnhealthyInfoModel.class);
+
+        logger.info("[testModelSerialize] model {}", Codec.DEFAULT.encode(data));
+        Assert.assertEquals(Collections.singleton("cluster1"), data.getUnhealthyClusterNames());
+        Assert.assertEquals(Collections.singleton(new UnhealthyInfoModel.RedisHostPort(new HostPort("127.0.0.1", 6379), true)),
+                data.getUnhealthyInstance().get("cluster1").get(new UnhealthyInfoModel.DcShard("jq", "shard1")));
     }
 
 }
