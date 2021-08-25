31fc690c4e5c78ceb16d43df3ab8bc3d9492ae71
phyxdown <phyxdown@qq.com>
2021-08-25 16:01:01 +0800

    use stubbing instead of simulating

--- redis/redis-keeper/src/test/java/com/ctrip/xpipe/redis/keeper/store/meta/DefaultMetaStoreTest.java
+++ redis/redis-keeper/src/test/java/com/ctrip/xpipe/redis/keeper/store/meta/DefaultMetaStoreTest.java
@@ -1,17 +1,15 @@
 package com.ctrip.xpipe.redis.keeper.store.meta;
 
 import com.ctrip.xpipe.AbstractTest;
-import com.ctrip.xpipe.redis.core.meta.KeeperState;
 import com.ctrip.xpipe.redis.core.protocal.protocal.EofMarkType;
-import com.ctrip.xpipe.redis.core.protocal.protocal.EofType;
+import com.ctrip.xpipe.redis.core.store.ReplicationStoreMeta;
 import com.ctrip.xpipe.redis.keeper.exception.replication.UnexpectedReplIdException;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 
-import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  * @author chen.zhu
@@ -22,10 +20,15 @@ public class DefaultMetaStoreTest extends AbstractTest {
 
 
     @Test (expected = UnexpectedReplIdException.class)
-    public void fixPsync0() throws IOException {
+    public void fixPsync0MakeSureReplIdsAreSame() throws IOException {
 
-        DefaultMetaStore metaStore = new DefaultMetaStore(new File("/tmp/xpipe/test"), "20180118165046194-20180118165046194-294c90b4c9ed4d747a77b1b0f22ec28a8068013b");
+        DefaultMetaStore metaStore = spy(new DefaultMetaStore(new File("/tmp/xpipe/test"), "20180118165046194-20180118165046194-294c90b4c9ed4d747a77b1b0f22ec28a8068013b"));
         metaStore.becomeActive();
-        metaStore.checkReplIdAndUpdateRdbInfo("rdb_1620671301121_e67222d2-eee1-48c4-bde7-5c6d37734ca4", new EofMarkType("94480e125b6ebb54dc7b9eae7b9c8ea00aeed56e"), 572767153, null);
+
+        ReplicationStoreMeta meta = mock(ReplicationStoreMeta.class);
+        when(meta.getReplId()).thenReturn("ReplId A");
+
+        doReturn(meta).when(metaStore).dupReplicationStoreMeta();
+        metaStore.checkReplIdAndUpdateRdbInfo("rdb_1620671301121_e67222d2-eee1-48c4-bde7-5c6d37734ca4", new EofMarkType("94480e125b6ebb54dc7b9eae7b9c8ea00aeed56e"), 572767153, "ReplId B");
     }
 }
