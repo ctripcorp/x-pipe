0a42badba23d5d47c9a7f735c6b5373bd49e0a0d
lishanglin <sl_li@trip.com>
2021-09-22 14:00:52 +0800

    fix console start fail

--- redis/package/redis-metaserver-package/src/main/scripts/startup.sh
+++ redis/package/redis-metaserver-package/src/main/scripts/startup.sh
@@ -200,7 +200,7 @@ declare -i max_counter=16 # 16*5=80s
 declare -i total_time=0
 
 printf "Waiting for server startup" >> $STARTUP_LOG
-until [[ (( counter -ge max_counter )) || "$(curl -X GET --silent --connect-timeout 1 --head $SERVER_URL | grep "Coyote")" != "" ]];
+until [[ (( counter -ge max_counter )) || "$(curl -X GET --silent --connect-timeout 1 --head $SERVER_URL/health | grep "HTTP/1.1 200")" != "" ]];
 do
     printf "." >> $STARTUP_LOG
     counter+=1
