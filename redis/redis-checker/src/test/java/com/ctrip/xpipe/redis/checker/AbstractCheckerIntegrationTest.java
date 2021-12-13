4a3e873615fec75b39dd6b6bc16acf9df622a906
lishanglin <sl_li@trip.com>
2021-12-13 21:54:18 +0800

    fix velocity

--- redis/redis-checker/src/test/java/com/ctrip/xpipe/redis/checker/AbstractCheckerIntegrationTest.java
+++ redis/redis-checker/src/test/java/com/ctrip/xpipe/redis/checker/AbstractCheckerIntegrationTest.java
@@ -23,6 +23,7 @@ import com.ctrip.xpipe.redis.core.spring.AbstractRedisConfigContext;
 import com.ctrip.xpipe.utils.OsUtils;
 import com.ctrip.xpipe.utils.XpipeThreadFactory;
 import com.google.common.util.concurrent.MoreExecutors;
+import org.apache.velocity.app.VelocityEngine;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -32,6 +33,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Properties;
 import java.util.concurrent.*;
 
 import static com.ctrip.xpipe.redis.checker.resource.Resource.*;
@@ -57,6 +59,14 @@ public class AbstractCheckerIntegrationTest extends AbstractCheckerTest {
     @SpringBootApplication
     @ConditionalOnProperty(name = { CHECKER_TEST })
     public static class CheckerTestConfig extends AbstractRedisConfigContext {
+
+        @Bean
+        public VelocityEngine getVelocityEngine() {
+            Properties props = new Properties();
+            props.put("resource.loader", "class");
+            props.put("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+            return new VelocityEngine(props);
+        }
 
         @Bean
         public MetaCache metaCache() {
