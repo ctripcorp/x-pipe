ecfa7dd3a47454efddf8fce57985eec36dab3d81
lishanglin <sl_li@Ctrip.com>
2021-12-15 19:42:30 +0800

    update log4j 2.15.0 (#497)
    * update log4j version
    
    * update org.springframework.boot version
    
    * update clog version
    
    * update spring logging version
    
    * update springboot 1.4.3 -> 1.5.0  spring 4.3.4 -> 4.3.6
    
    * distinguish uat env keeper with different jvm param by idc
    
    * fix velocity
    
    * fix tag miss
    
    Co-authored-by: 周国栋 <zhouguodong@zhouguodongdeMacBook-Pro.local>
    Co-authored-by: lishanglin <sl_li@trip.com>

--- redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/spring/CustomizedConfig.java
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/spring/CustomizedConfig.java
@@ -3,7 +3,7 @@ package com.ctrip.xpipe.redis.console.spring;
 import com.ctrip.xpipe.redis.core.spring.AbstractRedisConfigContext;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
