7d30fb5b0fd4b332320e88b37953a2ac93198ffa
llj李龙姣 <lilj@trip.com>
2022-02-17 19:19:36 +0800

    add log

--- redis/redis-checker/src/main/java/com/ctrip/xpipe/redis/checker/healthcheck/actions/sentinel/controller/CrossDcSentinelCheckController.java
+++ redis/redis-checker/src/main/java/com/ctrip/xpipe/redis/checker/healthcheck/actions/sentinel/controller/CrossDcSentinelCheckController.java
@@ -3,14 +3,21 @@ package com.ctrip.xpipe.redis.checker.healthcheck.actions.sentinel.controller;
 import com.ctrip.xpipe.redis.checker.healthcheck.CrossDcSupport;
 import com.ctrip.xpipe.redis.checker.healthcheck.RedisHealthCheckInstance;
 import com.ctrip.xpipe.redis.checker.healthcheck.actions.sentinel.SentinelActionController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import static com.ctrip.xpipe.redis.checker.healthcheck.actions.sentinel.SentinelHelloCheckAction.LOG_TITLE;
 
 @Component
 public class CrossDcSentinelCheckController implements SentinelActionController, CrossDcSupport {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @Override
     public boolean shouldCheck(RedisHealthCheckInstance instance) {
-       return !instance.getCheckInfo().isMaster();
+        logger.info("[{}][shouldCheck]{}", LOG_TITLE, instance.toString());
+        return !instance.getCheckInfo().isMaster();
     }
 
 }
