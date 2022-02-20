267d77eed04473b40f1f678927da22a5db1e38ba
phyxdown <phyxdown@qq.com>
2022-02-20 17:43:01 +0800

    1. ApplierRedisCommand on RedisOp 2. SequenceCommand waits for several SequenceCommands 3. workerThreads & stateThread of SequenceCommand

--- redis/redis-keeper/src/main/java/com/ctrip/xpipe/redis/keeper/applier/command/ApplierRedisCommand.java
+++ redis/redis-keeper/src/main/java/com/ctrip/xpipe/redis/keeper/applier/command/ApplierRedisCommand.java
@@ -1,8 +1,13 @@
 package com.ctrip.xpipe.redis.keeper.applier.command;
 
 import com.ctrip.xpipe.api.command.Command;
-import com.ctrip.xpipe.api.command.CommandFuture;
-import com.ctrip.xpipe.redis.keeper.applier.client.ApplierRedisClient;
+import com.ctrip.xpipe.redis.core.redis.operation.RedisKey;
+import com.ctrip.xpipe.redis.core.redis.operation.RedisMultiKeyOp;
+import com.ctrip.xpipe.redis.core.redis.operation.RedisOp;
+import com.ctrip.xpipe.redis.core.redis.operation.RedisSingleKeyOp;
+import com.google.common.collect.Lists;
+
+import java.util.List;
 
 /**
  * @author Slight
@@ -11,7 +16,17 @@ import com.ctrip.xpipe.redis.keeper.applier.client.ApplierRedisClient;
  */
 public interface ApplierRedisCommand<V> extends Command<V> {
 
-    byte[] key();
+    RedisOp redisOp();
 
-    CommandFuture<V> apply(ApplierRedisClient client);
+    default List<RedisKey> keys() {
+        RedisOp op = redisOp();
+        if (op instanceof RedisMultiKeyOp) {
+            return ((RedisMultiKeyOp<?>) op).getKeys();
+        }
+        if (op instanceof RedisSingleKeyOp) {
+            RedisKey key = ((RedisSingleKeyOp<?>) op).getKey();
+            return Lists.newArrayList(key);
+        }
+        return Lists.newArrayList();
+    }
 }
