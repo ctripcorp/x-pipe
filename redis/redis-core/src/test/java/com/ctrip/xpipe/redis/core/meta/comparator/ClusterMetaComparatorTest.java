cc2cbef0f7f08edd66c2eec4b48c270ddf09d672
aiyueqi <aiyueqi@alumni.sjtu.edu.cn>
2022-03-31 16:30:40 +0800

    change ClusterMetaComparator: extract shardMeta in sourceMeta and compare

--- redis/redis-core/src/test/java/com/ctrip/xpipe/redis/core/meta/comparator/ClusterMetaComparatorTest.java
+++ redis/redis-core/src/test/java/com/ctrip/xpipe/redis/core/meta/comparator/ClusterMetaComparatorTest.java
@@ -3,6 +3,7 @@ package com.ctrip.xpipe.redis.core.meta.comparator;
 
 import com.ctrip.xpipe.redis.core.entity.ClusterMeta;
 import com.ctrip.xpipe.redis.core.entity.ShardMeta;
+import com.ctrip.xpipe.redis.core.entity.SourceMeta;
 import com.ctrip.xpipe.redis.core.meta.MetaClone;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.junit.Assert;
@@ -46,10 +47,30 @@ public class ClusterMetaComparatorTest extends AbstractComparatorTest{
 		Assert.assertEquals(0, clusterMetaComparator.getMofified().size());
 	}
 
+	@Test
+	public void testSourceShardAdded() {
+
+		SourceMeta source = differentSourceShard();
+		ShardMeta shard = differentShard(current);
+		source.addShard(shard);
+		future.addSource(source);
+
+		ClusterMetaComparator clusterMetaComparator = new ClusterMetaComparator(current, future);
+		clusterMetaComparator.compare();
+
+		assertFalse(clusterMetaComparator.isConfigChange());
+
+		Assert.assertEquals(1, clusterMetaComparator.getAdded().size());
+		Assert.assertEquals(shard, clusterMetaComparator.getAdded().toArray()[0]);
+
+		Assert.assertEquals(0, clusterMetaComparator.getRemoved().size());
+		Assert.assertEquals(0, clusterMetaComparator.getMofified().size());
+	}
+
 	
 	@Test
 	public void testEqual(){
-		
+
 		ClusterMetaComparator clusterMetaComparator = new ClusterMetaComparator(current, future);
 		clusterMetaComparator.compare();
 
@@ -59,6 +80,30 @@ public class ClusterMetaComparatorTest extends AbstractComparatorTest{
 		Assert.assertEquals(0, clusterMetaComparator.getRemoved().size());
 		Assert.assertEquals(0, clusterMetaComparator.getMofified().size());
 		
+	}
+
+	@Test
+	public void testSourceShardEqual() {
+
+		ShardMeta shard = differentShard(current);
+
+		SourceMeta source = differentSourceShard();
+		SourceMeta source2 = differentSourceShard();
+
+		source.addShard(shard);
+		source2.addShard(shard);
+
+		current.addSource(source);
+		future.addSource(source2);
+
+		ClusterMetaComparator clusterMetaComparator = new ClusterMetaComparator(current, future);
+		clusterMetaComparator.compare();
+
+		assertFalse(clusterMetaComparator.isConfigChange());
+
+		Assert.assertEquals(0, clusterMetaComparator.getAdded().size());
+		Assert.assertEquals(0, clusterMetaComparator.getRemoved().size());
+		Assert.assertEquals(0, clusterMetaComparator.getMofified().size());
 	}
 
 
@@ -82,6 +127,28 @@ public class ClusterMetaComparatorTest extends AbstractComparatorTest{
 	}
 
 	@Test
+	public void testSourceShardRemoved() {
+
+		SourceMeta sourceMeta = differentSourceShard();
+		ShardMeta shard = differentShard(current);
+
+		sourceMeta.addShard(shard);
+		current.addSource(sourceMeta);
+
+		ClusterMetaComparator clusterMetaComparator = new ClusterMetaComparator(current, future);
+		clusterMetaComparator.compare();
+
+		assertFalse(clusterMetaComparator.isConfigChange());
+
+		Assert.assertEquals(0, clusterMetaComparator.getAdded().size());
+
+		Assert.assertEquals(1, clusterMetaComparator.getRemoved().size());
+		Assert.assertEquals(shard, clusterMetaComparator.getRemoved().toArray()[0]);
+
+		Assert.assertEquals(0, clusterMetaComparator.getMofified().size());
+	}
+
+	@Test
 	public void testModified(){
 		
 		ShardMeta shardMeta = (ShardMeta) future.getShards().values().toArray()[0];
@@ -99,6 +166,31 @@ public class ClusterMetaComparatorTest extends AbstractComparatorTest{
 		Assert.assertEquals(shardMeta.getId(), comparator.getCurrent().getId());
 	}
 
+	@Test
+	public void testSourceShardModified(){
+
+		SourceMeta sourceMeta = differentSourceShard();
+		ShardMeta shardMeta = differentShard(current);
+		sourceMeta.addShard(shardMeta);
+		current.addSource(sourceMeta);
+
+		SourceMeta sourceMeta2 = differentSourceShard();
+		ShardMeta shardMeta2 = new ShardMeta(shardMeta.getId());
+		shardMeta2.addKeeper(differentKeeper(shardMeta2));
+		sourceMeta2.addShard(shardMeta2);
+		future.addSource(sourceMeta2);
+
+		ClusterMetaComparator clusterMetaComparator = new ClusterMetaComparator(current, future);
+		clusterMetaComparator.compare();
+
+		Assert.assertEquals(0, clusterMetaComparator.getAdded().size());
+
+		Assert.assertEquals(0, clusterMetaComparator.getRemoved().size());
+
+		Assert.assertEquals(1, clusterMetaComparator.getMofified().size());
+		ShardMetaComparator comparator = (ShardMetaComparator) clusterMetaComparator.getMofified().toArray()[0];
+		Assert.assertEquals(shardMeta.getId(), comparator.getCurrent().getId());
+	}
 
 	@Test
 	public void testConfigChanged() {
