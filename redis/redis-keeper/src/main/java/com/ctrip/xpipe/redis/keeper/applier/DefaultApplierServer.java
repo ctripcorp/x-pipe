2afdad247bb3ac4fee8cd787f761f1407c7ef9e1
phyxdown <phyxdown@qq.com>
2022-06-03 17:59:48 +0800

    inject replication & parser to applier server

--- redis/redis-keeper/src/main/java/com/ctrip/xpipe/redis/keeper/applier/DefaultApplierServer.java
+++ redis/redis-keeper/src/main/java/com/ctrip/xpipe/redis/keeper/applier/DefaultApplierServer.java
@@ -2,10 +2,13 @@ package com.ctrip.xpipe.redis.keeper.applier;
 
 import com.ctrip.xpipe.client.redis.AsyncRedisClient;
 import com.ctrip.xpipe.client.redis.AsyncRedisClientFactory;
+import com.ctrip.xpipe.redis.core.redis.operation.RedisOpParser;
 import com.ctrip.xpipe.redis.keeper.applier.lwm.ApplierLwmManager;
 import com.ctrip.xpipe.redis.keeper.applier.sequence.ApplierSequenceController;
 import com.ctrip.xpipe.redis.keeper.applier.lwm.DefaultLwmManager;
 import com.ctrip.xpipe.redis.keeper.applier.sequence.DefaultSequenceController;
+import com.ctrip.xpipe.redis.keeper.applier.xsync.ApplierXsyncReplication;
+import com.ctrip.xpipe.redis.keeper.applier.xsync.DefaultXsyncReplication;
 
 /**
  * @author Slight
@@ -21,12 +24,22 @@ public class DefaultApplierServer extends AbstractInstanceNode {
     public ApplierLwmManager lwmManager;
 
     @InstanceDependency
+    public ApplierXsyncReplication replication;
+
+    @InstanceDependency
     public AsyncRedisClient client;
 
-    public DefaultApplierServer(String clusterName) throws Exception {
+    @InstanceDependency
+    public RedisOpParser parser;
+
+    public DefaultApplierServer(String clusterName, RedisOpParser parser) throws Exception {
+
         this.sequence = new DefaultSequenceController();
         this.lwmManager = new DefaultLwmManager();
+        this.replication = new DefaultXsyncReplication();
+
         this.client = AsyncRedisClientFactory.DEFAULT.getOrCreateClient(clusterName);
+        this.parser = parser;
     }
 
     @Override
