ec949f8a562f35cd4f41e847dc75b17823f3ab72
phyxdown <phyxdown@qq.com>
2022-06-20 22:08:18 +0800

    fix some of int-tests

--- redis/redis-integration-test/src/test/java/com/ctrip/xpipe/redis/integratedtest/console/app/ConsoleSpringApp.java
+++ redis/redis-integration-test/src/test/java/com/ctrip/xpipe/redis/integratedtest/console/app/ConsoleSpringApp.java
@@ -6,7 +6,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
-@ComponentScan(value = {"com.ctrip.xpipe.redis.integratedtest.console.spring.console", "com.ctrip.xpipe.redis.console.spring"}, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = {com.ctrip.xpipe.redis.console.spring.CheckerContextConfig.class, com.ctrip.xpipe.redis.console.spring.ConsoleCheckerContextConfig.class, com.ctrip.xpipe.redis.console.spring.ConsoleContextConfig.class}))
+@ComponentScan(
+        value = {"com.ctrip.xpipe.redis.integratedtest.console.spring.console", "com.ctrip.xpipe.redis.console.spring"}
+        , excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = {
+                com.ctrip.xpipe.redis.console.spring.CheckerContextConfig.class,
+                com.ctrip.xpipe.redis.console.spring.ConsoleCheckerContextConfig.class,
+                com.ctrip.xpipe.redis.console.spring.ConsoleContextConfig.class
+        })
+)
 public class ConsoleSpringApp {
 
     public static void main(String[] args) {
