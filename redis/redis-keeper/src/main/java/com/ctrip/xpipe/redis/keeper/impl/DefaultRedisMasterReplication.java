279693d7d615e6252e92efee615b693ad2beac03
phyxdown <phyxdown@qq.com>
2022-10-19 11:39:17 +0800

    psync always parse AUX

--- redis/redis-keeper/src/main/java/com/ctrip/xpipe/redis/keeper/impl/DefaultRedisMasterReplication.java
+++ redis/redis-keeper/src/main/java/com/ctrip/xpipe/redis/keeper/impl/DefaultRedisMasterReplication.java
@@ -188,9 +188,9 @@ public class DefaultRedisMasterReplication extends AbstractRedisMasterReplicatio
 	protected Psync createPsync() {
 
 		boolean allowKeeperPsync = redisKeeperServer.getTryConnectMasterCnt() <= 1;
-		boolean supportGtid = redisKeeperServer.getKeeperReplType().supportGtidSet();
+
 		Psync psync = new DefaultPsync(clientPool, redisMaster.masterEndPoint(), redisMaster.getReplicationStoreManager(),
-				allowKeeperPsync, supportGtid, scheduled);
+				allowKeeperPsync, scheduled);
 		psync.addPsyncObserver(this);
 		psync.addPsyncObserver(redisKeeperServer);
 
