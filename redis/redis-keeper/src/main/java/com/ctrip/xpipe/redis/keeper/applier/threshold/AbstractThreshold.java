f57b1448b2d9689233339f2a8babb011188f32db
phyxdown <phyxdown@qq.com>
2022-12-11 23:41:17 +0800

    add AbstractThreshold

---
+++ redis/redis-keeper/src/main/java/com/ctrip/xpipe/redis/keeper/applier/threshold/AbstractThreshold.java
@@ -0,0 +1,9 @@
+package com.ctrip.xpipe.redis.keeper.applier.threshold;
+
+/**
+ * @author Slight
+ *
+ *         Dec 09, 2022 11:41
+ */
+public class AbstractThreshold {
+}
---
+++ redis/redis-keeper/src/main/java/com/ctrip/xpipe/redis/keeper/applier/threshold/Threshold.java
@@ -0,0 +1,9 @@
+package com.ctrip.xpipe.redis.keeper.applier.threshold;
+
+/**
+ * @author Slight
+ *
+ *         Dec 09, 2022 11:39
+ */
+public class Threshold {
+}
