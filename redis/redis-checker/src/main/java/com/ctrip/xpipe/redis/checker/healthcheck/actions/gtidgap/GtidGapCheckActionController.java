0bf247c498063b308ce84ecf615d9ddbecd813d3
llj李龙姣 <lilj@trip.com>
2023-01-10 17:26:18 +0800

    add gtid gap check action

---
+++ redis/redis-checker/src/main/java/com/ctrip/xpipe/redis/checker/healthcheck/actions/gtidgap/GtidGapCheckActionController.java
@@ -0,0 +1,7 @@
+package com.ctrip.xpipe.redis.checker.healthcheck.actions.gtidgap;
+
+import com.ctrip.xpipe.redis.checker.healthcheck.HealthCheckActionController;
+import com.ctrip.xpipe.redis.checker.healthcheck.RedisHealthCheckInstance;
+
+public interface GtidGapCheckActionController extends HealthCheckActionController<RedisHealthCheckInstance>  {
+}
