ec2b4b7581babbeb02a32ca55d18d1665bf7ef7b
ndi-trip <131156346+ndi-trip@users.noreply.github.com>
2023-05-31 10:14:18 +0800

    support beacon cluster split & optimize jvm params (#706)
    * support beacon cluster split
    
    * optimize jvm params

--- redis/redis-console/src/test/java/com/ctrip/xpipe/redis/console/healthcheck/nonredis/beacon/UnknownClusterExcludeJobTest.java
+++ redis/redis-console/src/test/java/com/ctrip/xpipe/redis/console/healthcheck/nonredis/beacon/UnknownClusterExcludeJobTest.java
@@ -4,6 +4,7 @@ import com.ctrip.xpipe.redis.console.AbstractConsoleTest;
 import com.ctrip.xpipe.api.migration.auto.MonitorService;
 import com.ctrip.xpipe.redis.console.migration.auto.BeaconSystem;
 import com.google.common.collect.Sets;
+import org.assertj.core.util.Lists;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +13,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
@@ -23,31 +25,39 @@ import java.util.concurrent.ExecutionException;
 public class UnknownClusterExcludeJobTest extends AbstractConsoleTest {
 
     @Mock
-    private MonitorService monitorService;
+    private MonitorService monitorService0;
+    @Mock
+    private MonitorService monitorService1;
+    private List<MonitorService> monitorServices;
 
     private Set<String> expectedClusters;
 
     @Before
     public void setupUnknownClusterExcludeJobTest() {
-        expectedClusters = Sets.newHashSet("cluster1", "cluster2");
+        expectedClusters = Sets.newHashSet("cluster1", "cluster2", "cluster6");
+        monitorServices = Lists.newArrayList(monitorService0, monitorService1);
     }
 
     @Test
     public void testUnregisterUnknownCluster() throws Exception {
-        UnknownClusterExcludeJob job = new UnknownClusterExcludeJob(expectedClusters, monitorService, 5);
-        Mockito.when(monitorService.fetchAllClusters(Mockito.anyString())).thenReturn(Sets.newHashSet("cluster1", "cluster3", "cluster4"));
+        UnknownClusterExcludeJob job = new UnknownClusterExcludeJob(expectedClusters, monitorServices, 5);
+        Mockito.when(monitorService0.fetchAllClusters(Mockito.anyString())).thenReturn(Sets.newHashSet("cluster1", "cluster3", "cluster4"));
+        Mockito.when(monitorService1.fetchAllClusters(Mockito.anyString())).thenReturn(Sets.newHashSet("cluster2", "cluster5"));
         Set<String> excludesClusters = job.execute().get();
 
-        Assert.assertEquals(Sets.newHashSet("cluster3", "cluster4"), excludesClusters);
-        Mockito.verify(monitorService, Mockito.times(2)).unregisterCluster(Mockito.anyString(), Mockito.anyString());
-        Mockito.verify(monitorService).unregisterCluster(BeaconSystem.getDefault().getSystemName(), "cluster3");
-        Mockito.verify(monitorService).unregisterCluster(BeaconSystem.getDefault().getSystemName(), "cluster4");
+        Assert.assertEquals(Sets.newHashSet("cluster3", "cluster4", "cluster5"), excludesClusters);
+        Mockito.verify(monitorService0, Mockito.times(2)).unregisterCluster(Mockito.anyString(), Mockito.anyString());
+        Mockito.verify(monitorService0).unregisterCluster(BeaconSystem.getDefault().getSystemName(), "cluster3");
+        Mockito.verify(monitorService0).unregisterCluster(BeaconSystem.getDefault().getSystemName(), "cluster4");
+        Mockito.verify(monitorService1, Mockito.times(1)).unregisterCluster(Mockito.anyString(), Mockito.anyString());
+        Mockito.verify(monitorService1).unregisterCluster(BeaconSystem.getDefault().getSystemName(), "cluster5");
     }
 
     @Test(expected = TooManyNeedExcludeClusterException.class)
     public void testTooManyClusterToUnregister() throws Throwable {
-        UnknownClusterExcludeJob job = new UnknownClusterExcludeJob(expectedClusters, monitorService, 1);
-        Mockito.when(monitorService.fetchAllClusters(Mockito.anyString())).thenReturn(Sets.newHashSet("cluster1", "cluster3", "cluster4"));
+        UnknownClusterExcludeJob job = new UnknownClusterExcludeJob(expectedClusters, monitorServices, 2);
+        Mockito.when(monitorService0.fetchAllClusters(Mockito.anyString())).thenReturn(Sets.newHashSet("cluster1", "cluster4", "cluster7"));
+        Mockito.when(monitorService1.fetchAllClusters(Mockito.anyString())).thenReturn(Sets.newHashSet("cluster5"));
 
         try {
             job.execute().get();
--- services/ctrip-service/src/main/java/com/ctrip/xpipe/service/beacon/BeaconService.java
+++ services/ctrip-service/src/main/java/com/ctrip/xpipe/service/beacon/BeaconService.java
@@ -12,6 +12,7 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.web.client.RestOperations;
 
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -25,25 +26,44 @@ public class BeaconService implements MonitorService {
     protected static final String PATH_GET_CLUSTERS = "/api/v1/monitor/{system}/clusters";
     protected static final String PATH_CLUSTER = "/api/v1/monitor/{system}/cluster/{cluster}";
 
-    private String getAllClustersPath;
-    private String clusterPath;
+    private final String getAllClustersPath;
+    private final String clusterPath;
 
+    private String name;
     private String host;
+    private int weight;
 
-    private Logger logger = LoggerFactory.getLogger(BeaconService.class);
+    private final Logger logger = LoggerFactory.getLogger(BeaconService.class);
 
     private static final ParameterizedTypeReference<BeaconResp<Set<String>>> clustersRespTypeDef =
             new ParameterizedTypeReference<BeaconResp<Set<String>>>(){};
 
-    public BeaconService(String host) {
+    public BeaconService(String name, String host, int weight) {
+        this.name = name;
         this.host = host;
+        this.weight = weight;
         getAllClustersPath = host + PATH_GET_CLUSTERS;
         clusterPath = host + PATH_CLUSTER;
     }
 
     @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
     public String getHost() {
         return this.host;
+    }
+
+    @Override
+    public int getWeight() {
+        return this.weight;
+    }
+
+    @Override
+    public void setWeight(int weight) {
+        this.weight = weight;
     }
 
     @Override
@@ -74,7 +94,11 @@ public class BeaconService implements MonitorService {
 
     @Override
     public void unregisterCluster(String system, String clusterName) {
-        ResponseEntity<BeaconResp> respResponseEntity = restTemplate.exchange(clusterPath, HttpMethod.DELETE, null, BeaconResp.class, system, clusterName);
+        if (system == null || clusterName == null) {
+            return;
+        }
+        ResponseEntity<BeaconResp> respResponseEntity = restTemplate.exchange(clusterPath, HttpMethod.DELETE,
+            null, BeaconResp.class, system, clusterName);
         BeaconResp beaconResp = respResponseEntity.getBody();
         if (!beaconResp.isSuccess()) {
             logger.info("[unregisterCluster] fail, {}", beaconResp.getMsg());
@@ -83,10 +107,32 @@ public class BeaconService implements MonitorService {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        BeaconService that = (BeaconService)o;
+
+        if (weight != that.weight)
+            return false;
+        if (!Objects.equals(name, that.name))
+            return false;
+        return Objects.equals(host, that.host);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (host != null ? host.hashCode() : 0);
+        result = 31 * result + weight;
+        return result;
+    }
+
+    @Override
     public String toString() {
-        return "BeaconService{" +
-                "host='" + host + '\'' +
-                '}';
+        return "BeaconService{" + "name='" + name + '\'' + ", host='" + host + '\'' + ", weight=" + weight + '}';
     }
 }
 
