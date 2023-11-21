6eeeaf03611595005e03f51995ee89016592fd34
songyuyuyu <88360486+songyuyuyu@users.noreply.github.com>
2023-11-21 10:37:27 +0800

    fix keeper auto migration test fail (#748)

--- redis/redis-console/src/test/java/com/ctrip/xpipe/redis/console/keeper/AutoMigrateOverloadKeeperContainerActionTest.java
+++ redis/redis-console/src/test/java/com/ctrip/xpipe/redis/console/keeper/AutoMigrateOverloadKeeperContainerActionTest.java
@@ -1,5 +1,6 @@
 package com.ctrip.xpipe.redis.console.keeper;
 
+import com.ctrip.xpipe.redis.checker.alert.AlertManager;
 import com.ctrip.xpipe.redis.checker.model.DcClusterShard;
 import com.ctrip.xpipe.redis.checker.model.KeeperContainerUsedInfoModel;
 import com.ctrip.xpipe.redis.console.model.MigrationKeeperContainerDetailModel;
@@ -30,6 +31,9 @@ public class AutoMigrateOverloadKeeperContainerActionTest {
 
     @Mock
     private ShardModelService shardModelService;
+
+    @Mock
+    private AlertManager alertManager;
 
     @Before
     public void beforeAutoMigrateOverloadKeeperContainerActionTest() {
