81172fc98e3cd8909f102f1adee4b4adc54999e5
passerbyzed <96860338+passerbyzed@users.noreply.github.com>
2023-11-29 20:48:44 +0800

    fix proxy doclean npe bug (#753)
    Co-authored-by: yifuzhou <yifuzhou@trip.com>

--- redis/redis-proxy/src/test/java/com/ctrip/xpipe/redis/proxy/tunnel/DefaultTunnelManagerTest.java
+++ redis/redis-proxy/src/test/java/com/ctrip/xpipe/redis/proxy/tunnel/DefaultTunnelManagerTest.java
@@ -1,19 +1,21 @@
 package com.ctrip.xpipe.redis.proxy.tunnel;
 
+import com.ctrip.xpipe.redis.core.proxy.ProxyResourceManager;
 import com.ctrip.xpipe.redis.proxy.AbstractRedisProxyServerTest;
 import com.ctrip.xpipe.redis.proxy.Tunnel;
+import com.ctrip.xpipe.redis.proxy.monitor.TunnelMonitorManager;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelConfig;
 import io.netty.channel.DefaultChannelPromise;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import java.net.InetSocketAddress;
 
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * @author chen.zhu
@@ -22,11 +24,13 @@ import static org.mockito.Mockito.when;
  */
 public class DefaultTunnelManagerTest extends AbstractRedisProxyServerTest {
 
+    @Mock
     private DefaultTunnelManager manager;
 
     @Before
     public void beforeDefaultTunnelManagerTest() {
         manager = (DefaultTunnelManager) tunnelManager();
+        manager = spy(manager);
     }
 
     @Test
@@ -36,29 +40,15 @@ public class DefaultTunnelManagerTest extends AbstractRedisProxyServerTest {
         Channel frontChannel2 = fakeChannel();
         manager.create(frontChannel1, protocol("Proxy Route proxy://127.0.0.1:8009"));
         manager.create(frontChannel2, protocol("Proxy Route proxy://127.0.0.1:8009"));
-
         Assert.assertEquals(2, manager.tunnels().size());
 
         when(frontChannel1.isActive()).thenReturn(false);
         when(frontChannel2.isActive()).thenReturn(true);
         manager.doClean();
-
         Assert.assertEquals(1, manager.tunnels().size());
-    }
 
-    @Test
-    public void testDoClean2() throws Exception {
-        startListenServer(8009);
-        Channel frontChannel1 = fakeChannel();
-        Channel frontChannel2 = fakeChannel();
-        when(frontChannel2.isActive()).thenReturn(true);
-        Tunnel tunnel1 = manager.create(frontChannel1, protocol("Proxy Route proxy://127.0.0.1:8009"));
-        manager.create(frontChannel2, protocol());
-
-        Assert.assertEquals(2, manager.tunnels().size());
-
-        tunnel1.release();
-        Assert.assertEquals(1, manager.tunnels().size());
+        manager.setLifecycleStateStarted();
+        manager.doClean();
     }
 
     private Channel fakeChannel() {
