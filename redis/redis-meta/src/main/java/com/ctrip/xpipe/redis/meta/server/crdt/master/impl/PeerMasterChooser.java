8609ab6d3d6954f10e30511310791a686ae2d875
lishanglin <sl_li@trip.com>
2023-12-22 14:05:03 +0800

    remove redundant peerMaster after unbinding peerDc

--- redis/redis-meta/src/main/java/com/ctrip/xpipe/redis/meta/server/crdt/master/impl/PeerMasterChooser.java
+++ redis/redis-meta/src/main/java/com/ctrip/xpipe/redis/meta/server/crdt/master/impl/PeerMasterChooser.java
@@ -4,11 +4,16 @@ import com.ctrip.xpipe.command.ParallelCommandChain;
 import com.ctrip.xpipe.concurrent.KeyedOneThreadTaskExecutor;
 import com.ctrip.xpipe.redis.core.entity.ClusterMeta;
 import com.ctrip.xpipe.redis.meta.server.crdt.master.MasterChooseCommandFactory;
+import com.ctrip.xpipe.redis.meta.server.crdt.master.command.RedundantMasterClearCommand;
 import com.ctrip.xpipe.redis.meta.server.keeper.keepermaster.MasterChooser;
 import com.ctrip.xpipe.redis.meta.server.meta.CurrentMetaManager;
 import com.ctrip.xpipe.redis.meta.server.meta.DcMetaCache;
 import com.ctrip.xpipe.tuple.Pair;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.*;
 
 public class PeerMasterChooser extends CurrentMasterChooser implements MasterChooser {
@@ -37,11 +42,14 @@ public class PeerMasterChooser extends CurrentMasterChooser implements MasterCho
         ParallelCommandChain parallelCommandChain = new ParallelCommandChain(executors);
         String currentDc = dcMetaCache.getCurrentDc();
 
-        for (String dcId : clusterMeta.getDcs().split("\\s*,\\s*")) {
+        String[] rawDcs = clusterMeta.getDcs().split("\\s*,\\s*");
+        Set<String> dcs = new HashSet<>(Arrays.asList(rawDcs));
+        masterChooseCommandFactory.buildRedundantMasterClearCommand(clusterDbId, shardDbId, dcs).execute();
+
+        for (String dcId : dcs) {
             if (currentDc.equalsIgnoreCase(dcId)) continue;
             parallelCommandChain.add(masterChooseCommandFactory.buildPeerMasterChooserCommand(dcId, clusterDbId, shardDbId));
         }
-
         peerMasterChooseExecutor.execute(Pair.of(clusterDbId, shardDbId), parallelCommandChain);
     }
 
