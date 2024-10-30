3bff9634ac6cebbcd3cd6306e9f47c813a18b239
qifanwang <qifanwang@trip.com>
2024-10-30 19:57:42 +0800

    beacon checker

--- core/src/main/java/com/ctrip/xpipe/api/migration/auto/data/MonitorClusterMeta.java
+++ core/src/main/java/com/ctrip/xpipe/api/migration/auto/data/MonitorClusterMeta.java
@@ -1,8 +1,8 @@
 package com.ctrip.xpipe.api.migration.auto.data;
 
-import java.util.Collections;
-import java.util.Map;
-import java.util.Set;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import java.util.*;
 
 /**
  * @author lishanglin
@@ -37,6 +37,22 @@ public class MonitorClusterMeta {
 
     public void setExtra(Map<String, String> extra) {
         this.extra = extra;
+    }
+
+    public int generateHashCodeForBeaconCheck () {
+        List<MonitorGroupMeta> nodeList = new ArrayList<>(nodeGroups);
+        nodeList.sort(Comparator.comparing(MonitorGroupMeta::getName, Comparator.nullsFirst(Comparator.naturalOrder()))
+                .thenComparing(MonitorGroupMeta::getIdc, Comparator.nullsFirst(Comparator.naturalOrder()))
+                .thenComparing(MonitorGroupMeta::isMasterGroup)
+                .thenComparing(monitorGroupMeta -> monitorGroupMeta.getNodes().toString()));
+        HashCodeBuilder builder = new HashCodeBuilder();
+        for(MonitorGroupMeta group : nodeList) {
+            builder.append(group.getName())
+                    .append(group.getIdc())
+                    .append(group.isMasterGroup())
+                    .append(group.getNodes());
+        }
+        return builder.toHashCode();
     }
 
 }
