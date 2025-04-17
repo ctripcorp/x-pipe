dd5e7e0283cc54d2310369c67328dfcddaa74a05
hany_li <hany_li@trip.com>
2025-04-17 11:36:14 +0800

    10 s timeout

--- services/ctrip-service/src/main/java/com/ctrip/xpipe/service/client/redis/CRedisAsyncClient.java
+++ services/ctrip-service/src/main/java/com/ctrip/xpipe/service/client/redis/CRedisAsyncClient.java
@@ -170,7 +170,7 @@ public class CRedisAsyncClient implements AsyncRedisClient {
             public void onFailure(Throwable t) {
                 commandFuture.setFailure(t);
             }
-        }, 1, TimeUnit.SECONDS, credisNotifyThread);
+        }, 10, TimeUnit.SECONDS, credisNotifyThread);
         return commandFuture;
     }
 
