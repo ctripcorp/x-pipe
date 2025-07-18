4189382b516c32fc8e64788f3a8db30a837670d5
qifanwang <qifanwang@trip.com>
2025-07-18 16:14:01 +0800

    dal

--- services/ctrip-service/src/main/java/com/ctrip/xpipe/service/ignite/DalIgniteValidate.java
+++ services/ctrip-service/src/main/java/com/ctrip/xpipe/service/ignite/DalIgniteValidate.java
@@ -2,17 +2,25 @@ package com.ctrip.xpipe.service.ignite;
 
 import com.ctrip.platform.dal.dao.DalClientFactory;
 import com.ctrip.platform.dal.dao.configure.ignite.IgniteResult;
+import com.ctrip.platform.dal.dao.configure.ignite.SingleIgniteResult;
 import com.ctrip.platform.dal.exceptions.DalRuntimeException;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DalIgniteValidate {
 
+    private static final Logger log = LoggerFactory.getLogger(DalIgniteValidate.class);
+
     @PostConstruct
     public void init() {
         IgniteResult igniteResult = DalClientFactory.checkConnections();
         if (igniteResult.anyFail()) {
+            for(SingleIgniteResult result : igniteResult.getIgniteDetail()) {
+                log.error("Ignite check fail", result.getError());
+            }
             throw new DalRuntimeException(igniteResult.getErrorMsg());
         }
     }
