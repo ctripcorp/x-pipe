a9cc636bf723ff05b667b8e05a9ce3fc1990589a
qifanwang <qifanwang@trip.com>
2025-07-22 11:10:59 +0800

    fix cycle for withoutdb

--- redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/service/meta/impl/DcMetaServiceWithoutDB.java
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/service/meta/impl/DcMetaServiceWithoutDB.java
@@ -9,6 +9,7 @@ import com.ctrip.xpipe.redis.core.entity.DcMeta;
 import com.ctrip.xpipe.redis.core.meta.MetaCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -20,6 +21,7 @@ import java.util.Set;
 public class DcMetaServiceWithoutDB implements DcMetaService {
 
     @Autowired
+    @Lazy
     private MetaCache metaCache;
 
     @Autowired
