ee5f73c0c99d6a6877d9da52fa1042855ad9277a
yuanyuanqiu <yuanyuanqiu@trip.com>
2025-08-18 20:50:49 +0800

    optimize alert

--- redis/redis-meta/src/main/java/com/ctrip/xpipe/redis/meta/server/keeper/manager/KeeperMasterCheckNotAsExpectedException.java
+++ redis/redis-meta/src/main/java/com/ctrip/xpipe/redis/meta/server/keeper/manager/KeeperMasterCheckNotAsExpectedException.java
@@ -24,9 +24,7 @@ public class KeeperMasterCheckNotAsExpectedException extends MetaServerException
 	public enum KeeperAlertType {
 		CHECK_NOT_REDIS("not redis"),
 		CHECK_NOT_MASTER("not master"),
-		CHECK_MULTI_MASTER("multi master"),
-		CHANGE_NOT_FIND("can not find active keeper"),
-		COMMAND_FAIL("command fail");
+		CHECK_MULTI_MASTER("multi master");
 
 		private String desc;
 
