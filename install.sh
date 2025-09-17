46a166557f1469a3b10742e97f4f817e37132220
yifuzhou <yifuzhou@trip.com>
2025-09-17 20:39:42 +0800

    change install.sh

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ for _Version in `ls ctripgroup/scm/super-rule/ | grep -v maven-metadata`; do
 	mvn install:install-file -Dfile=ctripgroup/scm/super-rule/$_Version/super-rule-$_Version.pom -DpomFile=ctripgroup/scm/super-rule/$_Version/super-rule-$_Version.pom -Dpackaging=pom
 done
 
-for _Version in `ls com/ctrip/ctrip-super-rule/ | grep -v maven-metadata`; do
+for _Version in `ls com/ctrip/ctrip-super-rule/ | grep -v maven-metadata | grep -v resolver-status`; do
 	mvn install:install-file -Dfile=com/ctrip/ctrip-super-rule/$_Version/ctrip-super-rule-$_Version.pom -DpomFile=com/ctrip/ctrip-super-rule/$_Version/ctrip-super-rule-$_Version.pom -Dpackaging=pom
 done
 
@@ -100,7 +100,7 @@ done
 
 
 
-for _Version in `ls org/unidal/framework/parent/ | grep -v maven-metadata`; do
+for _Version in `ls org/unidal/framework/parent/ | grep -v maven-metadata | grep -v resolver-status`; do
 	mvn install:install-file -Dfile=org/unidal/framework/parent/$_Version/parent-$_Version.pom -DpomFile=org/unidal/framework/parent/$_Version/parent-$_Version.pom -Dpackaging=pom
 done
 
