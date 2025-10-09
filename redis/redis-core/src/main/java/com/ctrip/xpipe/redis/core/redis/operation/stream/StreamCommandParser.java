4dcd4cc7f6d861460b298de78e14d659f525c550
zh <h.zhang7@trip.com>
2025-10-09 15:58:26 +0800

    1、not copy buffer when StreamCommandParser do read

--- redis/redis-core/src/main/java/com/ctrip/xpipe/redis/core/redis/operation/stream/StreamCommandParser.java
+++ redis/redis-core/src/main/java/com/ctrip/xpipe/redis/core/redis/operation/stream/StreamCommandParser.java
@@ -1,9 +1,7 @@
 package com.ctrip.xpipe.redis.core.redis.operation.stream;
 
-import com.ctrip.xpipe.redis.core.exception.RedisRuntimeException;
 import com.ctrip.xpipe.redis.core.protocal.RedisClientProtocol;
 import com.ctrip.xpipe.redis.core.protocal.protocal.ArrayParser;
-import com.ctrip.xpipe.redis.core.redis.operation.RedisOpParser;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.CompositeByteBuf;
@@ -11,9 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
-
-import static com.ctrip.xpipe.redis.core.protocal.RedisClientProtocol.ASTERISK_BYTE;
 
 public class StreamCommandParser {
 
@@ -21,107 +16,68 @@ public class StreamCommandParser {
 
     private RedisClientProtocol<Object[]> protocolParser;
 
-    private RedisOpParser opParser;
-
-    private ByteBuf remainingBuf;
-
+    private CompositeByteBuf remainingBuf;
 
     private StreamCommandLister commandLister;
 
     private ByteBufAllocator allocator;
 
-    public StreamCommandParser(RedisOpParser opParser, StreamCommandLister commandLister) {
+    public StreamCommandParser(StreamCommandLister commandLister) {
         this.protocolParser = new ArrayParser();
-        this.commandLister = commandLister;
         allocator = ByteBufAllocator.DEFAULT;
-        this.opParser = opParser;
+        this.commandLister = commandLister;
     }
 
 
     public void doRead(ByteBuf byteBuf) throws IOException {
-
         try {
             while (byteBuf.readableBytes() > 0) {
                 int pre = byteBuf.readerIndex();
-                RedisClientProtocol<Object[]> protocol = null;
-                try {
-                    protocol = protocolParser.read(byteBuf);
-                } catch (RedisRuntimeException | NumberFormatException exception) {
-                    // 错误处理，碰到脏数据需要跳过，预期不会用脏数据
-                    log.error("[doRead] {}", exception.getMessage());
-                    int starIndex = findFirstStar(byteBuf, pre + 1);
-                    if(starIndex != -1) {
-                        log.info("[skip some data] from {}", byteBuf.slice(pre, starIndex - pre).toString(Charset.defaultCharset()));
-                        byteBuf.readerIndex(starIndex);
-                        this.reset();
-                        continue;
-                    } else {
-                        throw exception;
-                    }
-                }
+                RedisClientProtocol<Object[]> protocol = protocolParser.read(byteBuf);
+                compositeCurrentPayLoadBuffer(byteBuf, pre);
                 if (protocol == null) {
-                    ByteBuf  newRemainingBuf = allocator.buffer(getRemainLength() + byteBuf.writerIndex() - pre);
-                    if(remainingBuf != null && remainingBuf.readableBytes() > 0) {
-                        newRemainingBuf.writeBytes(remainingBuf);
-                    }
-                    newRemainingBuf.writeBytes(byteBuf.slice(pre,  byteBuf.writerIndex() - pre));
-                    this.relaseReaminBuf();
-                    this.remainingBuf = newRemainingBuf;
                     break;
-                }
-
-                CompositeByteBuf mergeBuf = allocator.compositeBuffer();
-                try {
-                    if(remainingBuf != null && remainingBuf.readableBytes() > 0) {
-                        mergeBuf.addComponent(true, remainingBuf);
-                        remainingBuf = null; // 旧的引用置空，其生命周期由mergeBuf管理
+                } else {
+                    try {
+                        Object[] payload = protocol.getPayload();
+                        commandLister.onCommand(payload, remainingBuf);
+                    } finally {
+                        relaseReaminBuf();
                     }
-                    ByteBuf finishBuf = byteBuf.slice(pre, byteBuf.readerIndex() - pre);
-                    finishBuf.retain();
-                    mergeBuf.addComponent(true, finishBuf);
-                    Object[] payload = protocol.getPayload();
-                    commandLister.onCommand(payload, mergeBuf);
-                } finally {
-                    mergeBuf.release();
+                    this.protocolParser.reset();
                 }
-                this.protocolParser.reset();
             }
-        } catch (Exception e) {
+        } catch (Throwable e) {
+            log.error("[doRead]", e);
             this.reset();
-            this.protocolParser.reset();
-        }
-        finally {
+        } finally {
             byteBuf.skipBytes(byteBuf.readableBytes());
         }
     }
 
-    private int findFirstStar(ByteBuf byteBuf, int beginOffset) {
-        for(int i = beginOffset; i < byteBuf.writerIndex(); i++) {
-            byte b = byteBuf.getByte(i);
-            if(b == ASTERISK_BYTE) {
-                return i;
-            }
+    private void compositeCurrentPayLoadBuffer(ByteBuf byteBuf, int pre) {
+        if (remainingBuf == null) {
+            remainingBuf = allocator.compositeBuffer();
         }
-        return -1;
+        ByteBuf slice = byteBuf.slice(pre, byteBuf.readerIndex() - pre);
+        slice.retain();
+        remainingBuf.addComponent(true, slice);
     }
 
     private void relaseReaminBuf() {
-        if(remainingBuf != null) {
+        if (remainingBuf != null) {
             remainingBuf.release();
             remainingBuf = null;
         }
     }
 
     public void reset() {
-        if(remainingBuf != null) {
-            this.remainingBuf.release();
-            this.remainingBuf = null;
-        }
+        relaseReaminBuf();
         this.protocolParser.reset();
     }
 
     public int getRemainLength() {
-        if(remainingBuf == null) {
+        if (remainingBuf == null) {
             return 0;
         }
         return this.remainingBuf.readableBytes();
--- redis/redis-core/src/test/java/com/ctrip/xpipe/redis/core/redis/operation/stream/StreamCommandParserTest.java
+++ redis/redis-core/src/test/java/com/ctrip/xpipe/redis/core/redis/operation/stream/StreamCommandParserTest.java
@@ -1,27 +1,24 @@
 package com.ctrip.xpipe.redis.core.redis.operation.stream;
 
 import com.ctrip.xpipe.redis.core.protocal.RedisClientProtocol;
-import com.ctrip.xpipe.redis.core.protocal.RedisProtocol;
-import com.ctrip.xpipe.redis.core.redis.operation.RedisOpParser;
 import com.google.common.collect.Lists;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.UnpooledByteBufAllocator;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class StreamCommandParserTest {
 
     @Test
-    public void doRead() {
+    public void doRead_normalTest() {
         List<Object> command = new ArrayList<>();
-        RedisOpParser redisOpParser = Mockito.mock(RedisOpParser.class);
 
         StreamCommandLister streamCommandLister = new StreamCommandLister() {
             @Override
@@ -29,30 +26,181 @@ public class StreamCommandParserTest {
                 command.add(new Object[]{payload, commandBuf});
             }
         };
-        StreamCommandParser streamCommandParser = new StreamCommandParser(redisOpParser, streamCommandLister);
+        StreamCommandParser streamCommandParser = new StreamCommandParser(streamCommandLister);
+
+        ByteBuf byteBuf = getArraysRepl(Lists.newArrayList(
+                "set",
+                "key1",
+                "value1"
+        ));
+        int readableBytes = byteBuf.readableBytes();
+
+        try {
+            streamCommandParser.doRead(byteBuf);
+        } catch (IOException e) {
+            fail();
+        }
+        assertEquals(1, command.size());
+        Object[] first = (Object[]) command.get(0);
+        assertEquals(3, ((Object[]) first[0]).length);
+        assertEquals("set", ((Object[]) first[0])[0].toString());
+        assertEquals("key1", ((Object[]) first[0])[1].toString());
+        assertEquals("value1", ((Object[]) first[0])[2].toString());
+        ByteBuf cmdBuf = (ByteBuf) first[1];
+        assertEquals(readableBytes, cmdBuf.readableBytes());
+    }
+
+    @Test
+    public void doRead_abnormalTest() {
+        List<Object> command = new ArrayList<>();
+
+        StreamCommandLister streamCommandLister = new StreamCommandLister() {
+            @Override
+            public void onCommand(Object[] payload, ByteBuf commandBuf) throws IOException {
+                command.add(new Object[]{payload, commandBuf});
+            }
+        };
+        StreamCommandParser streamCommandParser = new StreamCommandParser(streamCommandLister);
+
+        ByteBuf byteBuf = UnpooledByteBufAllocator.DEFAULT.buffer();
+        byteBuf.writeBytes(getArraysRepl(Lists.newArrayList(
+                "set",
+                "key1",
+                "value1"
+        )));
+
+        int readableBytes = byteBuf.readableBytes();
+
+        // build some dirty data in the head
+        String dirty = "hello\r\nworld\r\n";
+        ByteBuf newBuf = UnpooledByteBufAllocator.DEFAULT.buffer(readableBytes + dirty.length());
+        newBuf.writeBytes(dirty.getBytes());
+        newBuf.writeBytes(byteBuf);
+        try {
+            streamCommandParser.doRead(newBuf);
+        } catch (IOException e) {
+            fail();
+        }
+        assertEquals(0, command.size());
+
+        // dirty data in the tail
+        command.clear();
+        newBuf.clear();
+        newBuf.writeBytes(getArraysRepl(Lists.newArrayList(
+                "set",
+                "key1",
+                "value1"
+        )));
+        newBuf.writeBytes(dirty.getBytes());
+        try {
+            streamCommandParser.doRead(newBuf);
+        } catch (IOException e) {
+            fail();
+        }
+        assertEquals(1, command.size());
+        Object[] first = (Object[]) command.get(0);
+        assertEquals(3, ((Object[]) first[0]).length);
+        assertEquals("set", ((Object[]) first[0])[0].toString());
+        assertEquals("key1", ((Object[]) first[0])[1].toString());
+        assertEquals("value1", ((Object[]) first[0])[2].toString());
+        ByteBuf cmdBuf = (ByteBuf) first[1];
+        assertEquals(readableBytes, cmdBuf.readableBytes());
+
+        // dirty data in the middle
+        command.clear();
+        byteBuf.readerIndex(0);
+        newBuf.clear();
+        newBuf.writeBytes(getArraysRepl(Lists.newArrayList(
+                "set",
+
+                "key1"
+        )));
+
+    }
+
+    @Test
+    public void doRead_ReConnectTest() {
+        // 模拟发送部分包的时候，连接断开，然后重连后从头开始发送，结果正确
+        List<Object> command = new ArrayList<>();
+        StreamCommandLister streamCommandLister = new StreamCommandLister() {
+            @Override
+            public void onCommand(Object[] payload, ByteBuf commandBuf) throws IOException {
+                command.add(new Object[]{payload, commandBuf});
+            }
+        };
+        StreamCommandParser streamCommandParser = new StreamCommandParser(streamCommandLister);
+        ByteBuf wholeBuf = getArraysRepl(Lists.newArrayList(
+                "set",
+                "key1",
+                "value1"
+        ));
+        // 发送部分包
+        ByteBuf part = wholeBuf.readSlice(10);
+        try {
+            streamCommandParser.doRead(part);
+        } catch (IOException e) {
+            fail();
+        }
+        assertEquals(0, command.size());
+        // 重连
+        streamCommandParser.reset();
+        // 重新发送所有
+        wholeBuf.readerIndex(0);
+        try {
+            streamCommandParser.doRead(wholeBuf);
+        } catch (IOException e) {
+            fail();
+        }
+        assertEquals(1, command.size());
+        Object[] first = (Object[]) command.get(0);
+        assertEquals(3, ((Object[]) first[0]).length);
+        assertEquals("set", ((Object[]) first[0])[0].toString());
+        assertEquals("key1", ((Object[]) first[0])[1].toString());
+        assertEquals("value1", ((Object[]) first[0])[2].toString());
+    }
+
+    @Test
+    public void doRead_bigKeysTest() {
+        List<Object> command = new ArrayList<>();
+
+        StreamCommandLister streamCommandLister = new StreamCommandLister() {
+            @Override
+            public void onCommand(Object[] payload, ByteBuf commandBuf) throws IOException {
+                command.add(new Object[]{payload, commandBuf});
+            }
+        };
+        StreamCommandParser streamCommandParser = new StreamCommandParser(streamCommandLister);
 
         ByteBuf wholeBuf = getArraysRepl(Lists.newArrayList(
                 "set",
                 "key1",
                 getString(20_000_000)
         ));
+        System.out.println("wholeBuf: " + wholeBuf.readableBytes());
+        int size = 65536;
+        int piece = wholeBuf.readableBytes() / size;
 
-        long start = System.nanoTime();
-        // wholeBuf拆分，要考虑不能整除的情况，最终要处理所有数据，不能缺失
-        int piece = wholeBuf.readableBytes() / 512;
-        try {
-            for (int i = 0; i < piece; i++) {
-                ByteBuf part = wholeBuf.readSlice(512);
-                streamCommandParser.doRead(part);
+        for (int r = 0; r < 10; r++) {
+            long start = System.nanoTime();
+            // wholeBuf拆分，要考虑不能整除的情况，最终要处理所有数据，不能缺失
+            try {
+                for (int i = 0; i < piece; i++) {
+                    ByteBuf part = wholeBuf.readSlice(size);
+                    streamCommandParser.doRead(part);
+                }
+                ByteBuf lastPart = wholeBuf.readSlice(wholeBuf.readableBytes());
+                streamCommandParser.doRead(lastPart);
+            } catch (IOException e) {
+                fail();
             }
-            ByteBuf lastPart = wholeBuf.readSlice(wholeBuf.readableBytes());
-            streamCommandParser.doRead(lastPart);
-        } catch (IOException e) {
-            fail();
+            long end = System.nanoTime();
+            System.out.println("cost: " + (end - start) / 1_000_000 + " ms");
+            System.out.println(Arrays.toString(command.toArray()));
+
+            wholeBuf.readerIndex(0);
+            streamCommandParser.reset();
+            command.clear();
         }
-        long end = System.nanoTime();
-        System.out.println("cost: " + (end - start) / 1_000_000 + " ms");
-        System.out.println(Arrays.toString(command.toArray()));
 
     }
 
@@ -63,12 +211,19 @@ public class StreamCommandParserTest {
         buffer.writeBytes((String.valueOf(expected.size())).getBytes());
         buffer.writeBytes("\r\n".getBytes());
         for (String s : expected) {
-            buffer.writeByte(RedisClientProtocol.DOLLAR_BYTE);
-            buffer.writeBytes((String.valueOf(s.length())).getBytes());
-            buffer.writeBytes("\r\n".getBytes());
-            buffer.writeBytes(s.getBytes());
-            buffer.writeBytes("\r\n".getBytes());
+            buffer.writeBytes(getBulkStringRepl(s));
         }
+        return buffer;
+    }
+
+    private ByteBuf getBulkStringRepl(String expected) {
+        // $<number-of-bytes>\r\n<bytes>\r\n
+        ByteBuf buffer = UnpooledByteBufAllocator.DEFAULT.buffer();
+        buffer.writeByte(RedisClientProtocol.DOLLAR_BYTE);
+        buffer.writeBytes((String.valueOf(expected.length())).getBytes());
+        buffer.writeBytes("\r\n".getBytes());
+        buffer.writeBytes(expected.getBytes());
+        buffer.writeBytes("\r\n".getBytes());
         return buffer;
     }
 
