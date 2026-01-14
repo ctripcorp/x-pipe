1deb70ee00f0e686dee015d5ded7cee34dda9af4
TB <b.tian@trip.com>
2026-01-14 20:00:33 +0800

    add passive dr data incosistency

--- redis/redis-integration-test/src/test/java/com/ctrip/xpipe/redis/integratedtest/keeper/KeeperMultiDcChangePrimaryXsync.java
+++ redis/redis-integration-test/src/test/java/com/ctrip/xpipe/redis/integratedtest/keeper/KeeperMultiDcChangePrimaryXsync.java
@@ -120,6 +120,79 @@ public class KeeperMultiDcChangePrimaryXsync extends AbstractKeeperIntegratedMul
         assertReplOffset(newRedisMaster);
     }
 
+    @Test
+    public void testBeforeChangePrimaryWriteMultiIdc() throws Exception{
+
+        String primaryDc = getPrimaryDc();
+        String backupDc = getBackupDc();
+        //change backup to primary
+        logger.info("[gtid set enabled] {}:{}", getRedisMaster().getIp(), getRedisMaster().getPort());
+        setRedisToGtidEnabled(getRedisMaster().getIp(), getRedisMaster().getPort());
+        Thread.sleep(2000);
+        sendMessageToMaster(getRedisMaster(), 1);
+        makeBackupDcKeeperRight(getBackupDc());
+        Thread.sleep(2000);
+
+
+        for(RedisMeta slave : getRedisSlaves()) {
+            setRedisToGtidEnabled(slave.getIp(), slave.getPort());
+        }
+
+        when(dcMetaCache.getShardRedises(getClusterDbId(), getShardDbId())).thenReturn(getDcRedises(backupDc, getClusterId(), getShardId()));
+        when(currentMetaManager.getSurviveKeepers(getClusterDbId(), getShardDbId())).thenReturn(getDcKeepers(backupDc, getClusterId(), getShardId()));
+
+        ClusterMeta clusterMeta = mock(ClusterMeta.class);
+        when(clusterMeta.getActiveDc()).thenReturn(primaryDc);
+        when(currentMetaManager.getClusterMeta(getClusterDbId())).thenReturn(clusterMeta);
+
+        assertMultiDcGtid(getRedisMaster());
+        assertReplOffset(getRedisMaster());
+
+        logger.info(remarkableMessage("[make dc primary]change dc primary to:" + backupDc));
+        BecomePrimaryAction becomePrimaryAction = new BecomePrimaryAction(getClusterDbId(), getShardDbId(), dcMetaCache,
+                currentMetaManager, sentinelManager, offsetWaiter, new ExecutionLog(currentTestName()),
+                getXpipeNettyClientKeyedObjectPool(), newMasterChooser, scheduled, executors);
+        MetaServerConsoleService.PrimaryDcChangeMessage message = becomePrimaryAction.changePrimaryDc(getClusterDbId(), getShardDbId(), backupDc, new MasterInfo());
+        logger.info("{}", message);
+
+        RedisMeta newRedisMaster = newMasterChooser.getLastChoosenMaster();
+        logger.info("new master is {}", newRedisMaster);
+
+        sendMessageToMaster(getRedisMaster(), 5);
+
+        sendMessageToMaster(newRedisMaster, 5);
+
+        logger.info(remarkableMessage("[make dc backup]change dc primary to:" + backupDc));
+        when(dcMetaCache.getPrimaryDc(getClusterDbId(), getShardDbId())).thenReturn(backupDc);
+        when(multiDcService.getActiveKeeper(backupDc, getClusterDbId(), getShardDbId())).thenReturn(getDcKeepers(backupDc, getClusterId(), getShardId()).get(0));
+
+        when(dcMetaCache.getShardRedises(getClusterDbId(), getShardDbId())).thenReturn(getDcRedises(primaryDc, getClusterId(), getShardId()));
+        when(currentMetaManager.getKeeperActive(getClusterDbId(), getShardDbId())).thenReturn(getKeeperActive(primaryDc));
+        when(currentMetaManager.getSurviveKeepers(getClusterDbId(), getShardDbId())).thenReturn(getDcKeepers(primaryDc, getClusterId(), getShardId()));
+
+
+        BecomeBackupAction becomeBackupAction = new BecomeBackupAction(getClusterDbId(), getShardDbId(), dcMetaCache,
+                currentMetaManager, sentinelManager, new ExecutionLog(currentTestName()),
+                getXpipeNettyClientKeyedObjectPool(), multiDcService, scheduled, executors);
+        message = becomeBackupAction.changePrimaryDc(getClusterDbId(), getShardDbId(), backupDc, new MasterInfo());
+        logger.info("becomeBackupAction {}", message);
+
+        List<RedisMeta> allRedises = getRedises(primaryDc);
+        allRedises.addAll(getRedises(backupDc));
+        allRedises.remove(newRedisMaster);
+
+        logger.info("{}\n{}", newRedisMaster, allRedises);
+        Thread.sleep(5000);
+        sendMessageToMaster(newRedisMaster, 200);
+
+        Thread.sleep(1000);
+        newRedisMaster.setMaster(null);
+
+        assertMultiDcGtid(newRedisMaster);
+        assertReplOffset(newRedisMaster);
+    }
+
+
     @Override
     protected KeeperConfig getKeeperConfig() {
         return new TestKeeperConfig(1 << 20, 100, 100 * (1 << 20), 2000);
