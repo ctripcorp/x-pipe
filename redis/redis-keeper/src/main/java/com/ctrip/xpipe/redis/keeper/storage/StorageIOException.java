412832f2c9195a73c822b20935e369720f043d41
chenan.zhang <chenanzhang@trip.com>
2026-06-26 19:37:53 +0800

    tmp modify api

---
+++ redis/redis-keeper/src/main/java/com/ctrip/xpipe/redis/keeper/storage/StorageIOException.java
@@ -0,0 +1,12 @@
+package com.ctrip.xpipe.redis.keeper.storage;
+
+public class StorageIOException extends RuntimeException {
+
+    public StorageIOException(Throwable cause) {
+        super(cause);
+    }
+
+    public StorageIOException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
