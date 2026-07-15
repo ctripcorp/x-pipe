3c7f52381201692c3872ef87a721b75b5a9c526e
chenan.zhang <chenanzhang@trip.com>
2026-07-15 12:55:57 +0800

    segment cache init

--- redis/redis-keeper/src/main/java/com/ctrip/xpipe/redis/keeper/storage/CacheEntry.java
+++ redis/redis-keeper/src/main/java/com/ctrip/xpipe/redis/keeper/storage/CacheEntry.java
@@ -1,5 +1,17 @@
 package com.ctrip.xpipe.redis.keeper.storage;
 
+import io.netty.buffer.ByteBuf;
+
+import java.util.concurrent.ConcurrentHashMap;
+
 abstract class CacheEntry {
     int refCount;
+    boolean writerOpen = false;
+    // -1: no cache data yet
+    volatile long cacheStartOffset = -1;
+    // Exclusive upper bound of all cached data; also the max readable offset for the file.
+    volatile long cacheEndOffset = 0;
+    // Exclusive upper bound of data written to the backing FS.
+    volatile long writtenToFsOffset = 0;
+    final ConcurrentHashMap<Long, ByteBuf> chunks = new ConcurrentHashMap<>();
 }
