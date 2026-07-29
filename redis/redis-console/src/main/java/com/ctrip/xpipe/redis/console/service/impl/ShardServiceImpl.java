ca546d82798fa7acb79ba82a250e8d8c89ca245a
hany_li <hany_li@trip.com>
2026-07-29 15:09:05 +0800

    multi one way azgroup

--- redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/service/impl/ShardServiceImpl.java
+++ redis/redis-console/src/main/java/com/ctrip/xpipe/redis/console/service/impl/ShardServiceImpl.java
@@ -445,13 +445,24 @@ public class ShardServiceImpl extends AbstractConsoleService<ShardTblDao> implem
 
 		ClusterTbl clusterTbl = clusterService.find(clusterName);
 		// create dcClusterShard in all dcClusters of cluster if dcClusterTbls is null
-        if (dcClusterTbls == null) {
-            dcClusterTbls = dcClusterService.findClusterRelated(clusterTbl.getId()).stream().filter(dcClusterTbl -> {
-                ClusterType azGroupType =
-                    azGroupClusterRepository.selectAzGroupTypeById(dcClusterTbl.getAzGroupClusterId());
-                return azGroupType != ClusterType.SINGLE_DC;
-            }).collect(Collectors.toList());
-        }
+		if (dcClusterTbls == null) {
+			List<DcClusterTbl> allDcClusterTbls = dcClusterService.findClusterRelated(clusterTbl.getId());
+			Set<Long> oneWayAzGroupClusterIds = new HashSet<>();
+			List<DcClusterTbl> filtered = new ArrayList<>();
+			for (DcClusterTbl dcClusterTbl : allDcClusterTbls) {
+				ClusterType azGroupType =
+					azGroupClusterRepository.selectAzGroupTypeById(dcClusterTbl.getAzGroupClusterId());
+				if (azGroupType != ClusterType.SINGLE_DC) {
+					oneWayAzGroupClusterIds.add(dcClusterTbl.getAzGroupClusterId());
+					filtered.add(dcClusterTbl);
+				}
+			}
+			if (oneWayAzGroupClusterIds.size() > 1) {
+				throw new BadRequestException(
+					String.format("Cluster %s has multiple ONE_WAY azGroups, use createRegionShard instead", clusterName));
+			}
+			dcClusterTbls = filtered;
+		}
 
 		List<DcClusterShardTbl> dcClusterShardTbls = new LinkedList<>();
 		for (DcClusterTbl dcClusterTbl : dcClusterTbls) {
