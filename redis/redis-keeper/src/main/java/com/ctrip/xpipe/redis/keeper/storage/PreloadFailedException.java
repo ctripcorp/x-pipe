3949ffc448d491c59189e5862643a973503c940f
chenan.zhang <chenanzhang@trip.com>
2026-09-03 17:03:48 +0800

    delete useless code

--- redis/redis-keeper/src/main/java/com/ctrip/xpipe/redis/keeper/storage/PreloadFailedException.java
+++
@@ -1,9 +0,0 @@
-package com.ctrip.xpipe.redis.keeper.storage;
-
-// Thrown when FULL_CACHE preload fails during write before data is cached.
-public class PreloadFailedException extends RuntimeException {
-
-    public PreloadFailedException(String message) {
-        super(message);
-    }
-}
